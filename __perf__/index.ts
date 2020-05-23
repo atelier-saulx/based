36b32cdf749d271ab7d6b41c697cfd761370aae9
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-05-23 22:38:38 +0200

    Improve perf tests

--- __perf__/index.ts
+++ __perf__/index.ts
@@ -1,16 +1,20 @@
 import { PerformanceObserver } from 'perf_hooks';
+import printResult from './util/print-result';
+import finalSize from './final-size';
 import modify from './modify';
 import serialization from './serialization';
 
 const tests = [
+	finalSize,
 	modify,
 	serialization
 ];
 100000.00
 const obs = new PerformanceObserver((list) => {
 	const entry = list.getEntries()[0];
-	const duration = `${entry.duration.toFixed(2)}`;
-	console.log(`${entry.name.padEnd(40)} ${duration.padStart(11)} ms`);
+	const duration = entry.duration.toFixed(2);
+
+	printResult(entry.name, duration, 'ms');
 });
 obs.observe({ entryTypes: ['function'] });
 
