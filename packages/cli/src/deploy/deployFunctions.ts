a2801811ada202cf9b514fd985f7407e525f6bf4
Nuno Frade <nfrade.tribal@gmail.com>
2022-04-19 10:45:49 +0200

    Adds latest fixes from auth and vault branches

---
+++ packages/cli/src/deploy/deployFunctions.ts
@@ -0,0 +1,56 @@
+import { Based } from '@based/client'
+import { BasedFunctionConfig } from './finder'
+import { printEmptyLine, prefixSuccess, prefixError, prefixWarn } from '../tui'
+import chalk from 'chalk'
+import ora from 'ora'
+import { Config } from '../types'
+
+export default async function (
+  client: Based,
+  envid: string,
+  config: Config,
+  fns: BasedFunctionConfig[],
+  unchangedFns: number
+) {
+  // Deploy functions
+  const s = Date.now()
+  if (fns.length > unchangedFns) {
+    printEmptyLine()
+    const spinner = ora('Deploying function(s)...').start()
+    await Promise.all(
+      fns.map(async (fun) => {
+        if (fun.status !== 'unchanged') {
+          try {
+            await client.call('updateFunction', {
+              env: envid,
+              observable: fun.observable,
+              shared: fun.shared,
+              name: fun.name,
+              code: fun.code,
+              fromFile: true,
+            })
+
+            spinner.stop()
+
+            console.info(
+              prefixSuccess +
+                `${'Succesfully deployed function'} ${chalk.blue(
+                  fun.name
+                )} ${'to'} ${chalk.blue(
+                  `${config.project}/${config.env}`
+                )} ${chalk.grey('in ' + (Date.now() - s) + 'ms')}`
+            )
+          } catch (err) {
+            spinner.stop()
+            console.info(
+              prefixError + chalk.red('Cannot deploy function'),
+              err.message
+            )
+          }
+        }
+      })
+    )
+  } else {
+    console.info(prefixWarn + `No functions to deploy`)
+  }
+}
