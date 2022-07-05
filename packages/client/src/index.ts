a76f54f307b0f3b2b73cd108784603197dd15783
Jim de Beer <beerdejim@gmail.com>
2022-07-05 16:53:51 +0200

    same token does not break

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -810,30 +810,34 @@ export class Based extends Emitter {
     options?: SendTokenOptions
   ): Promise<false | string | number> {
     return new Promise((resolve) => {
-      this.client.auth.push(resolve)
-      if (
-        (token && token !== this.client.token) ||
-        (token === false && this.client.token)
-      ) {
-        if (typeof token === 'string') {
-          const { renewOptions, refreshToken, ...redactedOptions } =
-            options || {}
-          if (renewOptions) {
-            this.client.renewOptions = renewOptions
-          }
-          if (refreshToken) {
-            this.client.renewOptions = {
-              ...this.client.renewOptions,
-              refreshToken,
+      if (token && token === this.client.token) {
+        resolve(token)
+      } else {
+        this.client.auth.push(resolve)
+        if (
+          (token && token !== this.client.token) ||
+          (token === false && this.client.token)
+        ) {
+          if (typeof token === 'string') {
+            const { renewOptions, refreshToken, ...redactedOptions } =
+              options || {}
+            if (renewOptions) {
+              this.client.renewOptions = renewOptions
             }
+            if (refreshToken) {
+              this.client.renewOptions = {
+                ...this.client.renewOptions,
+                refreshToken,
+              }
+            }
+            sendToken(this.client, token, redactedOptions)
+          } else {
+            // this is very important
+            // may need to add a req Id (and a timer how long it takes)
+            sendToken(this.client)
           }
-          sendToken(this.client, token, redactedOptions)
-        } else {
-          // this is very important
-          // may need to add a req Id (and a timer how long it takes)
-          sendToken(this.client)
+          this.emit('auth', token)
         }
-        this.emit('auth', token)
       }
     })
   }
