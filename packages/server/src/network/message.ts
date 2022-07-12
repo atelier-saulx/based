05c10c490faae950e813ec0a51499322973d998f
Jim de Beer <beerdejim@gmail.com>
2022-07-12 17:56:22 +0200

    clean

--- packages/server/src/network/message.ts
+++ packages/server/src/network/message.ts
@@ -2,37 +2,9 @@ import uws from '@based/uws'
 import zlib from 'zlib'
 import { isObservableFunctionSpec } from '../functions'
 import { BasedServer } from '../server'
-
-const decodeHeader = (
-  nr: number
-): { type: number; isDeflate: boolean; len: number } => {
-  const len = nr >> 4
-  const meta = nr & 15
-  const type = meta >> 1
-  const isDeflate = meta & 1
-  return {
-    type,
-    isDeflate: isDeflate === 1,
-    len,
-  }
-}
+import { decodeHeader, readUint8 } from '../utils'
 
 const textDecoder = new TextDecoder()
-
-const readUint8 = (buff: Uint8Array, start: number, len: number): number => {
-  let n = 0
-  const s = len - 1 + start
-  for (let i = s; i >= start; i--) {
-    n = n * 256 + buff[i]
-  }
-  return n
-}
-
-/*
-function toArrayBuffer(buffer) {
-  return buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
-}
-*/
 
 const reader = (
   server: BasedServer,
@@ -42,26 +14,23 @@ const reader = (
 ): number => {
   const { len, isDeflate, type } = decodeHeader(readUint8(arr, start, 4))
   if (type === 0) {
+    // make this into a function
+
     const reqId = readUint8(arr, start + 4, 3)
     const namelen = arr[7]
     const name = new Uint8Array(arr.slice(start + 8, start + 8 + namelen))
     const nameParsed = textDecoder.decode(name)
 
-    // allowing chunks as well bit more annoying...
-
-    // TEMP
     const payload = new Uint8Array(
       arr.slice(start + 8 + namelen, start + len + 4)
     )
     let p
-    // this will happen in the worker not here...
     if (!isDeflate) {
       p = textDecoder.decode(payload)
     } else {
       const buffer = zlib.inflateRawSync(payload)
       p = textDecoder.decode(buffer)
     }
-    // console.info(nameParsed, reqId, isDeflate, p)
 
     server.functions
       .get(nameParsed)
