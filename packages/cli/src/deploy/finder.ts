f3e969158841ca47ab4f1089aaee2b752f0b99db
Nuno Frade <nfrade.tribal@gmail.com>
2022-07-18 14:34:31 +0200

    Adds cli support for arrays in based.config.js files

--- packages/cli/src/deploy/finder.ts
+++ packages/cli/src/deploy/finder.ts
@@ -4,16 +4,18 @@ import { fsWalk } from '@saulx/fs-walker'
 import { GlobalOptions } from '../command'
 import { DeployOptions } from '.'
 
-export type BasedFunctionConfig = {
-  path: string
+type BasedFunctionConfigFile = {
   name: string
   observable: boolean
   shared: boolean
-  code?: string
-  status?: 'update' | 'new' | 'unchanged' | 'err'
-  fromFile?: boolean
   bundle?: boolean
+  entry?: string
 }
+export type BasedFunctionConfig = BasedFunctionConfigFile & {
+  path: string
+  code?: string
+}
+
 /**
  * Recursively finds schema files and functions that conform to the Based project file structure.
  * Each function must be in its own folder and contain an index.js/.ts file, and a
@@ -70,45 +72,63 @@ export async function findSchemaAndFunctions(
     async (fnConfigFile, _info) => {
       const folder = path.parse(path.resolve(fnConfigFile)).dir
 
-      let basedConfig: {
-        name: string
-        observable: boolean
-        shared: boolean
-        bundle: boolean
-      }
+      let basedConfig: BasedFunctionConfigFile | BasedFunctionConfigFile[]
       try {
         basedConfig = require(fnConfigFile)
       } catch (e) {
         const configBody: string = await fs.readFile(fnConfigFile, 'utf8')
         basedConfig = eval(configBody)
       }
-      const configMandatoryFields = ['name', 'observable']
-      for (const prop of configMandatoryFields) {
-        if (!(prop in basedConfig)) {
-          throw new Error(
-            `Missing mandatory field "${prop}" for function at "./${folder}/based.config.js"`
-          )
-        }
-      }
 
-      let indexesFound = []
-      const dir = await fs.readdir(folder)
-      ;['index.js', 'index.ts'].forEach((indexFileName) => {
-        if (dir.includes(indexFileName)) {
-          indexesFound.push(path.join(folder, indexFileName))
+      if (Array.isArray(basedConfig)) {
+        for (const basedConfigItem of basedConfig) {
+          ;['name', 'observable', 'entry'].forEach((fieldName) => {
+            if (typeof basedConfigItem[fieldName] === 'undefined') {
+              throw new Error(
+                `Missing mandatory field "${fieldName}" for function at "./${folder}/based.config.js"`
+              )
+            }
+          })
+          try {
+            await fs.access(path.join(folder, basedConfigItem.entry))
+          } catch (_err) {
+            throw new Error(
+              `Cannot find entry file for function ${basedConfigItem.name} at "./${folder}/based.config.js"`
+            )
+          }
+          fns.push({
+            path: path.join(folder, basedConfigItem.entry),
+            ...basedConfigItem,
+          })
         }
-      })
+      } else {
+        ;['name', 'observable'].forEach((fieldName) => {
+          if (!(fieldName in basedConfig)) {
+            throw new Error(
+              `Missing mandatory field "${fieldName}}" for function at "./${folder}/based.config.js"`
+            )
+          }
+        })
 
-      if (indexesFound.length > 1) {
-        throw new Error(`Multiple indexes found for function at ${folder}/`)
+        let indexesFound = []
+        const dir = await fs.readdir(folder)
+        ;['index.js', 'index.ts'].forEach((indexFileName) => {
+          if (dir.includes(indexFileName)) {
+            indexesFound.push(path.join(folder, indexFileName))
+          }
+        })
+
+        if (indexesFound.length > 1) {
+          throw new Error(`Multiple indexes found for function at ${folder}/`)
+        }
+        if (indexesFound.length < 1) {
+          throw new Error(`No index file found for function at ${folder}/`)
+        }
+        fns.push({
+          path: indexesFound[0],
+          ...basedConfig,
+        })
       }
-      if (indexesFound.length < 1) {
-        throw new Error(`No index file found for function at ${folder}/`)
-      }
-      fns.push({
-        path: indexesFound[0],
-        ...basedConfig,
-      })
     },
     {
       itemMatchFn: async (item) =>
