3f602dece9c3c124211b6aa4dc19a0ef8b4b59a7
Youri Daamen <youzi@users.noreply.github.com>
2022-08-02 13:23:57 +0200

    Removed commented code

--- packages/cli/src/buildApp/index.ts
+++ packages/cli/src/buildApp/index.ts
@@ -68,34 +68,4 @@ command(
     }`
     ),
   ])
-
-  // printHeader(options, config)
-  // const output: Logout = { data: [] }
-  // const { token } = await getToken(config.cluster)
-  // if (!token) {
-  //   fail('Not logged in.', output, options)
-  // }
-  // const p = getBasedLocation(config.cluster)
-  // await emptyDir(p)
-  // const client = makeClient(config.cluster)
-  // client.auth(token)
-  // let spinner: ora.Ora
-  // try {
-  //   if (options.output === 'fancy') {
-  //     spinner = ora('Logging out').start()
-  //   }
-  //   await client.call('logout', {
-  //     token,
-  //   })
-  //   spinner && spinner.clear()
-  //   if (options.output === 'fancy') {
-  //     printEmptyLine()
-  //     console.info(prefixSuccess + 'Logged out.')
-  //   }
-  //   process.exit(0)
-  // } catch (err) {
-  //   spinner && spinner.clear()
-  //   options.debug && printError(err)
-  //   fail('Error loggin out.', output, options)
-  // }
 })
