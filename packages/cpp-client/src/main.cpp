a97d747d2ca1e2b4127b597d2f777c972eb42584
Francesco Marciani <marciani3@gmail.com>
2022-09-05 14:03:16 +0200

    scaffolding for cpp-core client

--- packages/cpp-client/src/main.cpp
+++ packages/cpp-client/src/main.cpp
@@ -5,23 +5,38 @@
 
 #include "client.hpp"
 #include "connection.hpp"
+#include "incoming.hpp"
 
 class HandlerStore {};
 
 int main() {
     std::cout << "hello yes" << std::endl;
 
-    BasedClient client("ws://localhost:9910");
+    // Decoder dec;
 
-    client.connect();
+    // unsigned char arr[4] = {112, 0, 0, 0};
 
-    int id = client.observe(
-        "based_observe",
-        [](std::string_view data, int checksum) { std::cout << data << std::endl; },
-        "{$id: \"flurp\", $all: true}",
-        [](std::string_view error) { std::cerr << error << std::endl; }, ObservableOpts(true, 100));
+    // std::string s(reinterpret_cast<char const*>(arr), 4);
 
-    std::cout << "OBS started, id = " << id << std::endl;
+    // int32_t res = dec.readUint8(s, 0, 4);
+    // dec.getDataType(res);
+
+    // std::cout << "res = " << res << ", 0x" << std::hex << res << std::endl;
+
+    BasedClient client;
+
+    client.connect("ws://localhost:9910");
+
+    // int id = client.observe(
+    //     "based_observe", "{$id: \"flurp\", $all: true}",
+    //     [](std::string_view data, int checksum) { std::cout << data << std::endl; },
+    //     [](std::string_view error) { std::cerr << error << std::endl; }, ObservableOpts(true,
+    //     100));
+
+    // std::cout << "OBS started, id = " << id << std::endl;
+
+    client.function("small", "",
+                    [](std::string_view data) { std::cout << "[RES] " << data << std::endl; });
 
     while (true) {
     }
