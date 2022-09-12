931334737fc3f2b7374347b1e6cbc1c6793cefa3
Francesco Marciani <marciani3@gmail.com>
2022-09-12 16:06:13 +0200

    cpp-client: add get

--- packages/cpp-client/src/main.cpp
+++ packages/cpp-client/src/main.cpp
@@ -20,34 +20,43 @@ int main() {
         // });
 
         std::getline(std::cin, cmd);
-        if (cmd == "q") done = true;
-        if (cmd.substr(0, 1) == "r") {
-            int rem_id = atoi(cmd.substr(2).c_str());
-            client.unobserve(rem_id);
+        if (cmd == "q") {
+            done = true;
             continue;
         }
-        if (i % 2 == 0) {
-            int id = client.observe(
-                "counter", "{\"b\":\"a\",\"a\":\"b\"}",
-                [i](std::string_view data, int checksum) {
-                    std::cout << "counter n " << i << " data = " << data
-                              << ", checksum = " << checksum << std::endl;
-                },
-                NULL, ObservableOpts(true, 100));
-            std::cout << "> Added new counter observable, id = " << id << std::endl;
-            i++;
 
-        } else {
-            int id = client.observe(
-                "counterPlus", "{\"b\":\"a\",\"a\":\"b\"}",
-                [i](std::string_view data, int checksum) {
-                    std::cout << "counterPlus n " << i << " data = " << data
-                              << ", checksum = " << checksum << std::endl;
-                },
-                NULL, ObservableOpts(true, 100));
-            std::cout << "> Added new counterPlus observable, id = " << id << std::endl;
-            i++;
-        }
+        client.get("counter", "{\"b\":\"a\",\"a\":\"bababa\"}",
+                   [](std::string data) { std::cout << "I GOT data = " << data << std::endl; });
+        client.get("counter", "",
+                   [](std::string data) { std::cout << "I GOT data 2 = " << data << std::endl; });
+
+        // if (cmd.substr(0, 1) == "r") {
+        //     int rem_id = atoi(cmd.substr(2).c_str());
+        //     client.unobserve(rem_id);
+        //     continue;
+        // }
+        // if (i % 2 == 0) {
+        //     int id = client.observe(
+        //         "counter", "{\"b\":\"a\",\"a\":\"b\"}",
+        //         [i](std::string_view data, int checksum) {
+        //             std::cout << "counter n " << i << " data = " << data
+        //                       << ", checksum = " << checksum << std::endl;
+        //         },
+        //         NULL, ObservableOpts(true, 100));
+        //     std::cout << "> Added new counter observable, id = " << id << std::endl;
+        //     i++;
+
+        // } else {
+        //     int id = client.observe(
+        //         "counterPlus", "{\"b\":\"a\",\"a\":\"b\"}",
+        //         [i](std::string_view data, int checksum) {
+        //             std::cout << "counterPlus n " << i << " data = " << data
+        //                       << ", checksum = " << checksum << std::endl;
+        //         },
+        //         NULL, ObservableOpts(true, 100));
+        //     std::cout << "> Added new counterPlus observable, id = " << id << std::endl;
+        //     i++;
+        // }
     }
     // client.disconnect();
 }
