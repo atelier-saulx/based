d37f494822b6350a26164819192c151d84a12fdc
Jim de Beer <beerdejim@gmail.com>
2022-10-27 16:24:20 +0200

    fix

--- packages/server/src/worker/ws/function.ts
+++ packages/server/src/worker/ws/function.ts
@@ -6,7 +6,6 @@ import {
 import { parentPort } from 'node:worker_threads'
 import { ClientContext } from '../../types'
 import { authorize } from '../authorize'
-// add authorize
 
 export default (
   name: string,
@@ -25,111 +24,27 @@ export default (
     parsedPayload = decodePayload(payload, isDeflate)
   }
 
-  authorize(context, name, payload)   .then((ok) => {
-    //     if (!ok) {
-    //       sendError(server, client, BasedErrorCode.AuthorizeRejectedError, route)
-    //       return false
-    //     }
-    
-    //     server.functions
-    //       .install(name)
-    //       .then((spec) => {
-    //         if (spec && !isObservableFunctionSpec(spec)) {
-    //           spec
-    //             .function(payload, client)
-    //             .then((v) => {
-    //               client.ws?.send(
-    //                 encodeFunctionResponse(reqId, valueToBuffer(v)),
-    //                 true,
-    //                 false
-    //               )
-    //             })
-    //             .catch((err) => {
-    //               sendError(server, client, BasedErrorCode.FunctionError, {
-    //                 route,
-    //                 requestId: reqId,
-    //                 err,
-    //               })
-    //             })
-    //         } else {
-    //           sendError(server, client, BasedErrorCode.FunctionNotFound, route)
-    //         }
-    //       })
-    //       .catch(() => {
-    //         sendError(server, client, BasedErrorCode.FunctionNotFound, route)
-    //       })
-    //   })
-    //   .catch((err) => {
-    //     sendError(server, client, BasedErrorCode.AuthorizeFunctionError, {
-    //       route,
-    //       requestId: reqId,
-    //       err,
-    //     })
-    //     return false
-    //   })
+  authorize(context, name, payload)
+    .then((ok) => {
+      if (ok) {
+        return false
+      }
 
-  fn(parsedPayload, {})
-    .then((v) => {
-      parentPort.postMessage({
-        id,
-        payload: encodeFunctionResponse(reqId, valueToBuffer(v)),
-      })
+      fn(parsedPayload, {})
+        .then((v) => {
+          parentPort.postMessage({
+            id,
+            payload: encodeFunctionResponse(reqId, valueToBuffer(v)),
+          })
+        })
+        .catch((err) => {
+          parentPort.postMessage({
+            id,
+            err,
+          })
+        })
     })
     .catch((err) => {
-      parentPort.postMessage({
-        id,
-        err,
-      })
+      console.error('WRONGDX', err)
     })
 }
-
-// sharedBuf
-
-// authorize has to go from here (scince we dont parse the payload yet)
-
-// authorize has to be a path to an authorize function as well....
-
-// server.auth.config
-//   .authorize(server, client, name, payload)
-//   .then((ok) => {
-//     if (!ok) {
-//       sendError(server, client, BasedErrorCode.AuthorizeRejectedError, route)
-//       return false
-//     }
-
-//     server.functions
-//       .install(name)
-//       .then((spec) => {
-//         if (spec && !isObservableFunctionSpec(spec)) {
-//           spec
-//             .function(payload, client)
-//             .then((v) => {
-//               client.ws?.send(
-//                 encodeFunctionResponse(reqId, valueToBuffer(v)),
-//                 true,
-//                 false
-//               )
-//             })
-//             .catch((err) => {
-//               sendError(server, client, BasedErrorCode.FunctionError, {
-//                 route,
-//                 requestId: reqId,
-//                 err,
-//               })
-//             })
-//         } else {
-//           sendError(server, client, BasedErrorCode.FunctionNotFound, route)
-//         }
-//       })
-//       .catch(() => {
-//         sendError(server, client, BasedErrorCode.FunctionNotFound, route)
-//       })
-//   })
-//   .catch((err) => {
-//     sendError(server, client, BasedErrorCode.AuthorizeFunctionError, {
-//       route,
-//       requestId: reqId,
-//       err,
-//     })
-//     return false
-//   })
