8b13dd918e9dad15dfa67f1877f0d322d6099eea
Olli Vanhoja <olli.vanhoja@gmail.com>
2022-11-09 17:25:53 +0100

    Split build scripts

--- packages/cpp-client/build-darwin.sh
+++ packages/cpp-client/build-darwin.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-# Mac OS
+set -e
+
 mkdir -p build_darwin
 pushd build_darwin
 cmake -G"Xcode" -DCMAKE_TOOLCHAIN_FILE=../darwin_x64.cmake -S .. -B .
-cmake --build . --config Release
-popd
-
-# Ios
-mkdir -p build_ios
-pushd build_ios
-cmake -G"Xcode" -DCMAKE_TOOLCHAIN_FILE=../ios_arm64.cmake -S .. -B .
 cmake --build . --config Release
 popd
