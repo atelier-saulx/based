c604231b08aaccfcebc7383c905a060ffd9a0cc9
Jim de Beer <beerdejim@gmail.com>
2022-11-09 22:23:03 +0100

    fix

---
+++ packages/core/test/browser/obsRandomUpdateCrash/index.ts
@@ -0,0 +1,16 @@
+export default async (payload, update) => {
+  let cnt = 0
+
+  update({ cnt: ++cnt })
+
+  const interval = setInterval(() => {
+    if (Math.random() > 0.75) {
+      throw new Error('RANDO CRASH!')
+    }
+
+    update({ cnt: ++cnt })
+  }, 5e3)
+  return () => {
+    clearInterval(interval)
+  }
+}
