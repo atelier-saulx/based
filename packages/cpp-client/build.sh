203263a88487a90e9d53ec8fd2e821668b053a70
Olli Vanhoja <olli.vanhoja@gmail.com>
2022-11-09 16:57:35 +0100

    CMake cross-compilation for Apple platforms

--- packages/cpp-client/build.sh
+++ packages/cpp-client/build.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-mkdir -p build
-cd build
-cmake -S .. -B .
-make
+
+# Mac OS
+mkdir -p build_darwin
+pushd build_darwin
+cmake -G"Xcode" -DCMAKE_TOOLCHAIN_FILE=../darwin_x64.cmake -S .. -B .
+cmake --build . --config Release
+popd
+
+# Ios
+mkdir -p build_ios
+pushd build_ios
+cmake -G"Xcode" -DCMAKE_TOOLCHAIN_FILE=../ios_arm64.cmake -S .. -B .
+cmake --build . --config Release
+popd
