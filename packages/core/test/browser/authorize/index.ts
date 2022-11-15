af17dea065434ab02974e383b78597f3cdea4c6d
Jim de Beer <beerdejim@gmail.com>
2022-11-15 15:45:18 +0100

    auth

--- packages/core/test/browser/authorize/index.ts
+++ packages/core/test/browser/authorize/index.ts
@@ -1,4 +1,25 @@
 export default async (client, name, payload) => {
+  /*
+
+  js
+user?._token == apiKey
+const token = await user._token
+const token = await user.token(envKey)
+if (user.isBasedUser) {
+        const isValid = await user.token()
+        ...
+}
+
+js
+√ based.set(model)
+√ based.get(query)
+√ based.delete(query)
+based.opts.env === 'dev'
+based.secret(envKey)
+based.observe(query, update)
+
+  */
+
   console.info('--> Need to auth', name, payload)
   return true
 }
