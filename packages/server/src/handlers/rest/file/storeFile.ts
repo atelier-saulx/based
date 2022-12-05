b8e2f7eda27ff8411ac46cf1976e62e1aca814c1
Francesco Marciani <marciani3@gmail.com>
2022-12-05 12:04:42 +0100

    catch error if fileupload fails

--- packages/server/src/handlers/rest/file/storeFile.ts
+++ packages/server/src/handlers/rest/file/storeFile.ts
@@ -15,33 +15,42 @@ export default async (
     throw new Error('No file upload handler available!')
   }
 
-  const result = await storeFile({
-    based: server.based,
-    stream,
-    id: opts.id,
-    mimeType: opts.type,
-    extension: opts.extension,
-    size: opts.size,
-    user,
-  })
+  try {
+    const result = await storeFile({
+      based: server.based,
+      stream,
+      id: opts.id,
+      mimeType: opts.type,
+      extension: opts.extension,
+      size: opts.size,
+      user,
+    })
 
-  const setObj: any = {
-    $id: opts.id,
-    src: result.src,
-    origin: result.origin,
-    status: result.status,
-    version: result.version,
-    mimeType: result.mimeType || opts.type,
+    const setObj: any = {
+      $id: opts.id,
+      src: result.src,
+      origin: result.origin,
+      status: result.status,
+      version: result.version,
+      mimeType: result.mimeType || opts.type,
+    }
+
+    if (result.thumb) {
+      setObj.thumb = result.thumb
+    }
+
+    if (result.statusText) {
+      setObj.statusText = result.statusText
+    }
+
+    // add version
+    server.based.set(setObj)
+  } catch (err) {
+    console.error('Could not upload file, error:\n', err)
+    server.based.set({
+      $id: opts.id,
+      status: 4,
+      statusText: 'error',
+    })
   }
-
-  if (result.thumb) {
-    setObj.thumb = result.thumb
-  }
-
-  if (result.statusText) {
-    setObj.statusText = result.statusText
-  }
-
-  // add version
-  server.based.set(setObj)
 }
