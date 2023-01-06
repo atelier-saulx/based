a4a91589cebd9708ccc0108c78e05ec22be2a1db
Jim de Beer <beerdejim@gmail.com>
2023-01-06 11:22:22 +0100

    fix req id

--- packages/core/test/authorize.ts
+++ packages/core/test/authorize.ts
@@ -1,51 +1,38 @@
 import test from 'ava'
 import { BasedCoreClient } from '../src/index'
-import createServer from '@based/edge-server'
+import { createSimpleServer } from '@based/server'
 import { BasedError, BasedErrorCode } from '../src/types/error'
 import { wait } from '@saulx/utils'
-import { join } from 'path'
 
 const setup = async () => {
   const coreClient = new BasedCoreClient()
 
-  const store = {
-    hello: {
-      observable: false,
-      functionPath: join(__dirname, '/functions/hello'),
-    },
-    counter: {
-      observable: true,
-      functionPath: join(__dirname, '/functions/counter'),
-    },
-  }
-
-  const server = await createServer({
+  const server = await createSimpleServer({
     port: 9910,
     functions: {
-      memCacheTimeout: 3e3,
-      idleTimeout: 3e3,
-      route: ({ name }) => {
-        if (name && store[name]) {
-          return {
-            name,
-            observable: store[name].observable,
-          }
+      hello: async (payload) => {
+        if (payload) {
+          return payload
         }
-        return false
+        return 'flap'
       },
-      uninstall: async () => {
-        return true
-      },
-      install: async ({ name }) => {
-        if (store[name]) {
-          return {
-            name,
-            checksum: 1,
-            ...store[name],
-          }
-        } else {
-          return false
+    },
+    observables: {
+      counter: async (_payload, update) => {
+        let cnt = 0
+        update(cnt)
+        const counter = setInterval(() => {
+          update(++cnt)
+        }, 1000)
+        return () => {
+          clearInterval(counter)
         }
+      },
+    },
+    auth: {
+      authorize: async (context, name) => {
+        console.info('flap??', name, '---', context.session?.authState)
+        return context.session?.authState === 'mock_token'
       },
     },
   })
@@ -53,15 +40,11 @@ const setup = async () => {
 }
 
 test.serial('authorize functions', async (t) => {
-  t.timeout(1000)
+  t.timeout(10000)
 
   const token = 'mock_token'
 
   const { coreClient, server } = await setup()
-
-  server.auth.updateConfig({
-    authorizePath: join(__dirname, 'functions', 'auth.js'),
-  })
 
   t.teardown(() => {
     coreClient.disconnect()
@@ -74,14 +57,19 @@ test.serial('authorize functions', async (t) => {
     },
   })
 
+  console.info('connect???')
+
   await t.throwsAsync(
-    coreClient.function('hello', {
+    coreClient.call('hello', {
       bla: true,
     })
   )
+
+  console.info('conblaaar!nect???')
+
   await coreClient.auth(token)
   await t.notThrowsAsync(
-    coreClient.function('hello', {
+    coreClient.call('hello', {
       bla: true,
     })
   )
@@ -95,17 +83,6 @@ test.serial('authorize observe', async (t) => {
   const { coreClient, server } = await setup()
 
   let counter: NodeJS.Timer
-
-  server.functions.update({
-    observable: true,
-    name: 'counter',
-    checksum: 2,
-    functionPath: join(__dirname, 'functions', 'counter.js'),
-  })
-
-  server.auth.updateConfig({
-    authorizePath: join(__dirname, 'functions', 'auth.js'),
-  })
 
   t.teardown(() => {
     coreClient.disconnect()
@@ -164,17 +141,6 @@ test.serial('authorize after observe', async (t) => {
 
   const { coreClient, server } = await setup()
   let counter: NodeJS.Timer
-
-  server.functions.update({
-    observable: true,
-    name: 'counter',
-    checksum: 2,
-    functionPath: join(__dirname, 'functions', 'counter.js'),
-  })
-
-  server.auth.updateConfig({
-    authorizePath: join(__dirname, 'functions', 'auth.js'),
-  })
 
   t.teardown(() => {
     coreClient.disconnect()
--- packages/server/src/incoming/ws/function.ts
+++ packages/server/src/incoming/ws/function.ts
@@ -131,6 +131,7 @@ export const functionMessage = (
 
       if (!ok) {
         sendError(server, ctx, BasedErrorCode.AuthorizeRejectedError, {
+          requestId,
           route,
         })
         return false
@@ -140,6 +141,7 @@ export const functionMessage = (
     })
     .catch((err) => {
       sendError(server, ctx, BasedErrorCode.AuthorizeFunctionError, {
+        requestId,
         route,
         err,
       })
