5cd6152eca27fc65e687b72138d336aa279cd5b7
Jim de Beer <beerdejim@gmail.com>
2023-02-22 15:58:39 +0100

    go

--- packages/client/src/persistentStorage/node.ts
+++ packages/client/src/persistentStorage/node.ts
@@ -1,20 +1,26 @@
 import { AuthState, BasedClient } from '..'
 import { join } from 'path'
 import gzip from 'zlib'
-import fs from 'fs/promises'
+import fs from 'fs'
 import { promisify } from 'util'
 
-const compress = promisify(gzip.createGzip)
+const writeFile = promisify(fs.writeFile)
+const readFile = promisify(fs.readFile)
+const rm = promisify(fs.rm)
+const stat = promisify(fs.stat)
 
-// total gets gzipped
+const compress = promisify(gzip.gzip)
+const unzip = promisify(gzip.gunzip)
 
 const writeToStorage = (client: BasedClient) => {
   if (!client.storageBeingWritten) {
-    client.storageBeingWritten = setTimeout(() => {
-      client.storageBeingWritten = null
+    client.storageBeingWritten = setTimeout(async () => {
+      const file = join(
+        client.storagePath,
+        'based-' + client.storageEnvKey + '.storage'
+      )
       // id, c, value
       const cache: any[] = []
-
       client.cache.forEach((c, id) => {
         if (c.persistent) {
           cache.push(id, c.checksum, c.value)
@@ -24,11 +30,9 @@ const writeToStorage = (client: BasedClient) => {
       if (client.authState.persistent) {
         f.authState = client.authState
       }
-
-      fs.writeFile(client.storagePath, JSON.stringify(f))
+      client.storageBeingWritten = null
+      await writeFile(file, await compress(JSON.stringify(f)))
     }, 1e3)
-    // just write the cache and authState
-    //   client.storageStaged
   }
 }
 
@@ -40,29 +44,48 @@ export const initStorageNode = async (client: BasedClient) => {
   console.info('    [Based-client] Start persistent storage')
   console.info('   ', file)
   console.info('')
-  const stat = await fs.stat(file).catch(() => null)
-  if (stat) {
-    //
-  } else {
-    await fs.writeFile(file, '').catch((err) => {
-      if (err) {
-        console.error(
-          '     [Based-client] Failed creating persistent storage, cannot write file'
-        )
-        // Do not try again
-        client.storagePath = null
+  const s = await stat(file).catch(() => null)
+  try {
+    if (s) {
+      const r = await readFile(file)
+      const unpacked = await unzip(r)
+      const c = JSON.parse(unpacked.toString())
+      try {
+        for (let i = 0; i < c.length - 3; i += 3) {
+          // go go go
+        }
+      } catch (err) {
+        console.error('    [Based-client] Corrupt persistent storage - clear')
+        clearStorageNode(client)
       }
-    })
-  }
+    } else {
+      const x = await compress(JSON.stringify({ cache: [] }))
+      await writeFile(file, x).catch((err) => {
+        if (err) {
+          console.error(
+            '     [Based-client] Failed creating persistent storage, cannot write file'
+          )
+          client.storagePath = null
+        }
+      })
+    }
+  } catch (err) {}
 }
 
 export const clearStorageNode = async (client: BasedClient) => {
+  const file = join(
+    client.storagePath,
+    'based-' + client.storageEnvKey + '.storage'
+  )
   clearTimeout(client.storageBeingWritten)
-  return fs.rm(client.storagePath)
+  return rm(file)
 }
 
-export const removeStorageNode = (client: BasedClient, key: string) => {
-  console.info('removeStorageNode', key)
+export const removeStorageNode = (
+  client: BasedClient,
+  // eslint-disable-next-line
+  key: string
+) => {
   writeToStorage(client)
 }
 
