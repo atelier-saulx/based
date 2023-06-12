e5ff111b3bee03964b0b163df1a68cee9a7da203
Jim de Beer <beerdejim@gmail.com>
2023-06-12 16:31:48 +0200

    channel throttle start

--- packages/client/test/throttle.ts
+++ packages/client/test/throttle.ts
@@ -62,3 +62,63 @@ test.serial('throttle', async (t) => {
   await server.destroy()
   await client.destroy()
 })
+
+test.serial('throttle channel', async (t) => {
+  const client = new BasedClient()
+  const server = new BasedServer({
+    port: 9910,
+    auth: {
+      authorize: async () => true,
+    },
+    functions: {
+      configs: {
+        counter: {
+          type: 'query',
+          throttle: 1000,
+          uninstallAfterIdleTime: 1e3,
+          fn: (_, __, update) => {
+            let cnt = 0
+            update(cnt)
+            const counter = setInterval(() => {
+              update(++cnt)
+            }, 100)
+            return () => {
+              clearInterval(counter)
+            }
+          },
+        },
+      },
+    },
+  })
+  await server.start()
+
+  client.connect({
+    url: async () => {
+      return 'ws://localhost:9910'
+    },
+  })
+
+  client.once('connect', (isConnected) => {
+    console.info('   connect', isConnected)
+  })
+  const obs1Results: any[] = []
+
+  const close = client
+    .query('counter', {
+      myQuery: 123,
+    })
+    .subscribe((d) => {
+      obs1Results.push(d)
+    })
+
+  await wait(2000)
+
+  t.is(obs1Results.length, 3)
+
+  close()
+
+  await wait(2000)
+
+  await server.destroy()
+  await client.destroy()
+})
