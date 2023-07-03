af678fc626edc6798c8c48520dd23df32eaf04fe
Jim de Beer <beerdejim@gmail.com>
2023-07-03 12:21:08 +0200

    initial setup

--- packages/schema/src/schema.ts
+++ packages/schema/src/schema.ts
@@ -1,2 +1,14 @@
 // Schema type
+// inspiration from https://json-schema.org/understanding-json-schema/index.html
+// but added a few extra types
+//   reference
+//   references
+//   set
+//   record
+
+// contentSchema can be used for JSON types as well
+// contentSchema can be used for reference / refrences
+
+// TODO parser / validator / parseOut / parseIn (parsIn after validator)
+
 export type Schema = {}
