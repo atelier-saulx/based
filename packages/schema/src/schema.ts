d87d9bc1c267895354d39732ed871987699bafa5
Jim de Beer <beerdejim@gmail.com>
2023-07-03 13:15:47 +0200

    start of type

--- packages/schema/src/schema.ts
+++ packages/schema/src/schema.ts
@@ -11,4 +11,62 @@
 
 // TODO parser / validator / parseOut / parseIn (parsIn after validator)
 
-export type Schema = {}
+export type BasedSchemaFieldType =
+  | 'string'
+  | 'number'
+  | 'integer'
+  | 'set'
+  | 'reference'
+  | 'references'
+  | 'record'
+  | 'array'
+  | 'object'
+  | 'text'
+
+export type BasedSchemaFieldShared = {
+  title?: string
+  description?: string
+  readOnly?: boolean
+  writeOnly?: boolean
+  $comment?: string
+  examples?: any[] // <--- make this generic
+  default?: any // <-- make this generic
+  // enum?: any[] // <--- dont need type
+  // const?: any // <--- dont need type
+}
+
+export type BasedSchemaFieldObject = {
+  type: 'object'
+  properties: {
+    [name: string]: BasedSchemaField
+  }
+  required?: string[]
+}
+
+export type BasedSchemaPattern = string // RE ^[A-Za-z_][A-Za-z0-9_]*$
+
+export type BasedSchemaFieldRecord = {
+  type: 'record'
+  items: BasedSchemaField
+  propertyNames?: {
+    pattern?: BasedSchemaPattern
+  }
+  required?: string[]
+}
+
+// return type can be typed - sort of
+export type BasedSchemaField = BasedSchemaFieldShared & BasedSchemaFieldObject
+
+export type BasedSchemaType = {
+  fields: {
+    [name: string]: BasedSchemaField
+  }
+}
+
+// type language
+export type Schema = {
+  languages?: string[]
+  types: {
+    [type: string]: BasedSchemaType
+  }
+}
