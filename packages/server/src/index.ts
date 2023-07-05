945aaef3fb593715a8fff2f0cda66b3f5b2ab1be
Tony Kovanen <tony.kovanen@gmail.com>
2023-07-05 13:22:18 +0200

    beginnings of runnable

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,6 +3,7 @@ import fs from 'fs'
 import { join } from 'path'
 import mkdirp from 'mkdirp'
 
+import chalk from 'chalk'
 import getPort from 'get-port'
 
 import { Options, ServerOptions } from './types'
@@ -49,4 +50,130 @@ const resolveOpts = async (opts: Options): Promise<ServerOptions> => {
   }
 
   return parsedOpts
+}
+
+const validate = (
+  opts: ServerOptions,
+  required: string[],
+  illegal: string[]
+): string | undefined => {
+  for (const field of required) {
+    if (!opts[field]) {
+      return `${field} is required`
+    }
+  }
+
+  for (const field of illegal) {
+    if (opts[field]) {
+      return `${field} is not a valid option`
+    }
+  }
+
+  if (opts.name === 'registry') {
+    return `Registry is a reserved name`
+  }
+
+  if (!opts.port) {
+    return `no port provided`
+  }
+
+  if (!opts.host) {
+    return `no host provided`
+  }
+
+  if (typeof opts.port !== 'number') {
+    return `port is not a number ${opts.port}`
+  }
+
+  if (typeof opts.dir !== 'string') {
+    return `string is not a string ${opts.dir}`
+  }
+}
+
+export async function startOrigin(opts: Options): Promise<SelvaServer> {
+  const parsedOpts = await resolveOpts(opts)
+  const err = validate(parsedOpts, ['registry', 'name'], [])
+  if (err) {
+    console.error(`Error starting origin selva server ${chalk.red(err)}`)
+    throw new Error(err)
+  }
+  if (!parsedOpts.name) {
+    parsedOpts.name = 'default'
+  }
+  return startServer('origin', parsedOpts)
+}
+
+export async function startReplica(opts: Options) {
+  const parsedOpts = await resolveOpts(opts)
+
+  const err = validate(parsedOpts, ['registry', 'name'], ['backups'])
+  if (err) {
+    console.error(`Error starting replica selva server ${chalk.red(err)}`)
+    throw new Error(err)
+  }
+  if (!parsedOpts.name && parsedOpts.default) {
+    parsedOpts.name = 'default'
+  }
+  return startServer('replica', parsedOpts)
+}
+
+export async function startRegistry(opts: Options): Promise<SelvaServer> {
+  const parsedOpts = await resolveOpts(opts)
+
+  const err = validate(
+    parsedOpts,
+    [],
+    ['registry', 'backups', 'name', 'default']
+  )
+
+  parsedOpts.name = 'registry'
+
+  if (err) {
+    console.error(`Error starting registry selva server ${chalk.red(err)}`)
+    throw new Error(err)
+  }
+  return startServer('registry', parsedOpts)
+}
+
+export async function start(opts: Options) {
+  const parsedOpts = await resolveOpts(opts)
+
+  // TODO: for now all in different ports, fix later
+  const err = validate(
+    parsedOpts,
+    [],
+    ['registry', 'backups', 'name', 'default']
+  )
+
+  if (err) {
+    console.error(`Error starting selva server ${chalk.red(err)}`)
+    throw new Error(err)
+  }
+
+  const registry = await startServer('registry', {
+    ...parsedOpts,
+    name: 'registry',
+  })
+
+  const origin = await startOrigin({
+    name: 'default',
+    registry,
+    // @ts-ignore
+    dir: opts.dir,
+    pipeRedisLogs: parsedOpts.pipeRedisLogs || {
+      stdout: true,
+      stderr: true,
+    },
+  })
+
+  registry.on('close', async () => {
+    // TODO: Remove comment
+    await origin.destroy()
+    // await timeseries.destroy()
+    // await tsRegistry.destroy()
+    // timeseriesPostgres.destroy() // not async
+    // await timeseriesWorker.destroy()
+  })
+
+  return registry
 }
---
+++ packages/server/src/server/index.ts
@@ -0,0 +1,160 @@
+type ServerType = 'origin' | 'replica'
+type ServerDescriptor = {
+  name: string
+  host: string
+  port: number
+  type: ServerType
+}
+
+import { ServerOptions } from '../types'
+import { EventEmitter } from 'events'
+import chalk from 'chalk'
+import {
+  BackupFns,
+  saveAndBackUp,
+  scheduleBackups,
+  loadBackup,
+} from '../backups'
+import { spawn, ChildProcess } from 'child_process'
+import path from 'path'
+
+export class SelvaServer extends EventEmitter {
+  public pm: ChildProcess
+  public type: ServerType
+  public port: number
+  public host: string
+  public name: string
+  public origin: ServerDescriptor
+  private backupFns: BackupFns
+  private backupDir: string
+  private backupCleanup: Function
+
+  constructor(serverType: ServerType) {
+    super()
+    this.setMaxListeners(10000)
+    this.type = serverType
+
+    this.on('error', () => {
+      // console.error(err)
+    })
+
+    // beforeExit.do(() => {
+    //   return this.destroy()
+    // })
+  }
+
+  async start(opts: ServerOptions) {
+    if (this.pm) {
+      return
+    }
+
+    this.port = opts.port
+    this.host = opts.host
+    this.name = opts.name
+
+    if (opts.backups && opts.backups.backupFns) {
+      this.backupFns = await opts.backups.backupFns
+    }
+
+    this.backupDir = opts.dir
+
+    if (opts.backups && opts.backups.loadBackup) {
+      await loadBackup(this.backupDir, this.backupFns)
+    }
+
+    const execPath = path.join(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      '..',
+      'selvad',
+      'selvad'
+    )
+
+    this.pm = spawn(execPath, [], {
+      env: {
+        ...process.env,
+        ...{
+          SELVA_MALLOC_CONF:
+            'prof:true,prof_active:true,prof_leak:true,lg_prof_interval:30,lg_prof_sample:17,prof_prefix:jeprof.out',
+          LOCPATH: path.join(
+            execPath,
+            '..',
+            'binaries',
+            'Linux_x86_64',
+            'locale'
+          ),
+          SELVA_PORT: String(this.port),
+          SERVER_SO_REUSE: '1',
+          SELVA_REPLICATION_MODE: '1',
+          AUTO_SAVE_INTERVAL: '1200',
+        },
+      },
+      stdio: 'inherit',
+    })
+
+    if (this.type === 'origin' && opts.backups && opts.backups.scheduled) {
+      this.backupCleanup = scheduleBackups(
+        opts.dir,
+        opts.backups.scheduled.intervalInMinutes,
+        this.backupFns
+      )
+    }
+  }
+
+  async destroy() {
+    if (!this.pm) {
+      this.pm.kill('SIGINT')
+      this.pm = undefined
+    }
+
+    // TODO: remove replica and origin from registry
+  }
+}
+
+function addSignalHandlers(server: SelvaServer): void {
+  process.setMaxListeners(10e4)
+
+  process.on('SIGINT', () => {
+    console.info('Got SIGINT, closing redis server')
+    if (server.pm) {
+      server.pm.kill('SIGINT')
+    }
+
+    setTimeout(() => {
+      console.info('Exiting after graceful shutdown')
+      process.exit(0)
+    }, 1e3 * 4).unref()
+  })
+  process.on('SIGTERM', () => {
+    console.info('Got SIGTERM, closing redis server')
+    if (server.pm) {
+      server.pm.kill('SIGTERM')
+    }
+
+    setTimeout(() => {
+      console.info('Exiting after graceful shutdown')
+      process.exit(0)
+    }, 1e3 * 4).unref()
+  })
+  process.on('exit', (code) => {
+    console.info('Process exiting with code', code)
+    server.destroy()
+    process.exit(0)
+  })
+}
+
+export const startServer = async (
+  type: ServerType,
+  opts: ServerOptions
+): Promise<SelvaServer> => {
+  const server = new SelvaServer(type)
+  await server.start(opts)
+
+  // add singnal handlers in selva itself to close redis
+  console.log('ADDING SIGNAL HANDLERS')
+  addSignalHandlers(server)
+
+  return server
+}
