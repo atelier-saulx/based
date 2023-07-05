b00ecbf56ec95f60c021e21606e90f11bdcbb993
Tony Kovanen <tony.kovanen@gmail.com>
2023-07-05 14:00:18 +0200

    add query-ast-parser

--- packages/query/src/index.ts
+++ packages/query/src/index.ts
@@ -1,1 +1,40 @@
-console.info('make me query time')
+import printAst from './printAst'
+import isFork from './isFork'
+import ast2rpn from './ast2rpn'
+import bfsExpr2rpn from './bfsExpr2rpn'
+
+import createAst from './parseFilters'
+
+import optimizeTypeFilters from './optimizeTypeFilters'
+
+import convertNow from './convertNow'
+
+import { Filter } from './types'
+
+import { Rpn } from './types'
+
+const createRpn = (
+  types: Record<string, { prefix?: string }>,
+  filters: Filter | Filter[]
+): Rpn | void => {
+  if (!Array.isArray(filters)) {
+    filters = [filters]
+  }
+  const fork = createAst(filters)
+  if (fork) {
+    return ast2rpn(types, fork)
+  }
+}
+
+export * from './types'
+
+export {
+  printAst,
+  isFork,
+  createAst,
+  ast2rpn,
+  bfsExpr2rpn,
+  createRpn,
+  convertNow,
+  optimizeTypeFilters,
+}
