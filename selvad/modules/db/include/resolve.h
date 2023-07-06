6c5eb78ff1559ccfbdae37da3255b4767c699f30
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-07-06 15:48:01 +0200

    Some access attributes

--- selvad/modules/db/include/resolve.h
+++ selvad/modules/db/include/resolve.h
@@ -25,6 +25,6 @@ int SelvaResolve_NodeId(
         struct SelvaHierarchy *hierarchy,
         struct selva_string **ids,
         size_t nr_ids,
-        Selva_NodeId node_id);
+        Selva_NodeId node_id) __attribute__((access(read_only, 2), access(write_only, 4)));
 
 #endif /* SELVA_RESOLVE */
