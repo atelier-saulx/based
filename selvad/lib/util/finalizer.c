08f85d2b2c3ebe421e9b4f273b6e1fc317f68b9d
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-07-07 13:02:04 +0200

    Use statically allocated finalizer items
    Allocating small structs for finalizers is quite slow. Generally we want
    to avoid malloc for all short lived buffers so this is a good candidate
    to do static alloc.
    
    Ref #18

--- selvad/lib/util/finalizer.c
+++ selvad/lib/util/finalizer.c
@@ -1,20 +1,52 @@
 /*
- * Copyright (c) 2022 SAULX
+ * Copyright (c) 2022-2023 SAULX
  * SPDX-License-Identifier: MIT
  */
 #include <stddef.h>
+#include "cdefs.h"
 #include "queue.h"
 #include "jemalloc.h"
 #include "util/finalizer.h"
+
+struct finalizer_item pool[1000];
+struct finalizer_stack free_list;
+
+__constructor static void init_finalizer_free_list(void)
+{
+    SLIST_INIT(&free_list);
+
+    for (size_t i = 0; i < num_elem(pool); i++) {
+        SLIST_INSERT_HEAD(&free_list, &pool[i], entries);
+    }
+}
 
 void finalizer_init(struct finalizer *fin)
 {
     SLIST_INIT(&fin->head);
 }
 
+static struct finalizer_item *new_item(void)
+{
+    struct finalizer_item *item;
+
+    item = SLIST_FIRST(&free_list);
+    if (item) {
+        SLIST_REMOVE_HEAD(&free_list, entries);
+    } else {
+        item = selva_malloc(sizeof(struct finalizer_item));
+    }
+
+    return item;
+}
+
+static void free_item(struct finalizer_item *item)
+{
+    SLIST_INSERT_HEAD(&free_list, item, entries);
+}
+
 void finalizer_add(struct finalizer *fin, void *p, void (*dispose)(void *p))
 {
-    struct finalizer_item *item = selva_malloc(sizeof(struct finalizer_item));
+    struct finalizer_item *item = new_item();
 
     item->dispose = dispose;
     item->p = p;
@@ -30,7 +62,7 @@ void finalizer_del(struct finalizer *fin, void *p) {
     SLIST_FOREACH_SAFE(item, head, entries, item_tmp) {
         if (item->p == p) {
             SLIST_REMOVE(head, item, finalizer_item, entries);
-            selva_free(item);
+            free_item(item);
             break;
         }
     }
@@ -46,7 +78,7 @@ void finalizer_run(struct finalizer *fin)
         item->dispose(item->p);
 
         SLIST_REMOVE_HEAD(head, entries);
-        selva_free(item);
+        free_item(item);
     }
 }
 
