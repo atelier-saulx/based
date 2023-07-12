0f59678712077b3da669318f57f0474459e57627
Tony Kovanen <tony.kovanen@gmail.com>
2023-07-12 15:38:37 +0200

    fix sets

--- packages/client/src/protocol/encode/modify/set.ts
+++ packages/client/src/protocol/encode/modify/set.ts
@@ -1,10 +1,21 @@
 import { createRecord } from 'data-record'
+import { SELVA_NODE_ID_LEN } from '../../types'
 import { ModifyOpSetType, SET_OP_BY_TYPE } from './types'
 import { SET_TYPE_TO_MODIFY_VALUE_TYPE } from './types'
+
+function refsToStr(ary: string[] = []): string {
+  return ary.map((s: string) => `${s.padEnd(SELVA_NODE_ID_LEN)}`).join('')
+}
+
+function strsToStr(ary: string[] = []): string {
+  return ary.map((s: string) => `${s}\0`).join('')
+}
 
 export function encodeSetOperation({
   setType,
   $value,
+  $add,
+  $delete,
 }: {
   setType: number
   $value?: any | any[]
@@ -12,16 +23,21 @@ export function encodeSetOperation({
   $delete?: any | any[]
 }): Buffer {
   if (setType === ModifyOpSetType.SELVA_MODIFY_OP_SET_TYPE_REFERENCE) {
+    console.log('hmm', setType, $value)
     return createRecord(SET_OP_BY_TYPE[setType], {
-      op_set_type: setType,
-      contraint_id: 0, // TODO: bidirectional constraint
-      delete_all: false, // TODO: add support
-      $value: $value.map((s: string) => `${s}\0`).join(''),
+      op_set_type: ModifyOpSetType.SELVA_MODIFY_OP_SET_TYPE_REFERENCE,
+      contraint_id: 0, // TODO: impl. bidirectional
+      delete_all: false, // TODO
+      $value: refsToStr($value),
+      $add: refsToStr($add),
+      $delete: refsToStr($delete),
     })
   } else if (setType === ModifyOpSetType.SELVA_MODIFY_OP_SET_TYPE_CHAR) {
     return createRecord(SET_OP_BY_TYPE[setType], {
       op_set_type: setType,
-      $value: $value.map((s: string) => `${s}\0`).join(''),
+      $value: strsToStr($value),
+      $add: strsToStr($add),
+      $delete: strsToStr($delete),
     })
   }
 
