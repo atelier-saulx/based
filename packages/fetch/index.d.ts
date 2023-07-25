7f22703c130d672d81d8260d54eb4194d5c814e0
Tony Kovanen <tony.kovanen@gmail.com>
2023-07-25 11:12:10 +0200

    fix

---
+++ packages/fetch/index.d.ts
@@ -0,0 +1,5 @@
+import fetch from 'cross-fetch'
+
+declare module 'test' {
+	export default typeof fetch
+}
---
+++ packages/fetch/index.js
@@ -0,0 +1,7 @@
+if (!!global.WebSocketPair) {
+	module.exports = fetch
+	module.exports.default = fetch
+} else {
+	module.exports = require('cross-fetch')
+	module.exports.default = require('cross-fetch')
+}
