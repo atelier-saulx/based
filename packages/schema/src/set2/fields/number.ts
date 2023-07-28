10136176cdea21edfb840bbd5901a39085deac42
kylemarch513 <kyle.march513@gmail.com>
2023-07-28 10:46:46 +0200

    numbner

---
+++ packages/schema/src/set2/fields/number.ts
@@ -0,0 +1,64 @@
+import { ParseError } from '../../set/error'
+import { FieldParser } from '../../walker'
+
+const fieldSchemaUtil = (args) => {
+  const { fieldSchema, value } = args
+  if (
+    fieldSchema.multipleOf &&
+    value / fieldSchema.multipleOf -
+      Math.floor(value / fieldSchema.multipleOf) !==
+      0
+  ) {
+    args.error(args, ParseError.incorrectFormat)
+    return false
+  }
+  if (fieldSchema.maximum) {
+    if (fieldSchema.exclusiveMaximum) {
+      if (value >= fieldSchema.maximum) {
+        args.error(args, ParseError.exceedsMaximum)
+        return false
+      }
+    } else if (value > fieldSchema.maximum) {
+      args.error(args, ParseError.exceedsMaximum)
+      return false
+    }
+  }
+  if (fieldSchema.minimum) {
+    if (fieldSchema.exclusiveMinimum) {
+      if (value <= fieldSchema.minimum) {
+        args.error(args, ParseError.subceedsMinimum)
+        return false
+      }
+    } else if (value < fieldSchema.minimum) {
+      args.error(args, ParseError.subceedsMinimum)
+      return false
+    }
+  }
+  return true
+}
+
+export const number: FieldParser<'number'> = async (args) => {
+  if (typeof args.value !== 'number') {
+    args.error(args, ParseError.incorrectFieldType)
+    return
+  }
+  if (!fieldSchemaUtil(args)) {
+    return
+  }
+
+  return args
+}
+export const integer: FieldParser<'integer'> = async (args) => {
+  if (typeof args.value !== 'number') {
+    args.error(args, ParseError.incorrectFieldType)
+    return
+  }
+  if (args.value - Math.floor(args.value) !== 0) {
+    args.error(args, ParseError.incorrectFieldType)
+    return
+  }
+  if (!fieldSchemaUtil(args)) {
+    return
+  }
+  return args
+}
