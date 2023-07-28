b255ccec1c01a63895722593735a0a8f5a165a2e
Jim de Beer <jim@saulx.com>
2023-07-28 11:57:37 +0200

    array clean up

--- packages/schema/src/set2/fields/array.ts
+++ packages/schema/src/set2/fields/array.ts
@@ -5,9 +5,9 @@ import { BasedSetTarget } from '../../types'
 
 const parseArray = async (
   args: Args<BasedSetTarget, 'array'>,
-  val: any
+  value: any
 ): Promise<any[]> => {
-  const insert = Array.isArray(val) ? val : [val]
+  const insert = Array.isArray(value) ? value : [value]
   const q: Promise<any>[] = []
   const arr = new Array(insert.length)
   for (let i = 0; i < insert.length; i++) {
@@ -28,17 +28,58 @@ const parseArray = async (
   return arr
 }
 
+const operations: {
+  [key: string]: (
+    args: Args<BasedSetTarget, 'array'>,
+    value: any
+  ) => Promise<void>
+} = {
+  $insert: async (args, value) => {
+    if (
+      typeof value.$insert !== 'object' ||
+      typeof value.$remove.$idx !== 'number'
+    ) {
+      args.error(args, ParseError.incorrectFormat)
+      return
+    }
+    value.$insert.$value = parseArray(args, value.$insert.$value)
+    args.collect(args, value)
+  },
+  $push: async (args, value) => {
+    value.$push = parseArray(args, value.$push)
+    args.collect(args, value)
+  },
+  $unshift: async (args, value) => {
+    value.$unshift = parseArray(args, value.$push)
+    args.collect(args, value)
+  },
+  $remove: async (args, value) => {
+    if (typeof value.$remove.$idx !== 'number') {
+      args.error(args, ParseError.incorrectFormat)
+      return
+    }
+    args.collect(args, value)
+  },
+  $assign: async (args, value) => {
+    if (
+      typeof value.$assign !== 'object' ||
+      typeof value.$assign.$idx !== 'number'
+    ) {
+      args.error(args, ParseError.incorrectFormat)
+      return
+    }
+    await args.parse(args, value.$assign.$idx, args.fieldSchema.values)
+  },
+}
+
 export const array: FieldParser<'array'> = async (args) => {
   args.stop()
   const { error, collect, parse, fieldSchema } = args
-
   if (typeof args.value !== 'object') {
     error(args, ParseError.incorrectFormat)
     return
   }
-
   let value = '$value' in args.value ? args.value.$value : args.value
-
   if (Array.isArray(value)) {
     const q: Promise<any>[] = []
     collect(args, { $delete: true })
@@ -48,66 +89,17 @@ export const array: FieldParser<'array'> = async (args) => {
     await Promise.all(q)
     return
   }
-
   let hasOperation = false
-
-  if ('$insert' in value) {
-    if (
-      typeof value.$insert !== 'object' ||
-      typeof value.$remove.$idx !== 'number'
-    ) {
-      error(args, ParseError.incorrectFormat)
-      return
+  for (const key in value) {
+    if (operations[key]) {
+      if (hasOperation) {
+        error(args, ParseError.multipleOperationsNotAllowed)
+        return
+      }
+      await operations[key](args, value)
+      hasOperation = true
+    } else {
+      error(args, ParseError.fieldDoesNotExist)
     }
-    value.$insert.$value = parseArray(args, value.$insert.$value)
-    hasOperation = true
   }
-
-  if ('$remove' in value) {
-    if (hasOperation) {
-      error(args, ParseError.multipleOperationsNotAllowed)
-      return
-    }
-    if (typeof value.$remove.$idx !== 'number') {
-      error(args, ParseError.incorrectFormat)
-      return
-    }
-    hasOperation = true
-  }
-
-  if ('$push' in value) {
-    if (hasOperation) {
-      error(args, ParseError.multipleOperationsNotAllowed)
-      return
-    }
-    value.$push = parseArray(args, value.$push)
-    hasOperation = true
-  }
-
-  if ('$unshift' in value) {
-    if (hasOperation) {
-      error(args, ParseError.multipleOperationsNotAllowed)
-      return
-    }
-    value.$unshift = parseArray(args, value.$unshift)
-    hasOperation = true
-  }
-
-  if ('unshift' in value) {
-    if (hasOperation) {
-      error(args, ParseError.multipleOperationsNotAllowed)
-      return
-    }
-    if (
-      typeof value.$assign !== 'object' ||
-      typeof value.$assign.$idx !== 'number'
-    ) {
-      error(args, ParseError.incorrectFormat)
-      return
-    }
-    await parse(args, value.$assign.$idx, fieldSchema.values)
-    return
-  }
-
-  collect(args, value)
 }
