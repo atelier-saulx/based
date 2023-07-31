75971d9c32171ba0d057f302436e4e5ea041c78a
Jim de Beer <jim@saulx.com>
2023-07-31 10:29:12 +0200

    fix

--- packages/schema/test/walker.ts
+++ packages/schema/test/walker.ts
@@ -220,7 +220,7 @@ test('set walker', async (t) => {
 
 test.only('perf', async (t) => {
   let d = Date.now()
-  for (let i = 0; i < 1e6; i++) {
+  for (let i = 0; i < 1e5; i++) {
     await setWalker2(schema, { $id: 'bl120', flap: true, x: { flap: true } })
   }
   console.info(Date.now() - d, 'ms')
