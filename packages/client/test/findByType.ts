528d19db6f09652ba4017788d3ec324aa6a2b8f4
Nuno Frade <nfrade.tribal@gmail.com>
2023-08-01 12:53:42 +0200

    Adds findByType test

---
+++ packages/client/test/findByType.ts
@@ -0,0 +1,300 @@
+import test from 'ava'
+import { BasedDbClient } from '../src'
+import { startOrigin } from '../../server/dist'
+import { SelvaServer } from '../../server/dist/server'
+import './assertions'
+
+let srv: SelvaServer
+let client: BasedDbClient
+test.beforeEach(async (t) => {
+  console.log('origin')
+  srv = await startOrigin({
+    port: 8081,
+    name: 'default',
+  })
+
+  console.log('connecting')
+  client = new BasedDbClient()
+  client.connect({
+    port: 8081,
+    host: '127.0.0.1',
+  })
+
+  console.log('updating schema')
+
+  await client.updateSchema({
+    languages: ['en'],
+    types: {
+      league: {
+        prefix: 'le',
+        fields: {
+          name: { type: 'string' },
+          thing: { type: 'string' },
+          matches: { type: 'references' },
+        },
+      },
+      match: {
+        prefix: 'ma',
+        fields: {
+          name: { type: 'string' },
+          description: { type: 'text' },
+          value: {
+            type: 'number',
+          },
+          status: { type: 'number' },
+        },
+      },
+    },
+  })
+})
+
+test.after(async (t) => {
+  await srv.destroy()
+  client.destroy()
+})
+
+// TODO: $traverse expressions broken
+//     message: 'Cannot convert undefined to a BigInt'
+test.serial.skip('find - by type', async (t) => {
+  // simple nested - single query
+  await client.set({
+    $id: 'le1',
+    type: 'league',
+    name: 'league 1',
+  })
+
+  await client.set({
+    $id: 'ma1',
+    parents: ['le1'],
+    type: 'match',
+    name: 'match 1',
+    value: 1,
+  })
+
+  await client.set({
+    $id: 'ma2',
+    parents: ['ma1'],
+    type: 'match',
+    name: 'match 2',
+    value: 2,
+  })
+
+  await client.set({
+    $id: 'ma3',
+    parents: ['ma1'],
+    type: 'match',
+    name: 'match 3',
+  })
+
+  await client.set({
+    $id: 'ma4',
+    parents: ['le1'],
+    type: 'match',
+    name: 'match 4',
+    value: 12312,
+  })
+
+  await client.set({
+    $id: 'le1',
+    matches: ['ma1', 'ma2', 'ma3'],
+  })
+
+  t.deepEqual(
+    await client.get({
+      $id: 'root',
+      id: true,
+      items: {
+        name: true,
+        nonsense: { $default: 'yes' },
+        $list: {
+          $find: {
+            $recursive: true,
+            $traverse: {
+              root: 'children',
+              league: 'children',
+              $any: false,
+            },
+            $filter: [
+              {
+                $field: 'type',
+                $operator: '=',
+                $value: 'match',
+              },
+              {
+                $field: 'value',
+                $operator: 'exists',
+              },
+            ],
+          },
+        },
+      },
+    }),
+    {
+      id: 'root',
+      items: [
+        { name: 'match 1', nonsense: 'yes' },
+        { name: 'match 4', nonsense: 'yes' },
+        // { name: 'match 2', nonsense: 'yes' },
+      ],
+    }
+  )
+
+  t.deepEqual(
+    await client.get({
+      $id: 'root',
+      id: true,
+      items: {
+        name: true,
+        nonsense: { $default: 'yes' },
+        $list: {
+          $find: {
+            $recursive: true,
+            $traverse: {
+              root: 'children',
+              league: { $first: ['matches', 'children'] },
+              $any: false,
+            },
+            $filter: [
+              {
+                $field: 'type',
+                $operator: '=',
+                $value: 'match',
+              },
+              {
+                $field: 'value',
+                $operator: 'exists',
+              },
+            ],
+          },
+        },
+      },
+    }),
+    {
+      id: 'root',
+      items: [
+        { name: 'match 1', nonsense: 'yes' },
+        { name: 'match 2', nonsense: 'yes' },
+      ],
+    }
+  )
+
+  t.deepEqual(
+    await client.get({
+      $id: 'root',
+      id: true,
+      items: {
+        name: true,
+        nonsense: { $default: 'yes' },
+        $list: {
+          $find: {
+            $recursive: true,
+            $traverse: {
+              root: 'children',
+              league: { $all: ['matches', 'children'] },
+              $any: false,
+            },
+            $filter: [
+              {
+                $field: 'type',
+                $operator: '=',
+                $value: 'match',
+              },
+              {
+                $field: 'value',
+                $operator: 'exists',
+              },
+            ],
+          },
+        },
+      },
+    }),
+    {
+      id: 'root',
+      items: [
+        { name: 'match 1', nonsense: 'yes' },
+        { name: 'match 2', nonsense: 'yes' },
+        { name: 'match 4', nonsense: 'yes' },
+      ],
+    }
+  )
+})
+
+// TODO: $operator = '!=' not working
+test.serial.skip('find - by IS NOT type', async (t) => {
+  await client.set({
+    $id: 'le1',
+    type: 'league',
+    name: 'league 1',
+  })
+
+  await client.set({
+    $id: 'ma1',
+    parents: ['le1'],
+    type: 'match',
+    name: 'match 1',
+    value: 1,
+  })
+
+  const res = await client.get({
+    matches: {
+      id: true,
+      $list: {
+        $find: {
+          $traverse: 'descendants',
+          $filter: {
+            $field: 'type',
+            $operator: '!=',
+            $value: 'league',
+          },
+        },
+      },
+    },
+  })
+  t.log('0-0000', res)
+  t.is(res.matches.length, 1)
+
+  const resWithLanguage = await client.get({
+    $language: 'en',
+    matches: {
+      id: true,
+      $list: {
+        $find: {
+          $traverse: 'descendants',
+          $filter: {
+            $field: 'type',
+            $operator: '!=',
+            $value: 'league',
+          },
+        },
+      },
+    },
+  })
+  t.is(resWithLanguage.matches.length, 1)
+
+  await client.set({
+    $id: 'ma2',
+    parents: ['le1'],
+    type: 'match',
+    name: 'match 2',
+    description: { en: 'some' },
+    value: 1,
+  })
+
+  const resWithLanguage1 = await client.get({
+    $language: 'en',
+    matches: {
+      id: true,
+      $list: {
+        $find: {
+          $traverse: 'descendants',
+          $filter: {
+            $field: 'description',
+            $operator: '!=',
+            $value: 'some',
+          },
+        },
+      },
+    },
+  })
+  t.is(resWithLanguage1.matches.length, 2)
+})
