a47254e70b2db6aacbf6042301f43c1524dba0b9
Nuno Frade <nfrade.tribal@gmail.com>
2023-08-03 10:41:32 +0200

    Adds test languageInNested

---
+++ packages/client/test/languageInNested.ts
@@ -0,0 +1,68 @@
+import test from 'ava'
+import { BasedDbClient } from '../src'
+import { startOrigin } from '../../server/dist'
+import { SelvaServer } from '../../server/dist/server'
+import { wait } from '@saulx/utils'
+import './assertions'
+
+let srv: SelvaServer
+let client: BasedDbClient
+const port = 8081
+test.beforeEach(async (t) => {
+  console.log('origin')
+  srv = await startOrigin({
+    port,
+    name: 'default',
+  })
+
+  console.log('connecting')
+  client = new BasedDbClient()
+  client.connect({
+    port,
+    host: '127.0.0.1',
+  })
+
+  console.log('updating schema')
+
+  await client.updateSchema({
+    languages: ['en'],
+    types: {
+      dictionary: {
+        prefix: 'di',
+        fields: {
+          words: {
+            type: 'object',
+            properties: {
+              rando: { type: 'text' },
+            },
+          },
+        },
+      },
+    },
+  })
+})
+
+test.afterEach(async (_t) => {
+  await srv.destroy()
+  client.destroy()
+  await wait(300)
+})
+
+test.serial('$language should be applied in nested text', async (t) => {
+  const dictionary = await client.set({
+    $language: 'en',
+    type: 'dictionary',
+    words: {
+      rando: 'my word',
+    },
+  })
+
+  t.deepEqual(
+    await client.get({
+      $id: dictionary,
+      $language: 'en',
+      words: true,
+    }),
+    { words: { rando: 'my word' } }
+  )
+})
