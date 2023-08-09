32a9ceb692133a54fd20a8b5f0b976df3c5796b1
Tony Kovanen <tony.kovanen@gmail.com>
2023-08-09 16:12:50 +0200

    put

--- packages/client/test/collision.ts
+++ packages/client/test/collision.ts
@@ -44,21 +44,7 @@ test.afterEach(async (_t) => {
   client.destroy()
 })
 
-// TODO: Setting too much nodes at the same time?
-// Error finding frame RangeError [ERR_OUT_OF_RANGE]: The value of "offset" is out of range. It must be >= 0 and <= 0. Received 1
-//     at new NodeError (node:internal/errors:399:5)
-//     at boundsError (node:internal/buffer:88:9)
-//     at Buffer.readUInt8 (node:internal/buffer:254:5)
-//     at h (/Users/nfrade/work/saulx/based-db/node_modules/data-record/src/accessors.ts:204:38)
-//     at deserialize (/Users/nfrade/work/saulx/based-db/node_modules/data-record/src/serializer.ts:141:17)
-//     at findFrame (/Users/nfrade/work/saulx/based-db/packages/client/src/protocol/decode/index.ts:20:50)
-//     at incoming (/Users/nfrade/work/saulx/based-db/packages/client/src/incoming.ts:28:22)
-//     at BasedDbClient.onData (/Users/nfrade/work/saulx/based-db/packages/client/src/index.ts:278:13)
-//     at Socket.<anonymous> (/Users/nfrade/work/saulx/based-db/packages/client/src/socket/index.ts:42:14)
-//     at Socket.emit (node:events:513:28) {
-//   code: 'ERR_OUT_OF_RANGE'
-// }
-test.serial.skip('collision', async (t) => {
+test.serial('collision', async (t) => {
   let n = 100
   const allIds: any = []
   while (n--) {
