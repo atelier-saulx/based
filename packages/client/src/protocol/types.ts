72eb3ba4a6bd56216a11215404426863a469cbf0
Tony Kovanen <tony.kovanen@gmail.com>
2023-08-09 10:33:54 +0200

    put

--- packages/client/src/protocol/types.ts
+++ packages/client/src/protocol/types.ts
@@ -218,7 +218,7 @@ export enum SelvaResultOrder {
    * Result is not ordered by any field but can be usually expected to have a
    * deterministic order.
    */
-  SELVA_RESULT_ORDER_NONE,
+  SELVA_RESULT_ORDER_NONE = 0,
   /**
    * Ascending order.
    */
