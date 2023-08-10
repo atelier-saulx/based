949edea9aea32f12612138d3ce8ca232b90edd72
Nuno Frade <nfrade.tribal@gmail.com>
2023-08-10 14:47:45 +0200

    Adds test edgeField

--- packages/client/test/edgeField.ts
+++ packages/client/test/edgeField.ts
@@ -33,6 +33,7 @@ test.beforeEach(async (_t) => {
     root: {
       fields: {
         value: { type: 'number' },
+        name: { type: 'string' },
         nested: {
           type: 'object',
           properties: {
@@ -45,12 +46,18 @@ test.beforeEach(async (_t) => {
             b: { type: 'references' },
           },
         },
+        c: { type: 'references' },
+        d: { type: 'references' },
         o: {
           type: 'object',
           properties: {
             a: { type: 'string' },
           },
         },
+        teams: { type: 'references' },
+        best: { type: 'references' },
+        thing: { type: 'reference' },
+        things: { type: 'references' },
       },
     },
     types: {
@@ -75,6 +82,77 @@ test.beforeEach(async (_t) => {
           refs: {
             type: 'references',
           },
+          ding: { type: 'string' },
+          dong: { type: 'string' },
+          thing: {
+            type: 'object',
+            properties: {
+              ding: { type: 'string' },
+            },
+          },
+        },
+      },
+      game: {
+        prefix: 'ga',
+        fields: {
+          title: { type: 'string' },
+          homeTeam: { type: 'references' },
+        },
+      },
+      player: {
+        prefix: 'pl',
+        fields: {
+          name: { type: 'string' },
+          o: {
+            type: 'object',
+            properties: {
+              a: { type: 'string' },
+            },
+          },
+          team: {
+            type: 'reference',
+            bidirectional: {
+              fromField: 'players',
+            },
+          },
+        },
+      },
+      team: {
+        prefix: 'te',
+        fields: {
+          name: { type: 'string' },
+          club: { type: 'references' },
+          o: {
+            type: 'object',
+            properties: {
+              a: { type: 'string' },
+            },
+          },
+          players: {
+            type: 'references',
+            bidirectional: {
+              fromField: 'team',
+            },
+          },
+        },
+      },
+      club: {
+        prefix: 'cl',
+        fields: {
+          name: { type: 'string' },
+          manager: { type: 'references' },
+        },
+      },
+      manager: {
+        prefix: 'mn',
+        fields: {
+          name: { type: 'string' },
+        },
+      },
+      somethingDa: {
+        prefix: 'da',
+        fields: {
+          name: { type: 'string' },
         },
       },
     },
@@ -792,4 +870,726 @@ test.serial('edge modify `delete_all`', async (t) => {
   t.deepEqual((await client.command('hierarchy.edgeGet', ['root', 'a.b']))[0], [
     0n,
   ])
+})
+
+test.serial('traverse by expression', async (t) => {
+  // Create nodes
+  await client.set({
+    $id: 'root',
+    o: {
+      a: 'hello',
+    },
+  })
+
+  await client.set({
+    $id: 'root',
+    c: { $add: ['ma1'] },
+  })
+  await client.set({
+    $id: 'root',
+    d: { $add: ['ma2'] },
+  })
+
+  t.deepEqualIgnoreOrder(
+    (
+      await find({
+        client,
+        dir: SelvaTraversal.SELVA_HIERARCHY_TRAVERSAL_BFS_EXPRESSION,
+        dir_opt_str: '{"c","d"}',
+        id: 'root',
+      })
+    )[0],
+    ['ma1', 'ma2']
+  )
+})
+
+test.serial('deref node references on find', async (t) => {
+  // Create nodes
+  await client.set({
+    $id: 'game1',
+    title: 'Best Game',
+  })
+  await client.set({
+    $id: 'team1',
+    name: 'Funny Team',
+  })
+  await client.set({
+    $id: 'club1',
+    name: 'Funny Club',
+  })
+  await client.set({
+    $id: 'mnager1',
+    name: 'dung',
+  })
+  await client.set({
+    $id: 'game1',
+    homeTeam: { $add: ['team1'] },
+  })
+  await client.set({
+    $id: 'team1',
+    club: { $add: ['club1'] },
+  })
+  await client.set({
+    $id: 'club1',
+    manager: { $add: ['mnager1'] },
+  })
+
+  t.deepEqual(
+    (
+      await find({
+        client,
+        res_type: protocol.SelvaFindResultType.SELVA_FIND_QUERY_RES_FIELDS,
+        res_opt_str:
+          'title\nhomeTeam.name\nhomeTeam.club.name\nhomeTeam.club.manager.name',
+        dir: SelvaTraversal.SELVA_HIERARCHY_TRAVERSAL_NODE,
+        id: 'game1',
+      })
+    )[0],
+    [
+      [
+        'game1',
+        [
+          'title',
+          'Best Game',
+          'homeTeam',
+          [['id', 'team1', 'name', 'Funny Team']],
+          'homeTeam',
+          [['id', 'team1', 'club', [['id', 'club1', 'name', 'Funny Club']]]],
+          'homeTeam',
+          [
+            [
+              'id',
+              'team1',
+              'club',
+              [['id', 'club1', 'manager', [['id', 'mnager1', 'name', 'dung']]]],
+            ],
+          ],
+        ],
+      ],
+    ]
+  )
+})
+
+test.serial('bidirectional edge fields', async (t) => {
+  // Create dynamic constraints
+  await client.command('hierarchy.addConstraint', [
+    'te',
+    'B',
+    'players',
+    'team',
+  ])
+  await client.command('hierarchy.addConstraint', [
+    'pl',
+    'SB',
+    'team',
+    'players',
+  ])
+
+  // Create nodes
+  await client.set({
+    $id: 'root',
+    o: {
+      a: 'hello',
+    },
+  })
+  await client.set({
+    $id: 'te1',
+    o: {
+      a: 'hello',
+    },
+  })
+  await client.set({
+    $id: 'pl1',
+    o: {
+      a: 'tim',
+    },
+  })
+  await client.set({
+    $id: 'pl2',
+    o: {
+      a: 'bob',
+    },
+  })
+  await client.set({
+    $id: 'pl3',
+    o: {
+      a: 'jack',
+    },
+  })
+
+  // Create edges
+  await client.set({
+    $id: 'root',
+    teams: { $add: ['te1'] },
+  })
+  await client.set({
+    $id: 'te1',
+    players: ['pl1', 'pl2', 'pl3'],
+  })
+
+  t.deepEqual((await client.command('hierarchy.edgeList', ['te1']))[0], [
+    'players',
+    ['pl1', 'pl2', 'pl3'],
+  ])
+  t.log(
+    await client.get({
+      $id: 'pl1',
+      team: true,
+    })
+  )
+  t.deepEqual((await client.command('hierarchy.edgeList', ['pl1']))[0], [
+    'team',
+    ['te1'],
+  ])
+
+  // Delete an edge
+  await client.set({
+    $id: 'pl3',
+    team: { $delete: true },
+  })
+  t.deepEqual((await client.command('hierarchy.edgeList', ['te1']))[0], [
+    'players',
+    ['pl1', 'pl2'],
+  ])
+  t.deepEqual((await client.command('hierarchy.edgeGet', ['pl3', 'team']))[0], [
+    2n,
+  ])
+
+  // Delete an edge
+  await client.set({
+    $id: 'pl2',
+    team: { $delete: true },
+  })
+  t.deepEqual((await client.command('hierarchy.edgeList', ['te1']))[0], [
+    'players',
+    ['pl1'],
+  ])
+  t.deepEqual((await client.command('hierarchy.edgeGet', ['pl2', 'team']))[0], [
+    2n,
+  ])
+})
+
+// TODO: Tony, what should this be testing?
+test.serial.skip('biedge missing symmetric constraint', async (t) => {
+  // Create dynamic constraints
+  // await client.redis.selva_hierarchy_addconstraint('___selva_hierarchy',
+  //     'te',
+  //     'B',
+  //     'players',
+  //     'team',
+  // )
+  await client.command('hierarchy.addConstraint', [
+    'te',
+    'B',
+    'players',
+    'team',
+  ])
+
+  // Create nodes
+  // await client.redis.selva_modify('root', '', '0', 'o.a', 'root')
+  await client.set({
+    $id: 'root',
+    o: {
+      a: 'root',
+    },
+  })
+  // await client.redis.selva_modify('te1', '', '0', 'o.a', 'dun')
+  await client.set({
+    $id: 'te1',
+    o: {
+      a: 'dun',
+    },
+  })
+  // await client.redis.selva_modify('pl1', '', '0', 'o.a', 'dan')
+  await client.set({
+    $id: 'pl1',
+    o: {
+      a: 'dan',
+    },
+  })
+
+  // const res = await client.redis.selva_modify('te1', '', '5', 'players', createRecord(setRecordDefCstring, {
+  //   op_set_type: 1,
+  //   delete_all: 0,
+  //   constraint_id: 2,
+  //   $add: joinIds(['pl1']),
+  //   $delete: null,
+  //   $value: null,
+  // }))
+  const res = await client.set({
+    $id: 'te1',
+    players: { $add: ['pl1'] },
+  })
+  t.log({ res })
+  // t.true(res[1] instanceof Error)
+})
+
+test.serial('edge type constraints', async (t) => {
+  // Create dynamic constraints
+  // await client.redis.selva_hierarchy_addconstraint('___selva_hierarchy',
+  //     'ro', // source node type
+  //     'S', // constraint flags
+  //     'best', // source field name
+  //     '', // bck field name
+  // )
+  await client.command('hierarchy.addConstraint', [
+    'ro', // source node type
+    'S', // constraint flags
+    'best', // source field name
+    '', // bck field name
+  ])
+  // await client.redis.selva_hierarchy_addconstraint('___selva_hierarchy',
+  //     'ro', // source node type
+  //     '', // constraint flags
+  //     'teams', // source field name
+  //     '', // bck field name
+  // )
+  await client.command('hierarchy.addConstraint', [
+    'ro', // source node type
+    '', // constraint flags
+    'teams', // source field name
+    '', // bck field name
+  ])
+  // await client.redis.selva_hierarchy_addconstraint('___selva_hierarchy',
+  //     'te',
+  //     'B',
+  //     'players',
+  //     'team',
+  // )
+  await client.command('hierarchy.addConstraint', [
+    'te',
+    'B',
+    'players',
+    'team',
+  ])
+  // await client.redis.selva_hierarchy_addconstraint('___selva_hierarchy',
+  //     'pl',
+  //     'B',
+  //     'team',
+  //     'players',
+  // )
+  await client.command('hierarchy.addConstraint', [
+    'pl',
+    'B',
+    'team',
+    'players',
+  ])
+
+  // // Create nodes
+  // await client.redis.selva_modify('root', '', '0', 'o.a', 'root')
+  await client.set({
+    $id: 'root',
+    o: {
+      a: 'root',
+    },
+  })
+  // await client.redis.selva_modify('te1', '', '0', 'o.a', 'dun')
+  await client.set({
+    $id: 'te1',
+    o: {
+      a: 'dun',
+    },
+  })
+  // await client.redis.selva_modify('pl1', '', '0', 'o.a', 'dan')
+  await client.set({
+    $id: 'pl1',
+    o: {
+      a: 'dan',
+    },
+  })
+  // await client.redis.selva_modify('pl2', '', '0', 'o.a', 'dandan')
+  await client.set({
+    $id: 'pl2',
+    o: {
+      a: 'dandan',
+    },
+  })
+  // await client.redis.selva_modify('in1', '', '0', 'o.a', 'dandan')
+  // await client.set({
+  //   $id: 'in1',
+  //   o: {
+  //     a: 'dandan',
+  //   },
+  // })
+
+  let res
+
+  // PASS
+  await t.notThrowsAsync(
+    client.set({
+      $id: 'root',
+      teams: { $add: ['te1'] },
+    })
+  )
+
+  // // PASS
+  // res = await client.redis.selva_modify('root', '', '5', 'best', createRecord(setRecordDefCstring, {
+  //   op_set_type: 1,
+  //   delete_all: 0,
+  //   constraint_id: 2,
+  //   $add: null,
+  //   $delete: null,
+  //   $value: joinIds(['te1']),
+  // }))
+  // t.deepEqual(res[1], 'UPDATED')
+  await t.notThrowsAsync(
+    client.set({
+      $id: 'root',
+      best: ['te1'],
+    })
+  )
+
+  // TODO: Tony, don't know what to test here?
+  // I can't make it fail
+
+  // FAIL
+  // res = await client.redis.selva_modify('te1', '', '5', 'players', createRecord(setRecordDefCstring, {
+  //   op_set_type: 1,
+  //   delete_all: 0,
+  //   constraint_id: 2,
+  //   $add: joinIds(['root']),
+  //   $delete: null,
+  //   $value: null,
+  // }))
+  // t.true(res[1] instanceof Error)
+
+  // PASS
+  // res = await client.redis.selva_modify('te1', '', '5', 'players', createRecord(setRecordDefCstring, {
+  //   op_set_type: 1,
+  //   delete_all: 0,
+  //   constraint_id: 2,
+  //   $add: joinIds(['pl1']),
+  //   $delete: null,
+  //   $value: null,
+  // }))
+  // t.deepEqual(res[1], 'UPDATED')
+  await t.notThrowsAsync(
+    client.set({
+      $id: 'te1',
+      players: { $add: ['pl1'] },
+    })
+  )
+
+  // TODO: Tony, don't know what to test here?
+
+  // FAIL
+  // res = await client.redis.selva_modify('pl2', '', '5', 'team', createRecord(setRecordDefCstring, {
+  //   op_set_type: 1,
+  //   delete_all: 0,
+  //   constraint_id: 2,
+  //   $add: joinIds(['root']),
+  //   $delete: null,
+  //   $value: null,
+  // }))
+  // t.true(res[1] instanceof Error)
+  // PASS
+  // res = await client.redis.selva_modify('pl2', '', '5', 'team', createRecord(setRecordDefCstring, {
+  //   op_set_type: 1,
+  //   delete_all: 0,
+  //   constraint_id: 2,
+  //   $add: joinIds(['te1']),
+  //   $delete: null,
+  //   $value: null,
+  // }))
+  // t.deepEqual(res[1], 'UPDATED')
+  await t.notThrowsAsync(
+    client.set({
+      $id: 'pl2',
+      team: 'te1',
+    })
+  )
+})
+
+test.serial('wildcard find with edge fields', async (t) => {
+  // Create nodes
+  await client.set({
+    $id: 'root',
+    name: 'hello',
+  })
+  await client.set({
+    $id: 'ma1',
+    ding: 'dong',
+    dong: 'ding',
+  })
+  await client.set({
+    $id: 'ma2',
+    ding: 'dong',
+    dong: 'ding',
+  })
+
+  await client.set({
+    $id: 'root',
+    thing: 'ma1',
+  })
+  await client.set({
+    $id: 'root',
+    things: { $add: ['ma1', 'ma2'] },
+  })
+  //
+  t.deepEqual(
+    (
+      await find({
+        client,
+        res_type: protocol.SelvaFindResultType.SELVA_FIND_QUERY_RES_FIELDS,
+        res_opt_str: 'thing.ding',
+        dir: SelvaTraversal.SELVA_HIERARCHY_TRAVERSAL_NODE,
+        id: 'root',
+      })
+    )[0],
+    [['root', ['thing', [['id', 'ma1', 'ding', 'dong']]]]]
+  )
+  t.deepEqual(
+    (
+      await find({
+        client,
+        res_type: protocol.SelvaFindResultType.SELVA_FIND_QUERY_RES_FIELDS,
+        res_opt_str: 'thing.*\n!thing.createdAt\n!thing.updatedAt',
+        dir: SelvaTraversal.SELVA_HIERARCHY_TRAVERSAL_NODE,
+        id: 'root',
+      })
+    )[0],
+    [
+      [
+        'root',
+        [
+          'thing',
+          [
+            [
+              'id',
+              'ma1',
+              'ding',
+              'dong',
+              'dong',
+              'ding',
+              'id',
+              'ma1',
+              'type',
+              'match',
+            ],
+          ],
+        ],
+      ],
+    ]
+  )
+  // Can't do this with multi-ref
+  t.deepEqual(
+    (
+      await find({
+        client,
+        res_type: protocol.SelvaFindResultType.SELVA_FIND_QUERY_RES_FIELDS,
+        res_opt_str: 'things.*\n!things.createdAt\n!things.updatedAt',
+        dir: SelvaTraversal.SELVA_HIERARCHY_TRAVERSAL_NODE,
+        id: 'root',
+      })
+    )[0],
+    [
+      [
+        'root',
+        [
+          'things',
+          [
+            [
+              'id',
+              'ma1',
+              'ding',
+              'dong',
+              'dong',
+              'ding',
+              'id',
+              'ma1',
+              'type',
+              'match',
+            ],
+            [
+              'id',
+              'ma2',
+              'ding',
+              'dong',
+              'dong',
+              'ding',
+              'id',
+              'ma2',
+              'type',
+              'match',
+            ],
+          ],
+        ],
+      ],
+    ]
+  )
+})
+
+test.serial.skip(
+  'wildcard find with edge fields and data fields',
+  async (t) => {
+    // Create nodes
+    // t.deepEqual(
+    //   await client.redis.selva_modify('root', '', '0', 'name', 'hello'),
+    //   ['root', 'UPDATED']
+    // )
+    await client.set({
+      $id: 'root',
+      name: 'hello',
+    })
+    // t.deepEqual(
+    //   await client.redis.selva_modify('ma1', '', '0', 'thing.ding', 'dong'),
+    //   ['ma1', 'UPDATED']
+    // )
+    await client.set({
+      $id: 'ma1',
+      thing: {
+        ding: 'dong',
+      },
+    })
+    // t.deepEqual(
+    //   await client.redis.selva_modify('da3', '', '0', 'name', 'dong'),
+    //   ['da3', 'UPDATED']
+    // )
+    await client.set({
+      $id: 'da3',
+      name: 'dong',
+    })
+
+    // const rec1 = createRecord(setRecordDefCstring, {
+    //   op_set_type: 1,
+    //   delete_all: 0,
+    //   constraint_id: 1,
+    //   $add: joinIds(['da3']),
+    //   $delete: null,
+    //   $value: null,
+    // })
+    //
+    // t.deepEqual(
+    //   await client.redis.selva_modify('ma2', '', '5', 'thing', rec1, '0', 'ding', 'dong'),
+    //   ['ma2', 'UPDATED', 'UPDATED']
+    // )
+    await client.set({
+      $id: 'ma2',
+      thing: 'da3',
+    })
+
+    // TODO: Tony need help migrating these
+    t.deepEqual(
+      // await client.redis.selva_hierarchy_find('', '___selva_hierarchy', 'node', 'fields', 'thing', 'ma1'),
+      (
+        await find({
+          client,
+          res_type: protocol.SelvaFindResultType.SELVA_FIND_QUERY_RES_FIELDS,
+          res_opt_str: 'thing',
+          dir: SelvaTraversal.SELVA_HIERARCHY_TRAVERSAL_NODE,
+          id: 'ma1',
+        })
+      )[0],
+      // await client.redis.selva_hierarchy_find('', '___selva_hierarchy', 'node', 'fields', 'thing.*', 'ma1'))
+      (
+        await find({
+          client,
+          res_type: protocol.SelvaFindResultType.SELVA_FIND_QUERY_RES_FIELDS,
+          res_opt_str: 'thing.*',
+          dir: SelvaTraversal.SELVA_HIERARCHY_TRAVERSAL_NODE,
+          id: 'ma1',
+        })
+      )[0]
+    )
+    // t.deepEqual(
+    //   await client.redis.selva_hierarchy_find('', '___selva_hierarchy', 'descendants', 'fields', 'thing.*\n!thing.createdAt\n!thing.updatedAt', 'root', '"ma" e'),
+    //   [
+    //     [
+    //       "ma1",
+    //       [
+    //         "thing",
+    //         [
+    //           "ding",
+    //           "dong"
+    //         ]
+    //       ]
+    //     ],
+    //     [
+    //       "ma2",
+    //       [
+    //         "thing",
+    //         [
+    //           [
+    //             "id",
+    //             "da3",
+    //             "id",
+    //             "da3",
+    //             "name",
+    //             "dong"
+    //           ]
+    //         ]
+    //       ]
+    //     ]
+    //   ]
+    // )
+  }
+)
+
+test.serial.skip('wildcard find with exclusions', async (t) => {
+  // Create nodes
+  // t.deepEqual(
+  //   await client.redis.selva_modify('root', '', '0', 'name', 'hello'),
+  //   ['root', 'UPDATED']
+  // )
+  await client.set({
+    $id: 'root',
+    name: 'hello',
+  })
+  // t.deepEqual(
+  //   await client.redis.selva_modify('ma1', '', '0', 'thing.ding', 'dong'),
+  //   ['ma1', 'UPDATED']
+  // )
+  await client.set({
+    $id: 'ma1',
+    thing: {
+      ding: 'dong',
+    },
+  })
+  // t.deepEqual(
+  //   await client.redis.selva_modify('da3', '', '0', 'name', 'dong'),
+  //   ['da3', 'UPDATED']
+  // )
+  await client.set({
+    $id: 'da3',
+    name: 'dong',
+  })
+
+  // const rec1 = createRecord(setRecordDefCstring, {
+  //   op_set_type: 1,
+  //   delete_all: 0,
+  //   constraint_id: 1,
+  //   $add: joinIds(['da3']),
+  //   $delete: null,
+  //   $value: null,
+  // })
+  //
+  // t.deepEqual(
+  //   await client.redis.selva_modify('ma2', '', '5', 'thing', rec1, '0', 'ding', 'dong'),
+  //   ['ma2', 'UPDATED', 'UPDATED']
+  // )
+  await client.set({
+    $id: 'ma2',
+    refs: 'da3',
+    ding: 'dong',
+  })
+
+  // TODO: TONY, also need help with this one
+  t.deepEqual(
+    // await client.redis.selva_hierarchy_find('', '___selva_hierarchy', 'descendants', 'fields', 'thing.*\n!id\n!thing.createdAt\n!thing.updatedAt', 'root', '"ma" e'),
+    (
+      await find({
+        client,
+        res_type: protocol.SelvaFindResultType.SELVA_FIND_QUERY_RES_FIELDS,
+        res_opt_str: 'thing.*\n!id\n!thing.createdAt\n!thing.updatedAt',
+        // dir: SelvaTraversal.SELVA_HIERARCHY_TRAVERSAL_BFS_DESCENDANTS,
+        dir: SelvaTraversal.SELVA_HIERARCHY_TRAVERSAL_BFS_EXPRESSION,
+        dir_opt_str: '"ma" e',
+        id: 'ma1',
+      })
+    )[0],
+    [
+      ['ma1', ['thing', ['ding', 'dong']]],
+      ['ma2', ['refs', [['id', 'da3', 'id', 'da3', 'name', 'dong']]]],
+    ]
+  )
 })
