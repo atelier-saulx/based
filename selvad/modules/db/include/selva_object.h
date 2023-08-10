atelier-saulx/based

commit 99be372ccb03a2099a3dc10aa300efa9cab760b4

so: Fix SELVA_OBJECT_GETKEY_DELETE
We used to track the containing object in `get_key`. Then the code
was optimized/simplified and now we don't always know the
obj<->key relationship. We must reintroduce the tracking to fix
deletes.

Example:

```
> object.get '' root a[0]
[
  "field",
  [
    "a",
    "hello",
  ],
],
> object.del root a[0].field
1,
```

selvad/modules/db/module/selva_object/selva_object.c
@@ -406,8 +406,10 @@ static struct SelvaObjectKey *alloc_key(struct SelvaObject *obj, const char *nam
 static void remove_key(struct SelvaObject *obj, struct SelvaObjectKey *key) {
     const intptr_t i = ((intptr_t)key - (intptr_t)obj->emb_keys) / EMBEDDED_KEY_SIZE;
 
-    /* Clear and free the key. */
-    RB_REMOVE(SelvaObjectKeys, &obj->keys_head, key);
+    /*
+     * Clear and free the key.
+     */
+    (void)RB_REMOVE(SelvaObjectKeys, &obj->keys_head, key);
     obj->obj_size--;
     clear_key_value(key);
 
@@ -552,6 +554,7 @@ static int get_key_obj(struct SelvaObject *obj, const char *key_name_str, size_t
     const size_t nr_parts = substring_count(key_name_str, sep, key_name_len) + 1;
     char buf[key_name_len + 1]; /* We assume that the length has been sanity checked at this point. */
     struct SelvaObjectKey *key = NULL;
+    struct SelvaObject *key_container = obj;
 
     memcpy(buf, key_name_str, key_name_len);
     buf[key_name_len] = '\0';
@@ -601,6 +604,7 @@ static int get_key_obj(struct SelvaObject *obj, const char *key_name_str, size_t
                 return err;
             }
 
+            key_container = obj;
             obj = key->value;
             assert(obj);
             is_timeseries = 0;
@@ -630,6 +634,7 @@ static int get_key_obj(struct SelvaObject *obj, const char *key_name_str, size_t
             }
 
             init_array_key(key, SELVA_OBJECT_OBJECT, ary_idx + 1);
+            key_container = NULL;
             obj = insert_new_obj_into_array_key(key, ary_idx);
         } else if ((err == SELVA_ENOENT || (err == 0 && key->type != SELVA_OBJECT_OBJECT && key->type != SELVA_OBJECT_ARRAY && nr_parts > nr_parts_found)) &&
                    (flags & SELVA_OBJECT_GETKEY_CREATE)) {
@@ -654,6 +659,7 @@ static int get_key_obj(struct SelvaObject *obj, const char *key_name_str, size_t
             key->type = SELVA_OBJECT_OBJECT;
             key->value = SelvaObject_New();
 
+            key_container = obj;
             obj = key->value;
         } else if (err) {
             /*
@@ -669,12 +675,14 @@ static int get_key_obj(struct SelvaObject *obj, const char *key_name_str, size_t
                 is_timeseries = 1;
             }
 
+            key_container = obj;
             obj = key->value;
             assert(obj);
         } else if (key->type == SELVA_OBJECT_ARRAY && key->subtype == SELVA_OBJECT_OBJECT && nr_parts > nr_parts_found && ary_idx >= 0) {
             /*
              * Keep nesting or return an object from the array if this was the last token.
              */
+            key_container = NULL;
             obj = SVector_GetIndex(key->array, ary_idx);
             if (!obj) {
                 if (flags & SELVA_OBJECT_GETKEY_CREATE) {
@@ -714,7 +722,10 @@ static int get_key_obj(struct SelvaObject *obj, const char *key_name_str, size_t
     }
 
     if (flags & SELVA_OBJECT_GETKEY_DELETE) {
-        remove_key(obj, key);
+        if (!key_container) {
+            return SELVA_ENOENT; /* weird */
+        }
+        remove_key(key_container, key);
         key = NULL;
     }
 

commit 75025b39785efca87719e59a7d79079810e6f54b

Always keep track of the SelvaObjectKey container
Unfortunately the commit 99be372ccb03a2099a3dc10aa300efa9cab760b4
broke more than it fixed. It's really hard to reliably track the
container object of the key and passing a wrong `obj` to the
`remove_key()` function may lead to catastrophic loss of data.

For now it will be much safer to always keep the pointer around in
the `SelvaObjectKey` struct. This will add 8 bytes to the
mentioned struct and 32 bytes to the `SelvaObjectKey` struct type
due to it containing 4 embedded keys. In that case it's
redundant to have that pointer but we only have one key type for
now.

selvad/modules/db/include/selva_object.h
@@ -22,7 +22,7 @@ enum SelvaObjectReplyFlags {
  * Size of struct SelvaObject.
  * This must match with the actual size of selva_object.c won't compile.
  */
-#define SELVA_OBJECT_BSIZE 304
+#define SELVA_OBJECT_BSIZE 336
 
 /**
  * Define a for holding a SelvaObject.

selvad/modules/db/module/selva_object/selva_object.c
@@ -59,6 +59,7 @@ struct SelvaObjectKey {
     enum SelvaObjectType subtype; /*!< Subtype of the value. Arrays use this. */
     SelvaObjectMeta_t user_meta; /*!< User defined metadata. */
     unsigned short name_len;
+    struct SelvaObject *container;
     RB_ENTRY(SelvaObjectKey) _entry;
     union {
         struct {
@@ -397,13 +398,21 @@ static struct SelvaObjectKey *alloc_key(struct SelvaObject *obj, const char *nam
     }
 
     memset(key, 0, key_size);
+    key->container = obj;
     memcpy(key->name, name_str, name_len);
     key->name_len = (typeof(key->name_len))name_len;
 
     return key;
 }
 
-static void remove_key(struct SelvaObject *obj, struct SelvaObjectKey *key) {
+static void remove_key(struct SelvaObjectKey *key) {
+    struct SelvaObject *obj = key->container;
+
+    if (!obj) {
+        /* Not an object key. */
+        return;
+    }
+
     const intptr_t i = ((intptr_t)key - (intptr_t)obj->emb_keys) / EMBEDDED_KEY_SIZE;
 
     /*
@@ -440,7 +449,7 @@ void SelvaObject_Clear(struct SelvaObject *obj, const char * const exclude[]) {
         }
 
         if (clear) {
-            remove_key(obj, key);
+            remove_key(key);
         }
     }
 }
@@ -554,7 +563,6 @@ static int get_key_obj(struct SelvaObject *obj, const char *key_name_str, size_t
     const size_t nr_parts = substring_count(key_name_str, sep, key_name_len) + 1;
     char buf[key_name_len + 1]; /* We assume that the length has been sanity checked at this point. */
     struct SelvaObjectKey *key = NULL;
-    struct SelvaObject *key_container = obj;
 
     memcpy(buf, key_name_str, key_name_len);
     buf[key_name_len] = '\0';
@@ -604,7 +612,6 @@ static int get_key_obj(struct SelvaObject *obj, const char *key_name_str, size_t
                 return err;
             }
 
-            key_container = obj;
             obj = key->value;
             assert(obj);
             is_timeseries = 0;
@@ -634,7 +641,6 @@ static int get_key_obj(struct SelvaObject *obj, const char *key_name_str, size_t
             }
 
             init_array_key(key, SELVA_OBJECT_OBJECT, ary_idx + 1);
-            key_container = NULL;
             obj = insert_new_obj_into_array_key(key, ary_idx);
         } else if ((err == SELVA_ENOENT || (err == 0 && key->type != SELVA_OBJECT_OBJECT && key->type != SELVA_OBJECT_ARRAY && nr_parts > nr_parts_found)) &&
                    (flags & SELVA_OBJECT_GETKEY_CREATE)) {
@@ -659,7 +665,6 @@ static int get_key_obj(struct SelvaObject *obj, const char *key_name_str, size_t
             key->type = SELVA_OBJECT_OBJECT;
             key->value = SelvaObject_New();
 
-            key_container = obj;
             obj = key->value;
         } else if (err) {
             /*
@@ -675,14 +680,12 @@ static int get_key_obj(struct SelvaObject *obj, const char *key_name_str, size_t
                 is_timeseries = 1;
             }
 
-            key_container = obj;
             obj = key->value;
             assert(obj);
         } else if (key->type == SELVA_OBJECT_ARRAY && key->subtype == SELVA_OBJECT_OBJECT && nr_parts > nr_parts_found && ary_idx >= 0) {
             /*
              * Keep nesting or return an object from the array if this was the last token.
              */
-            key_container = NULL;
             obj = SVector_GetIndex(key->array, ary_idx);
             if (!obj) {
                 if (flags & SELVA_OBJECT_GETKEY_CREATE) {
@@ -722,10 +725,7 @@ static int get_key_obj(struct SelvaObject *obj, const char *key_name_str, size_t
     }
 
     if (flags & SELVA_OBJECT_GETKEY_DELETE) {
-        if (!key_container) {
-            return SELVA_ENOENT; /* weird */
-        }
-        remove_key(key_container, key);
+        remove_key(key);
         key = NULL;
     }
 
@@ -822,7 +822,7 @@ static int get_key(struct SelvaObject *obj, const char *key_name_str, size_t key
      * Note that SELVA_OBJECT_GETKEY_DELETE ignores the array index.
      */
     if (flags & SELVA_OBJECT_GETKEY_DELETE) {
-        remove_key(obj, key);
+        remove_key(key);
         key = NULL;
     }
 