a6e7bdf1d4f6e7887313aab97b37d5ee4a8cc38d
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-08-10 16:33:57 +0200

    Port SelvaObject unit tests

--- selvad/test/util/test-cstrings.mk
+++ selvad/test/util/test-cstrings.mk
@@ -6,3 +6,4 @@ TEST_SRC += test-cstrings.c
 SRC-cstrings += ../../lib/util/cstrings.c
 SRC-cstrings += ../../lib/util/array_field.c
 SRC-cstrings += ../../lib/util/memrchr.c
+SRC-cstrings += ../../lib/util/mempbrk.c
