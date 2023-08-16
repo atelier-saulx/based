7283954d31b712b864e64828f565a427a2ef98b9
Tony Kovanen <tony.kovanen@gmail.com>
2023-08-16 09:37:37 +0200

    rn

--- packages/client/src/get/parse/opts/index.ts
+++ packages/client/src/get/parse/opts/index.ts
@@ -5,11 +5,10 @@ import {
   GetAggregate,
   GetCommand,
   GetNode,
-  GetTraverse,
-  GetTraverseIds,
   Path,
-} from '../types'
-import { hashCmd } from '../util'
+} from '../../types'
+import { hashCmd } from '../../util'
+import { parseList } from './list'
 
 function parseAlias(value: any): string[] | undefined {
   let $field = value.$field
@@ -380,81 +379,4 @@ export async function parseGetOpts(
     cmds: [visited, ...nested, ...topLevel],
     defaults: walked.defaultValues,
   }
-}
-
-function parseList(
-  type: 'aggregate' | 'traverse',
-  initial: GetTraverse | GetAggregate | GetTraverseIds,
-  key: string | number,
-  $list: any
-): GetTraverse | GetTraverseIds | GetAggregate {
-  const cmd: GetTraverse | GetAggregate | GetTraverseIds = { ...initial }
-
-  if ($list?.$find?.$find) {
-    cmd.type = 'ids'
-    ;(<GetTraverseIds>cmd).mainType = type
-
-    const { $sort, $limit, $offset } = $list
-    const opts = { $sort, $limit, $offset }
-
-    const nestedCmd: GetTraverse | GetAggregate | GetTraverseIds = parseList(
-      type,
-      {
-        ...initial,
-        type,
-      },
-      key,
-      { $find: $list.$find.$find, ...opts }
-    )
-
-    for (const opt in opts) {
-      delete $list[opt]
-    }
-
-    // calculate 'abstract markerId' - this will change when there is a concrete id
-    nestedCmd.cmdId = hashCmd(nestedCmd)
-    cmd.nestedFind = nestedCmd
-  }
-
-  const sourceField = $list?.$find?.$traverse ?? $list?.$field ?? String(key)
-  if (Array.isArray($list?.$find?.$traverse)) {
-    // find in id list
-    cmd.source = { idList: sourceField }
-  } else if (Array.isArray(sourceField)) {
-    cmd.traverseExpr = { $any: { $first: sourceField } }
-  } else if (typeof sourceField === 'object') {
-    cmd.traverseExpr = sourceField
-  } else {
-    cmd.sourceField = sourceField
-  }
-
-  if ($list?.$limit !== undefined || $list?.$offset !== undefined) {
-    cmd.paging = {
-      limit: $list?.$limit ?? -1,
-      offset: $list?.$offset ?? 0,
-    }
-  }
-
-  if ($list?.$sort !== undefined) {
-    const { $order, $field } = $list.$sort
-    cmd.sort = {
-      order: $order,
-      field: $field,
-    }
-  }
-
-  if ($list?.isSingle !== undefined) {
-    // @ts-ignore
-    cmd.isSingle = $list.isSingle
-  }
-
-  if ($list?.$find?.$filter) {
-    cmd.filter = $list?.$find?.$filter
-  }
-
-  if ($list?.$find?.$recursive) {
-    cmd.recursive = true
-  }
-
-  return cmd
 }
