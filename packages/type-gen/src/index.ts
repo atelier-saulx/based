2e984db5a02330a531cbe69aae07c06ebf159f5b
Jim de Beer <beerdejim@gmail.com>
2023-10-30 09:49:30 +0100

    fns

--- packages/type-gen/src/index.ts
+++ packages/type-gen/src/index.ts
@@ -1,54 +1,37 @@
 import { BasedFunctionConfig } from '@based/functions'
 import { readFile, writeFile } from 'fs-extra'
 
-export const updateTypes = async (
+export const updateTypesPath = async (
   fns: (
     | { config: BasedFunctionConfig; path: string }
     | { config: BasedFunctionConfig; payload: string; result: string }
   )[],
   opts: {
-    extraImports?: string[]
-    basedPath?: string
-  } = {}
-): Promise<string | void> => {
-  // check if client is correct version
-
-  const basedClientPath =
-    opts.basedPath ||
-    require.resolve('@based/client', { paths: [process.cwd()] })
-
-  if (!basedClientPath) {
-    throw new Error('Cannot find basedClient')
+    isAbstract?: boolean
+    imports?: string[]
+    originalDeclartionPath: string
+    declarationPath: string
   }
-
-  // original
-  const decPath = basedClientPath.replace('/index.js', '/index.d.ts')
-  const decPathOriginal = basedClientPath.replace(
-    '/index.js',
-    '/index_original.d.ts'
-  )
-
+): Promise<string | void> => {
   let decFile = ''
 
   try {
-    decFile = (await readFile(decPathOriginal)).toString('utf-8')
+    decFile = (await readFile(opts.originalDeclartionPath)).toString('utf-8')
   } catch (err) {
     throw new Error(
       'Cannot find original declaration file - you may need to upgrade to @based/client ^4.8.8'
     )
   }
 
-  let imports = opts.extraImports ? opts.extraImports.join('\n') + '\n' : '\n'
-
+  let imports = opts.imports ? opts.imports.join('\n') + '\n' : '\n'
   let callFns = '\n'
   let queryFns = '\n'
-
   let fnCnt = 0
   let queryCnt = 0
-
   let queryMap = '\n'
+  let needsParsing = opts.imports ?? false
 
-  let needsParsing = opts.extraImports ?? false
+  const abstractPrefix = opts.isAbstract ? 'abstract ' : ''
 
   for (const fn of fns) {
     if (fn.config.type === 'function') {
@@ -58,7 +41,7 @@ export const updateTypes = async (
         const name = 'FN_Type_' + fnCnt
         imports += `import type ${name} from '${fn.path}';\n`
         callFns += `
-      call(
+      ${abstractPrefix}call(
         name: '${fn.config.name}',
         payload: Parameters<typeof ${name}>[1],
         opts?: CallOptions
@@ -66,7 +49,7 @@ export const updateTypes = async (
     `
       } else {
         callFns += `
-        call(
+        ${abstractPrefix}call(
           name: '${fn.config.name}',
           payload: ${fn.payload},
           opts?: CallOptions
@@ -75,48 +58,103 @@ export const updateTypes = async (
       }
     } else if (fn.config.type === 'query') {
       queryCnt++
-
       if ('path' in fn) {
         const name = 'Q_Type_' + queryCnt
         imports += `import type ${name} from '${fn.path}';\n`
-
         queryMap += `'${fn.config.name}': { payload: Parameters<typeof ${name}>[1], result: Parameters<Parameters<typeof ${name}>[2]>[0] },`
-
         queryFns += `
-        query(name: '${fn.config.name}', payload: Parameters<typeof ${name}>[1], opts?: QueryOptions): BasedQuery<Parameters<typeof ${name}>[1],Parameters<Parameters<typeof ${name}>[2]>[0]>;
+        ${abstractPrefix}query(name: '${fn.config.name}', payload: Parameters<typeof ${name}>[1], opts?: QueryOptions): BasedQuery<Parameters<typeof ${name}>[1],Parameters<Parameters<typeof ${name}>[2]>[0]>;
     `
       } else {
         queryMap += `'${fn.config.name}': { payload: ${fn.payload}, result: ${fn.result} },`
-
         queryFns += `
-        query(name: '${fn.config.name}', payload: ${fn.payload}, opts?: QueryOptions): BasedQuery<${fn.payload},${fn.result}>;
+        ${abstractPrefix}query(name: '${fn.config.name}', payload: ${fn.payload}, opts?: QueryOptions): BasedQuery<${fn.payload},${fn.result}>;
     `
       }
     }
   }
-
   if (needsParsing) {
     let x = imports + decFile
     if (fnCnt > 0) {
       x = x.replace(
-        'call(name: string, payload?: any, opts?: CallOptions): Promise<any>;',
+        abstractPrefix +
+          'call(name: string, payload?: any, opts?: CallOptions): Promise<any>;',
         callFns
       )
     }
     if (queryCnt > 0) {
       x = x.replace(
-        `query(name: string, payload?: any, opts?: QueryOptions): BasedQuery;`,
+        abstractPrefix +
+          `query(name: string, payload?: any, opts?: QueryOptions): BasedQuery;`,
         queryFns
       )
-
-      console.info('fix')
-
       x = x.replace(
         /export type QueryMap = ([^@]*?)\};([^@]*?)\};/,
         `export type QueryMap = {${queryMap}}`
       )
     }
-    await writeFile(decPath, x)
-    return decPath
+    await writeFile(opts.declarationPath, x)
+    return opts.declarationPath
+  }
+}
+
+export const updateTypes = async (
+  fns: (
+    | { config: BasedFunctionConfig; path: string }
+    | { config: BasedFunctionConfig; payload: string; result: string }
+  )[],
+  opts: {
+    imports?: string[]
+    clientPath?: string
+    functionPath?: string
+  } = {}
+): Promise<{
+  clientPath: string | void
+  functionPath: string | void
+}> => {
+  const inputClientPath =
+    opts.clientPath ||
+    require.resolve('@based/client', { paths: [process.cwd()] })
+
+  const inputFunctionPath =
+    opts.functionPath ||
+    require.resolve('@based/functions', { paths: [process.cwd()] })
+
+  let clientPath: string | void
+  let functionPath: string | void
+
+  if (inputClientPath) {
+    const declarationPath = inputClientPath.replace('/index.js', '/index.d.ts')
+    const originalDeclartionPath = inputClientPath.replace(
+      '/index.js',
+      '/index_original.d.ts'
+    )
+
+    clientPath = await updateTypesPath(fns, {
+      imports: opts.imports,
+      originalDeclartionPath,
+      declarationPath,
+    })
+  }
+
+  if (inputFunctionPath) {
+    const declarationPath = inputFunctionPath.replace(
+      '/index.js',
+      '/client.d.ts'
+    )
+    const originalDeclartionPath = inputFunctionPath.replace(
+      '/index.js',
+      '/client_original.d.ts'
+    )
+    functionPath = await updateTypesPath(fns, {
+      imports: opts.imports,
+      originalDeclartionPath,
+      declarationPath,
+    })
+  }
+
+  return {
+    functionPath,
+    clientPath,
   }
 }
