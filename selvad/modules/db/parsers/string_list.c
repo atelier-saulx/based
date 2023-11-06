8a9393a8ef05db9764195f67a82516b431a22635
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-11-06 16:11:45 +0100

    string list doesn't always end with a separator
    If a string list doesn't end with a separator then `left` will
    underflow when decremented.

--- selvad/modules/db/parsers/string_list.c
+++ selvad/modules/db/parsers/string_list.c
@@ -15,43 +15,40 @@ struct selva_string **parse_string_list(
         size_t in_len,
         int separator)
 {
-    const char *cur = in_str;
-    size_t left = in_len;
     struct selva_string **list = NULL;
+    const char *cur = in_str;
+    ssize_t left = in_len;
     size_t n = 1;
 
-    const size_t list_size = n * sizeof(struct selva_string *);
-    list = selva_realloc(list, list_size);
+    list = selva_realloc(list, sizeof(struct selva_string *));
+    list[0] = NULL;
 
-    list[n - 1] = NULL;
-    if (cur[0] != '\0' || in_len > 0) {
-        do {
-            struct selva_string *el;
-            const char *next;
-            size_t len;
+    while (left > 0) {
+        struct selva_string *el;
+        const char *next;
+        size_t len;
 
-            /*
-             * Find the separator between the current and the next string.
-             */
-            next = memchr(cur, separator, left) ?: cur + left;
-            len = (size_t)((ptrdiff_t)next - (ptrdiff_t)cur);
+        /*
+         * Find the separator between the current and the next string.
+         */
+        next = memchr(cur, separator, left) ?: cur + left;
+        len = (size_t)((ptrdiff_t)next - (ptrdiff_t)cur);
 
-            /*
-             * Create a string.
-             */
-            el = selva_string_create(cur, len, 0);
-            selva_string_auto_finalize(fin, el);
+        /*
+         * Create a string.
+         */
+        el = selva_string_create(cur, len, 0);
+        selva_string_auto_finalize(fin, el);
 
-            /*
-             * Set to the array.
-             */
-            list = selva_realloc(list, ++n * sizeof(struct selva_string *));
-            list[n - 2] = el;
-            list[n - 1] = NULL;
+        /*
+         * Set to the array.
+         */
+        list = selva_realloc(list, ++n * sizeof(struct selva_string *));
+        list[n - 2] = el;
+        list[n - 1] = NULL;
 
-            cur = next + 1;
-            left -= len + 1;
-        } while (left);
+        cur = next + 1;
+        left -= len + 1;
     }
 
     finalizer_add(fin, list, selva_free);
