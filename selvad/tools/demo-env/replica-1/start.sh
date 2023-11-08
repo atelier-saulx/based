55099b3d65924f91408620c105bddeb7f4725a72
Olli Vanhoja <olli.vanhoja@gmail.com>
2023-11-08 11:56:31 +0100

    Improve the demo-env scripts

--- selvad/tools/demo-env/replica-1/start.sh
+++ selvad/tools/demo-env/replica-1/start.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Copyright (c) 2023 SAULX
 #
 # SPDX-License-Identifier: MIT
+cd "${0%/*}"
 LOCPATH=../../../binaries/Linux_x86_64/locale SELVA_PORT=3001 SERVER_SO_REUSE=1 SELVA_REPLICATION_MODE=2 exec ../../../selvad
