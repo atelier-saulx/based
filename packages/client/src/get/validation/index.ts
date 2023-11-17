80fdba9aa88105707bed0d0bde7dbf628859b1b5
Nuno Frade <nfrade.tribal@gmail.com>
2023-11-17 15:56:39 +0100

    Adds get validation for $list, $sort, $order

--- packages/client/src/get/validation/index.ts
+++ packages/client/src/get/validation/index.ts
@@ -1,15 +1,94 @@
 import { getValueByPath, nonRecursiveWalker } from '../../util'
 
-const LIST_ALOWED_CHILDREN = ['$find', '$sort', '$offset', '$limit']
-const listValidation = (query: any, path: string[]) => {
-  const queryPart = getValueByPath(query, path)
+const checkArgumentChildren = (
+  queryPart: any,
+  argument: string,
+  path: string[],
+  allowedChildren: string[]
+) => {
   Object.keys(queryPart).forEach((key) => {
-    if (!LIST_ALOWED_CHILDREN.includes(key)) {
+    if (!allowedChildren.includes(key)) {
       throw new Error(
-        `Query error: Invalid $list property "${key}" at "${path.join('.')}".`
+        `Query error: Invalid ${argument} property "${key}" at "${path.join(
+          '.'
+        )}".`
       )
     }
   })
+}
+
+const checkArgumentParent = (
+  argument: string,
+  path: string[],
+  allowedParents: string[]
+) => {
+  if (path.length === 1 || !allowedParents.includes(path[path.length - 2])) {
+    throw new Error(
+      `Query error: Argument ${argument} cannot be a child of "${
+        path.length === 1 ? 'root' : path[path.length - 2]
+      }" at "${path.join('.')}".`
+    )
+  }
+}
+
+const checkArgumentValues = (
+  queryPart: any,
+  argument: string,
+  path: string[],
+  allowedValues: string[]
+) => {
+  if (!allowedValues.includes(queryPart)) {
+    throw new Error(
+      `Query error: Invalid ${argument} argument value "${String(
+        queryPart
+      )}" at "${path.join('.')}".`
+    )
+  }
+}
+
+const checkArgumentType = (
+  queryPart: any,
+  argument: string,
+  path: string[],
+  allowedTypes: string[]
+) => {
+  if (!allowedTypes.includes(typeof queryPart)) {
+    throw new Error(
+      `Query error: Argument ${argument} must be of type ${
+        allowedTypes.length > 1
+          ? `${allowedTypes.join(' or ')}`
+          : `${allowedTypes[0]}`
+      } at "${path.join('.')}".`
+    )
+  }
+}
+
+const listValidation = (query: any, path: string[]) => {
+  const argument = '$list'
+  const queryPart = getValueByPath(query, path)
+  checkArgumentType(queryPart, argument, path, ['object'])
+  checkArgumentChildren(queryPart, argument, path, [
+    '$find',
+    '$sort',
+    '$offset',
+    '$limit',
+  ])
+}
+
+const sortValidation = (query: any, path: string[]) => {
+  const argument = '$sort'
+  const queryPart = getValueByPath(query, path)
+  checkArgumentType(queryPart, argument, path, ['object'])
+  checkArgumentChildren(queryPart, argument, path, ['$field', '$order'])
+  checkArgumentParent(argument, path, ['$list'])
+}
+
+const orderValidation = (query: any, path: string[]) => {
+  const argument = '$order'
+  const queryPart = getValueByPath(query, path)
+  checkArgumentParent(argument, path, ['$sort'])
+  checkArgumentType(queryPart, argument, path, ['string'])
+  checkArgumentValues(queryPart, argument, path, ['asc', 'desc'])
 }
 
 export const getQueryValidation = (query: any) => {
@@ -21,7 +100,12 @@ export const getQueryValidation = (query: any) => {
         case '$list':
           listValidation(query, path)
           break
-
+        case '$sort':
+          sortValidation(query, path)
+          break
+        case '$order':
+          orderValidation(query, path)
+          break
         default:
           break
       }
--- packages/client/test/getValidation.ts
+++ packages/client/test/getValidation.ts
@@ -1,6 +1,20 @@
 import test from 'ava'
 import { getQueryValidation } from '../src/get/validation'
 
+test('valid query', (t) => {
+  t.notThrows(() => {
+    getQueryValidation({
+      $id: 'id',
+      list: {
+        id: true,
+        $list: {
+          $sort: { $field: 'field', $order: 'asc' },
+        },
+      },
+    })
+  })
+})
+
 test('validate $list', (t) => {
   t.throws(
     () => {
@@ -19,7 +33,75 @@ test('validate $list', (t) => {
     },
     {
       message:
-        /^Query error: Invalid \$list property "something" at "list.\$list".$/,
+        'Query error: Invalid $list property "something" at "list.$list".',
+    }
+  )
+})
+
+test('validate $sort', (t) => {
+  t.throws(
+    () => {
+      getQueryValidation({
+        $id: 'id',
+        list: {
+          id: true,
+          $list: {
+            $sort: 'string',
+          },
+        },
+      })
+    },
+    {
+      message:
+        'Query error: Argument $sort must be of type object at "list.$list.$sort".',
+    }
+  )
+
+  t.throws(
+    () => {
+      getQueryValidation({
+        $id: 'id',
+        $sort: { $field: 'field', $order: 'asc' },
+      })
+    },
+    {
+      message:
+        'Query error: Argument $sort cannot be a child of "root" at "$sort".',
+    }
+  )
+
+  t.throws(
+    () => {
+      getQueryValidation({
+        $id: 'id',
+        one: {
+          two: {
+            $sort: { $field: 'field', $order: 'asc' },
+          },
+        },
+      })
+    },
+    {
+      message:
+        'Query error: Argument $sort cannot be a child of "two" at "one.two.$sort".',
+    }
+  )
+
+  t.throws(
+    () => {
+      getQueryValidation({
+        $id: 'id',
+        list: {
+          id: true,
+          $list: {
+            $sort: { $field: 'field', $order: 'something' },
+          },
+        },
+      })
+    },
+    {
+      message:
+        'Query error: Invalid $order argument value "something" at "list.$list.$sort.$order".',
     }
   )
 })
