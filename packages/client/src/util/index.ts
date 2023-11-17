6952ea12ce4954d2cd83e8701245e8648a9cef0b
Nuno Frade <nfrade.tribal@gmail.com>
2023-11-17 14:34:38 +0100

    Adds nonRecursiveWalker() to utils

--- packages/client/src/util/index.ts
+++ packages/client/src/util/index.ts
@@ -79,6 +79,9 @@ export function getValueByPath(
 ) {
   const p = typeof path === 'string' ? path.split('.') : path
   if (typeof obj === 'object') {
+    if (!path.length) {
+      return obj
+    }
     let current = obj
     for (let i = 0; i < p.length; i++) {
       const key = p[i]
@@ -126,4 +129,54 @@ export const getSchemaTypeFieldByPath = (
     }
   }
   return undefined
+}
+
+export const nonRecursiveWalker = (
+  obj: any,
+  fn: (target: any, path: string[], type: number) => void,
+  targetObjects?: boolean
+) => {
+  if (typeof obj !== 'object') {
+    return
+  }
+  if (typeof fn !== 'function') {
+    throw new Error('fn must be a function')
+  }
+
+  // const stack = new Array<string[]>()
+  //
+  // stack.push([])
+  //
+  // while (stack.length) {
+  //   const current = getValueByPath(obj, stack[0])
+  //   for (const key in current) {
+  //     if (typeof current[key] === 'object') {
+  //       stack.push(stack[0].concat(key))
+  //       if (targetObjects) {
+  //         fn(current[key], stack[0].concat(key), 1)
+  //       }
+  //     } else {
+  //       fn(current[key], stack[0].concat(key), 0)
+  //     }
+  //   }
+  //   stack.shift()
+  // }
+
+  const stack = new Array<[target: any, path: string[]]>()
+
+  stack.push([obj, []])
+
+  while (stack.length) {
+    for (const key in stack[0][0]) {
+      if (typeof stack[0][0][key] === 'object') {
+        stack.push([stack[0][0][key], stack[0][1].concat(key)])
+        if (targetObjects) {
+          fn(stack[0][0][key], stack[0][1].concat(key), 1)
+        }
+      } else {
+        fn(stack[0][0][key], stack[0][1].concat(key), 0)
+      }
+    }
+    stack.shift()
+  }
 }
--- packages/client/test/utils.ts
+++ packages/client/test/utils.ts
@@ -3,8 +3,10 @@ import {
   pathToQuery,
   getValueByPath,
   getSchemaTypeFieldByPath,
+  nonRecursiveWalker,
 } from '../src/util'
 import { BasedSchemaTypePartial } from '@based/schema'
+import { deepEqual } from 'assert'
 
 test('pathToQuery should create an object structure out of a path', (t) => {
   t.deepEqual(pathToQuery(['level1', 'level2', 'level3', 'level4'], true), {
@@ -92,6 +94,8 @@ test('getValueByPath', (t) => {
     undefined
   )
   t.deepEqual(getValueByPath({}, ['nonExisting', 'none']), undefined)
+  const obj = { one: 'one', two: { three: 'three' } }
+  t.deepEqual(getValueByPath(obj, []), obj)
 })
 
 test('getSchemaTypeFieldByPath', (t) => {
@@ -209,5 +213,104 @@ test('getSchemaTypeFieldByPath on recordsith object values', (t) => {
       ['level1', 'level2', 'level3', 'level4']
     ),
     { type: 'string' }
+  )
+})
+
+test('non recursive walker', (t) => {
+  let result: string[] = []
+  const obj = {
+    a1: {
+      a1b1: {
+        a1b1c1: 'a1b1c1',
+        a1b1c2: {
+          a1b1c2d1: 'a1b1c2d1',
+        },
+      },
+    },
+    a2: 'a2',
+    a3: {
+      a3b1: 'a3b1',
+    },
+  }
+  nonRecursiveWalker(
+    obj,
+    (target, path, type) => {
+      result.push(path.join('.'))
+      t.is(getValueByPath(obj, path), target)
+      if (path.join('.') === 'a1.a1b1') {
+        t.is(type, 1)
+      }
+      if (path.join('.') === 'a2') {
+        t.is(type, 0)
+      }
+    },
+    true
+  )
+  t.deepEqual(
+    result.sort(),
+    [
+      'a1',
+      'a1.a1b1',
+      'a1.a1b1.a1b1c1',
+      'a1.a1b1.a1b1c2',
+      'a1.a1b1.a1b1c2.a1b1c2d1',
+      'a2',
+      'a3',
+      'a3.a3b1',
+    ].sort()
+  )
+
+  result = []
+  nonRecursiveWalker(obj, (_target, path, _type) => {
+    result.push(path.join('.'))
+  })
+  t.deepEqual(
+    result.sort(),
+    ['a1.a1b1.a1b1c1', 'a1.a1b1.a1b1c2.a1b1c2d1', 'a2', 'a3.a3b1'].sort()
+  )
+
+  t.notThrows(() => {
+    nonRecursiveWalker(
+      {
+        a1: 'a1',
+        a2: null,
+        a3: {
+          a3b1: 'a3b1',
+          a3b2: {
+            a3b2c1: null,
+          },
+        },
+      },
+      (_target, _path, _type) => {}
+    )
+  })
+  t.notThrows(() => {
+    nonRecursiveWalker({}, (_target, _path, _type) => {
+      t.fail()
+    })
+  })
+  t.notThrows(() => {
+    nonRecursiveWalker(undefined, (_target, _path, _type) => {
+      t.fail()
+    })
+  })
+  t.notThrows(() => {
+    nonRecursiveWalker(null, (_target, _path, _type) => {
+      t.fail()
+    })
+  })
+  t.notThrows(() => {
+    nonRecursiveWalker('string', (_target, _path, _type) => {
+      t.fail()
+    })
+  })
+  t.throws(
+    () => {
+      // @ts-ignore
+      nonRecursiveWalker({}, 'string')
+    },
+    {
+      message: 'fn must be a function',
+    }
   )
 })
