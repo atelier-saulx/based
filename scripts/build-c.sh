cc4147b2c631eac2b423496b60dccd8a14056024
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-01-23 11:21:20 +0100

    Don't rebuild locales every time
    Fixes #285

--- scripts/build-c.sh
+++ scripts/build-c.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2023 SAULX
 # SPDX-License-Identifier: MIT
 
-rm -rf packages/server/selvad/local/*
+rm -rf packages/server/selvad/local/lib packages/server/selvad/local/modules packages/server/selvad/local/selvad
 cd selvad
 make
 INSTALL_DIR=../packages/server/selvad/local make install
