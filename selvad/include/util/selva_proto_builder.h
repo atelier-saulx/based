b03b2687fe5388600e9ceced82fd6e509a705ba2
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-02-09 19:32:05 +0100

    proto_builder: support replication_cmd

--- selvad/include/util/selva_proto_builder.h
+++ selvad/include/util/selva_proto_builder.h
@@ -1,12 +1,15 @@
 /*
- * Copyright (c) 2023 SAULX
+ * Copyright (c) 2023-2024 SAULX
  * SPDX-License-Identifier: MIT
  */
 #pragma once
 
+struct selva_proto_replication_cmd;
+
 struct selva_proto_builder_msg {
+    bool encapsulated; /*!< Encapsulated with an array if more than one value is written. */
+    size_t nr_values; /*!< Total number of values in the buffer when encapsulated is set. */
     size_t bsize;
-    size_t nr_values; /*!< Total number of values in the buffer. */
     uint8_t *buf;
 };
 
@@ -14,8 +17,9 @@ struct selva_proto_builder_msg {
  * Initialize a proto message builder struct.
  * This function allocates msg->buf in a way that it can be freed with either
  * using selva_proto_builder_deinit() or optionally with selva_free().
+ * @param encapsulate the message with an array if more than one value is inserted.
  */
-void selva_proto_builder_init(struct selva_proto_builder_msg *msg)
+void selva_proto_builder_init(struct selva_proto_builder_msg *msg, bool encapsulate)
     __attribute__((access(write_only, 1)));
 
 /**
@@ -55,4 +59,9 @@ void selva_proto_builder_insert_array(struct selva_proto_builder_msg *msg)
     __attribute__((access(read_write, 1)));
 
 void selva_proto_builder_insert_array_end(struct selva_proto_builder_msg *msg)
+    __attribute__((access(read_write, 1)));
+
+void selva_proto_builder_insert_replication_cmd(
+        struct selva_proto_builder_msg *msg,
+        struct selva_proto_replication_cmd *cmd)
     __attribute__((access(read_write, 1)));
