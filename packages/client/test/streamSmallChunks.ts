2c0b1f940f1155b1e19d21da643a7153bb002a03
Jim de Beer <beerdejim@gmail.com>
2024-02-13 16:07:05 +0100

    update utils

--- packages/client/test/streamSmallChunks.ts
+++ packages/client/test/streamSmallChunks.ts
@@ -1,84 +1,9 @@
 import test, { ExecutionContext } from 'ava'
 import { BasedServer } from '@based/server'
 import { BasedClient } from '../src/index.js'
-// import { readStream } from '@saulx/utils'
 import { Readable } from 'node:stream'
 import getPort from 'get-port'
-import { dirname, join } from 'node:path'
-import { fileURLToPath } from 'url'
-
-import { Stream, Writable } from 'stream'
-
-export const readStream = (
-  stream: Stream,
-  opts?: { throttle?: number; maxCunkSize?: number }
-): Promise<Buffer> =>
-  new Promise((resolve, reject) => {
-    const maxCunkSize = opts?.maxCunkSize ?? 0
-    const throttle = opts?.throttle ?? 0
-    const buffers: Buffer[] = []
-
-    const processChunk = (c, next) => {
-      const x = c.slice(0, maxCunkSize)
-      buffers.push(x)
-      const chunkP = c.slice(maxCunkSize)
-      console.log(
-        c.byteLength,
-        chunkP.byteLength + maxCunkSize,
-        x.byteLength === maxCunkSize
-      )
-
-      if (chunkP.byteLength > maxCunkSize) {
-        if (throttle) {
-          setTimeout(() => {
-            processChunk(chunkP, next)
-          }, throttle)
-        } else {
-          processChunk(chunkP, next)
-        }
-      } else {
-        if (throttle) {
-          setTimeout(() => {
-            next()
-          }, throttle)
-        } else {
-          next()
-        }
-      }
-    }
-
-    const s = new Writable({
-      write: (c, _encoding, next) => {
-        if (maxCunkSize && c.byteLength > maxCunkSize) {
-          processChunk(c, next)
-        } else {
-          if (typeof c === 'string') {
-            buffers.push(Buffer.from(c))
-          } else {
-            buffers.push(c)
-          }
-          if (throttle) {
-            setTimeout(() => {
-              next()
-            }, throttle)
-          } else {
-            next()
-          }
-        }
-      },
-    })
-
-    s.on('error', (err) => {
-      reject(err)
-    })
-
-    s.on('finish', () => {
-      console.info('DONE! !!!!', resolve)
-      resolve(Buffer.concat(buffers))
-    })
-
-    stream.pipe(s)
-  })
+import { readStream } from '@saulx/utils'
 
 type T = ExecutionContext<{ port: number; ws: string; http: string }>
 
@@ -133,7 +58,7 @@ test('stream small chunks', async (t: T) => {
     url: async () => t.context.ws,
   })
 
-  const len = 10000
+  const len = 1000
   const bigBod: any[] = []
   for (let i = 0; i < len; i++) {
     bigBod.push({ flap: 'snurp', i })
@@ -152,22 +77,26 @@ test('stream small chunks', async (t: T) => {
     }
   }
 
-  const result = await client.stream(
-    'hello',
-    {
-      payload: { power: true },
-      size: payload.byteLength,
-      mimeType: 'application/json',
-      contents: Readable.from(generate()),
-    },
-    (p) => {
-      console.log('PROGRESS', Math.round(p * 100), '%')
-    }
-  )
+  try {
+    const result = await client.stream(
+      'hello',
+      {
+        payload: { power: true },
+        size: payload.byteLength,
+        mimeType: 'application/json',
+        contents: Readable.from(generate()),
+      },
+      (p) => {
+        console.log('PROGRESS', Math.round(p * 100), '%')
+      }
+    )
 
-  console.info('DERP', result)
+    console.info('DERP', result)
 
-  t.is(result, len)
+    t.is(result, len)
+  } catch (err) {
+    console.error(err)
+  }
 
   // cycles of 3 secs
   client.disconnect()
