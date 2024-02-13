c768f63d94d8461ba8f1e24b2b62ec6dfb8e1f3f
Jim de Beer <beerdejim@gmail.com>
2024-02-13 16:23:17 +0100

    fix

--- packages/client/test/stream.ts
+++ packages/client/test/stream.ts
@@ -198,7 +198,7 @@ test('stream functions - streamContents error', async (t: T) => {
   await server.destroy()
 })
 
-test('stream functions - path', async (t: T) => {
+test.only('stream functions - path', async (t: T) => {
   const server = new BasedServer({
     port: t.context.port,
     functions: {
