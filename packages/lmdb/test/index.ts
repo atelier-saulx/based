c407574ec5b2b1f154d9ab7c91c2c4fb32e36834
Jim de Beer <beerdejim@gmail.com>
2024-03-14 08:24:32 +0100

    fix

--- packages/lmdb/test/index.ts
+++ packages/lmdb/test/index.ts
@@ -1,17 +1,18 @@
 import test from 'ava'
 import lmdb from 'node-lmdb'
-import { wait } from '@saulx/utils'
+import { setByPath, wait } from '@saulx/utils'
 import { Worker } from 'node:worker_threads'
 import { dirname } from 'node:path'
 import { fileURLToPath } from 'url'
 import fs from 'node:fs/promises'
 import zlib from 'node:zlib'
+import { BasedSchemaFieldType } from '@based/schema'
 
 const __dirname = dirname(fileURLToPath(import.meta.url).replace('/dist/', '/'))
 const d = dirname(fileURLToPath(import.meta.url))
 
 const amount = 1000
-const rounds = 100
+const rounds = 1
 
 const worker = (i: number) => {
   return new Promise((resolve) => {
@@ -130,8 +131,140 @@ test('create server', async (t) => {
   cursor.close()
   txn.commit()
 
-  // 60 bytes
-  // 5 bytes
+  const lenMap = {
+    integer: 4, // 32bit
+    boolean: 1, // 1bit (bit overhead)
+    // double-precision 64-bit binary format IEEE 754 value
+    number: 8, // 64bit
+    // means put in own
+    string: 0,
+  }
+
+  // redesign as well
+  const type: any = {
+    fields: {
+      flap: {
+        type: 'integer',
+      },
+      nip: {
+        type: 'string',
+      },
+      mep: {
+        type: 'number',
+      },
+      snurp: {
+        type: 'object',
+        properties: {
+          derp: { type: 'integer' },
+          bla: { type: 'string' },
+          hup: {
+            type: 'object',
+            properties: {
+              x: { type: 'integer' },
+              isDope: { type: 'boolean' },
+            },
+          },
+        },
+      },
+    },
+  }
+
+  const convertSchemaToProto = (
+    type: any,
+    result: any = {
+      _cnt: 0,
+      fields: {},
+      dbMap: {
+        _len: 0,
+        tree: {},
+      },
+    },
+    path: string[] = [],
+    top: boolean = true,
+  ) => {
+    let target: any
+    if (type.fields) {
+      target = type.fields
+    } else if (type.properties) {
+      target = type.properties
+    }
+    for (const key in target) {
+      const f = target[key]
+      const p = [...path, key]
+      if (f.type === 'object') {
+        convertSchemaToProto(f, result, p, false)
+      } else {
+        result.fields[p.join('.')] = {
+          index: result._cnt,
+          type: f.type,
+          path: p,
+        }
+        result._cnt++
+      }
+    }
+
+    if (top) {
+      for (const key in result.fields) {
+        const f = result.fields[key]
+        const len = lenMap[f.type]
+        if (len) {
+          if (!result.dbMap._) {
+            result.dbMap._ = []
+          }
+          result.dbMap._len += len
+          result.dbMap._.push(f)
+          setByPath(result.dbMap.tree, f.path, [f.index, 0])
+        } else {
+          setByPath(result.dbMap.tree, f.path, [f.index, 1])
+          result.dbMap[f.index] = f
+        }
+      }
+    }
+
+    return result
+  }
+
+  // unsafe prob
+
+  const walkObj = (obj, tree, schema, arr: Uint8Array) => {
+    for (const key in obj) {
+      const t = tree[key]
+      const o = obj[key]
+      if (typeof o === 'object') {
+        walkObj(o, t, schema, arr)
+      } else {
+        console.log('write', t)
+      }
+    }
+  }
+
+  const genSetId = (obj, schema, buf?: Buffer) => {
+    let arr
+    if (!buf) {
+      arr = new Uint8Array(schema.dbMap._len)
+    }
+
+    walkObj(obj, schema.tree, schema, arr)
+
+    // const buf = Buffer.allocUnsafe(10);
+
+    return Buffer.from(arr)
+  }
+
+  const schemaField = convertSchemaToProto(type)
+
+  console.dir(schemaField, { depth: 10 })
+
+  console.dir(
+    'set',
+    genSetId(
+      {
+        flap: 12,
+        snurp: { derp: 100, hup: { isDope: true, x: 1 } },
+      },
+      schemaField,
+    ),
+  )
 
   console.info('HELLO DONE')
 
