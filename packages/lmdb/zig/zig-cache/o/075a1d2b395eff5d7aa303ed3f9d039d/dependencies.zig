78d83fcf5a8a9af098a145782c7741c9a1dcb3c6
Jim de Beer <beerdejim@gmail.com>
2024-03-15 12:53:29 +0100

    remove

--- packages/lmdb/zig/zig-cache/o/075a1d2b395eff5d7aa303ed3f9d039d/dependencies.zig
+++
@@ -1,8 +0,0 @@
-pub const imports = struct {
-    pub const @"lmdb.lmdb" = @import("12209bfdbf43d479c2cac0ca2212853ab2136f1621f49461f62c7932a7819bc29110");
-    pub const lmdb = @import("12208c41a60cc152bb8b16d355f75b5302d3f1030188fdf941966dfbf2cf3930721f");
-};
-pub const build_root = struct {
-    pub const lmdb = "/Users/jimdebeer/.cache/zig/p/12208c41a60cc152bb8b16d355f75b5302d3f1030188fdf941966dfbf2cf3930721f";
-    pub const @"lmdb.lmdb" = "/Users/jimdebeer/.cache/zig/p/12209bfdbf43d479c2cac0ca2212853ab2136f1621f49461f62c7932a7819bc29110";
-};
--- packages/lmdb/zig/zig-cache/o/b644b26c6826e81096a798da2389fae7/cimport.h
+++
@@ -1,1 +0,0 @@
-#include <lmdb.h>
