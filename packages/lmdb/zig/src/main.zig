d2fee8c8e7fa26bb35fd14307b819dea6b8e48c6
Jim de Beer <beerdejim@gmail.com>
2024-03-15 12:32:45 +0100

    fix

--- packages/lmdb/zig/src/main.zig
+++ packages/lmdb/zig/src/main.zig
@@ -34,17 +34,14 @@ fn bla() !void {
     var i: u32 = 0;
     // var key: [4]u8 = undefined;
     const value = "bla";
-    var buffer: [6]u8 = undefined; // Define a buffer to hold the string
 
     while (i < 1_000_000) : (i += 1) {
         // std.mem.writeInt(u32, &key, i, .big);
 
-        _ = std.fmt.bufPrint(&buffer, "{}", .{i}) catch unreachable;
-
-        try txn.set(&buffer, &buffer);
+        var buffer: [6]u8 = undefined; // Define a buffer to hold the string
+        const str = try std.fmt.bufPrint(&buffer, "{}", .{i});
+        try txn.set(str, value);
     }
-
-    std.debug.print("{s}", value);
 
     std.debug.print("1M took ms: {}\n", .{@divFloor(std.time.nanoTimestamp() - currentTime, 1_000_000)});
 
