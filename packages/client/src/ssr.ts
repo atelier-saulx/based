6953455060ff4589287a817f6bcb1bb2b8b2fb50
Jim de Beer <beerdejim@gmail.com>
2024-04-13 14:03:16 +0200

    selective query cache

--- packages/client/src/ssr.ts
+++ packages/client/src/ssr.ts
@@ -1,8 +1,9 @@
+import { genObserveId } from './genObserveId.js'
 import { BasedClient } from './index.js'
 import { CacheValue } from './types/cache.js'
 
 export const createCacheObject = (
-  client: BasedClient
+  client: BasedClient,
 ): {
   [key: string]: CacheValue
 } => {
@@ -13,8 +14,22 @@ export const createCacheObject = (
   return m
 }
 
-export const createCacheScriptTag = (client: BasedClient): string => {
+export const createCacheScriptTag = (
+  client: BasedClient,
+  queries?: { endpoint: string; payload?: any }[],
+): string => {
+  if (queries) {
+    const m: any = {}
+    for (const q of queries) {
+      const key = genObserveId(q.endpoint, q.payload)
+      if (client.cache.has(key)) {
+        m[key] = client.cache.get(key)
+      }
+    }
+    return `<script>window.__basedcache__=${JSON.stringify(m)}</script>`
+  }
+
   return `<script>window.__basedcache__=${JSON.stringify(
-    createCacheObject(client)
+    createCacheObject(client),
   )}</script>`
 }
---
+++ packages/client/test/ssr.ts
@@ -0,0 +1,69 @@
+import test, { ExecutionContext } from 'ava'
+import { BasedClient } from '../src/index.js'
+import { BasedServer } from '@based/server'
+import { wait } from '@saulx/utils'
+import getPort from 'get-port'
+import { createCacheScriptTag } from '../src/ssr.js'
+
+type T = ExecutionContext<{ port: number; ws: string; http: string }>
+
+test.beforeEach(async (t: T) => {
+  t.context.port = await getPort()
+  t.context.ws = `ws://localhost:${t.context.port}`
+  t.context.http = `http://localhost:${t.context.port}`
+})
+
+test('query cache', async (t: T) => {
+  const client = new BasedClient()
+
+  const server = new BasedServer({
+    port: t.context.port,
+    rateLimit: { ws: 1e9, http: 1e9, drain: 1e3 },
+    functions: {
+      configs: {
+        counter: {
+          type: 'query',
+          uninstallAfterIdleTime: 1e3,
+          fn: (_, __, update) => {
+            const x: string[] = []
+            for (let i = 0; i < 3; i++) {
+              x.push('flap ' + i)
+            }
+            update(x)
+            return () => {}
+          },
+        },
+      },
+    },
+  })
+
+  await server.start()
+
+  client.connect({
+    url: async () => {
+      return t.context.ws
+    },
+  })
+
+  await client.query('counter').get()
+  await client.query('counter', { bla: true }).get()
+
+  const script = createCacheScriptTag(client, [{ endpoint: 'counter' }])
+
+  t.deepEqual(
+    script,
+    '<script>window.__basedcache__={"5609164081779":{"v":["flap 0","flap 1","flap 2"],"c":2931330242745,"s":28}}</script>',
+  )
+
+  const scriptAll = createCacheScriptTag(client)
+
+  t.deepEqual(
+    scriptAll,
+    '<script>window.__basedcache__={"5609164081779":{"v":["flap 0","flap 1","flap 2"],"c":2931330242745,"s":28},"12895924860639":{"v":["flap 0","flap 1","flap 2"],"c":2931330242745,"s":28}}</script>',
+  )
+
+  await wait(1500)
+
+  await server.destroy()
+  client.destroy()
+})
