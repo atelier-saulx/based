ad05251abe9af37896912116c372f0e625100b85
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-04-26 16:38:43 +0200

    Improve node expire
    - Pack expire into 30 bits and set epoch as 2024
    - Now we can also pack node flags into 2 bits and have 32 bits
      for some later use in the struct

--- selvad/modules/db/include/hierarchy.h
+++ selvad/modules/db/include/hierarchy.h
@@ -113,16 +113,14 @@ enum SelvaNodeFlags {
  */
 typedef struct SelvaHierarchyNode {
     Selva_NodeId id; /* Must be first. */
-    enum SelvaNodeFlags flags;
+    enum SelvaNodeFlags flags: 2;
+#define SELVA_HIERARCHY_EXPIRE_EPOCH 1704067200000
     /**
      * Expiration timestamp for this node.
-     * epoch = UNIX 2023-01-01T00:00:00Z = 1672531200000 (UNIX)
      * 0 = never expires
-     * As this is a 32-bit unsigned integer, it means that we should be good
-     * until the year 2106.
-     * 1970+(2^32)/60/60/24/365 = 2106
+     * max_life = <epoch year>+(2^<bits>)/60/60/24/365
      */
-    uint32_t expire;
+    uint32_t expire: 30;
     struct trx_label trx_label;
     struct SelvaHierarchyMetadata metadata;
     RB_ENTRY(SelvaHierarchyNode) _index_entry;
