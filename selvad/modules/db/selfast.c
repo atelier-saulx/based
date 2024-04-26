f0361b44cd43ba1e263e12037e2bc20ce235c812
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-04-26 06:41:03 +0200

    Fast find

--- selvad/modules/db/selfast.c
+++ selvad/modules/db/selfast.c
@@ -15,6 +15,7 @@
 #include "selva_proto.h"
 #include "selva_server.h"
 #include "selva_io.h"
+#include "rpn.h"
 #include "comparator.h"
 #include "edge.h"
 #include "hierarchy.h"
@@ -122,6 +123,55 @@ static void do_find(struct selva_string *out_buf)
     (void)selva_server_run_cmd2buf(CMD_ID_HIERARCHY_FIND, ts_now(), &buf, sizeof(buf), out_buf);
 }
 
+struct fast_find_ctx {
+    struct rpn_ctx *rpn_ctx;
+    struct rpn_expression *filter;
+    ssize_t limit;
+    struct selva_string *out_buf;
+};
+
+static int fast_find_cb(
+        struct SelvaHierarchy *hierarchy,
+        const struct SelvaHierarchyTraversalMetadata *,
+        struct SelvaHierarchyNode *node,
+        void *arg)
+{
+    struct fast_find_ctx *ctx = (struct fast_find_ctx *)arg;
+    struct rpn_ctx *rpn_ctx = ctx->rpn_ctx;
+    int take = false;
+
+    rpn_set_reg(rpn_ctx, 0, node->id, SELVA_NODE_ID_SIZE, RPN_SET_REG_FLAG_IS_NAN);
+    rpn_ctx->data.hierarchy = hierarchy;
+    rpn_ctx->data.node = node;
+    rpn_ctx->data.obj = SelvaHierarchy_GetNodeObject(node);
+
+    (void)rpn_bool(rpn_ctx, ctx->filter, &take);
+    if (take) {
+        selva_string_append(ctx->out_buf, node->id, SELVA_NODE_ID_SIZE);
+        if (ctx->limit > 0) ctx->limit--;
+    }
+
+    return ctx->limit == 0;
+}
+
+
+static void do_fast_find(struct selva_string *out_buf)
+{
+    __auto_free_rpn_ctx struct rpn_ctx *rpn_ctx = rpn_init(1);
+    __auto_free_rpn_expression struct rpn_expression *filter = rpn_compile("#1 \"vc\" g I");
+    struct SelvaHierarchyCallback cb = {
+        .node_cb = fast_find_cb,
+        .node_arg = &(struct fast_find_ctx){
+            .rpn_ctx = rpn_ctx,
+            .filter = filter,
+            .limit = 1000,
+            .out_buf = out_buf,
+        },
+    };
+
+    (void)SelvaHierarchy_TraverseAll(main_hierarchy, &cb);
+}
+
 static void selfast(struct selva_server_response_out *resp, const void *buf, size_t len)
 {
     struct timespec ts_start, ts_end, ts_diff;
@@ -151,6 +201,7 @@ static void selfast(struct selva_server_response_out *resp, const void *buf, siz
 
     SELVA_LOG(SELVA_LOGL_INFO, "n: %d t: %f ms", N_MODIFY, t);
 
+#if 0
     ts_monotime(&ts_start);
     struct selva_string *out_buf = selva_string_create(NULL, 0, SELVA_STRING_MUTABLE);
     do_find(out_buf);
@@ -160,6 +211,7 @@ static void selfast(struct selva_server_response_out *resp, const void *buf, siz
     t = timespec2ms(&ts_diff);
 
     SELVA_LOG(SELVA_LOGL_INFO, "find took: %f ms", t);
+#endif
 
     /*
      * Print response.
@@ -170,6 +222,24 @@ static void selfast(struct selva_server_response_out *resp, const void *buf, siz
     SELVA_LOG(SELVA_LOGL_ERR, "len: %zu", msg_len);
     selva_proto_print(stdout, msg, msg_len);
     selva_string_free(out_buf);
+#endif
+
+    ts_monotime(&ts_start);
+    struct selva_string *out_buf = selva_string_create(NULL, 0, SELVA_STRING_MUTABLE);
+    do_fast_find(out_buf);
+    ts_monotime(&ts_end);
+
+    timespec_sub(&ts_diff, &ts_end, &ts_start);
+    t = timespec2ms(&ts_diff);
+
+    SELVA_LOG(SELVA_LOGL_INFO, "find took: %f ms", t);
+
+#if 0
+    size_t out_len;
+    const char *out_str = selva_string_to_str(out_buf, &out_len);
+    for (size_t i = 0; i < out_len; i += SELVA_NODE_ID_SIZE) {
+        SELVA_LOG(SELVA_LOGL_INFO, "%.*s", (int)SELVA_NODE_ID_SIZE, &out_str[i]);
+    }
 #endif
 }
 
