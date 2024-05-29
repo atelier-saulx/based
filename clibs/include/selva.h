e8643b1064ab5ccaf85f9346ebdf94a920afc97e
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-05-29 10:24:51 +0200

    More typing

--- clibs/include/selva.h
+++ clibs/include/selva.h
@@ -4,7 +4,15 @@
  */
 #pragma once
 
+#include <sys/types.h>
+#include <stddef.h>
 #include <stdint.h>
+#include "tree.h"
+#include "util/mempool.h"
+#include "util/svector.h"
+#include "util/trx.h"
+
+RB_HEAD(selva_node_index_tree, SelvaNode);
 
 enum SelvaFieldType {
     SELVA_FIELD_TYPE_EDGE = 0,
@@ -32,6 +40,7 @@ typedef uint64_t node_id_t;
 struct SelvaNodeSchema {
     uint16_t nr_emb_fields;
     uint16_t nr_dyn_fields;
+    uint16_t nr_fields; /* !< nr_emb_fields + nr_dyn_fields. */
     field_t created_field;
     field_t updated_field;
     struct SelvaFieldSchema {
@@ -42,19 +51,25 @@ struct SelvaNodeSchema {
             struct SelvaNodeSchema *src_node;
             struct SelvaNodeSchema *dst_node;
             field_t inverse_field;
-        };
+        } edge_constraint;
     } field_schemas[] __counted_by(nr_fields);
 };
 
+/**
+ * Selva node.
+ */
 struct SelvaNode {
     node_id_t node_id;
     uint32_t expire;
-    uint16_t ns_index; /*!< Index to NodeSchema. */
+    uint16_t ns_index; /*!< Index to SelvDb.type[] to get the NodeSchema. */
     struct trx_label trx_label;
     char *dyn_fields;
-    char emb_fields[];
+    char emb_fields[]; /*!< This is counted by nr_emb_fields in SelvaNodeSchema. */
 };
 
+/**
+ * Database instance.
+ */
 struct SelvaDb {
     /**
      * Global transaction state.
@@ -65,11 +80,27 @@ struct SelvaDb {
     struct {
         struct selva_node_schema *ns;
         struct selva_node_index_tree index_head;
+#if 0
         struct {
             STATIC_SELVA_OBJECT(_obj_data);
         } aliases;
+#endif
         struct mempool nodepool;
     } *type __counted_by(nr_types);
+
+    /**
+     * Expiring nodes.
+     */
+    struct {
+        SVector list; /*!< List of all expiring nodes. */
+#define HIERARCHY_EXPIRING_NEVER UINT32_MAX
+        int tim_id; /*!< 1 sec timer. */
+        /**
+         * Timestamp of the node expiring next.
+         * Set to HIERARCHY_EXPIRING_NEVER if nothing is expiring.
+         */
+        uint32_t next;
+    } expiring;
 };
 
 __attribute__((visibility("default"))) struct SelvaDb *selva_create_db(void);
--- clibs/lib/selva/main.c
+++ clibs/lib/selva/main.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 SAULX
+ * SPDX-License-Identifier: MIT
+ */
+#include "selva.h"
+
 __attribute__((visibility("default"))) int best(void)
 {
     return 2;
