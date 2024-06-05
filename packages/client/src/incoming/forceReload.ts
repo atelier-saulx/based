8f3117f084b3dcdf6d593b5164f3861fcdebfe0d
Jim de Beer <beerdejim@gmail.com>
2024-06-05 13:24:23 +0200

    fix

--- packages/client/src/incoming/forceReload.ts
+++ packages/client/src/incoming/forceReload.ts
@@ -2,6 +2,11 @@ import { BasedClient } from '../index.js'
 
 export const cacheId = '/-74rxe8kzryxek07e36yr'
 
+// clear based cache (extra byte)
+// if to background reconnect needs to know if it handled the bust
+// add seqId
+// keep seqId in mem for 24h ? :/ ? how long
+// add seqId on connect after access key
 export const forceReload = (client: BasedClient, type: number) => {
   // 0 = all
   // 1 = browser
