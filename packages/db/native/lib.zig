92eb05d6bbac2c70ea37343e8489bd273844a13b
Jim de Beer <beerdejim@gmail.com>
2024-06-17 12:43:15 +0200

    zst

--- packages/db/native/lib.zig
+++ packages/db/native/lib.zig
@@ -8,6 +8,7 @@ const Sets = @import("set/set.zig");
 const Dels = @import("del/del.zig");
 const Query = @import("./query/query.zig");
 const Modify = @import("./set/modify.zig");
+const Compress = @import("./zstd.zig");
 
 const jsThrow = errors.jsThrow;
 const createEnv = Envs.createEnv;
@@ -21,9 +22,8 @@ const delBatch4 = Dels.delBatch4;
 const delBatch8 = Dels.delBatch8;
 const query = Query.getQuery;
 const modify = Modify.modify;
-
+const zstd = Compress.zstd;
 const dbthrow = errors.mdbCheck;
-
 const NapiError = error{NapiError};
 
 pub fn throwError(env: c.napi_env, err: Error) c.napi_value {
@@ -66,5 +66,6 @@ export fn napi_register_module_v1(env: c.napi_env, exports: c.napi_value) c.napi
     registerFunction(env, exports, "delBatch8", delBatch8) catch return null;
     registerFunction(env, exports, "getQuery", query) catch return null;
     registerFunction(env, exports, "modify", modify) catch return null;
+    registerFunction(env, exports, "zstd", zstd) catch return null;
     return exports;
 }
---
+++ packages/db/native/zstd.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const c = @import("./c.zig");
+const napi = @import("./napi.zig");
+
+pub fn zstd(env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.napi_value {
+    return getZstd(env, info) catch |err| {
+        napi.jsThrow(env, @errorName(err));
+        return null;
+    };
+}
+
+fn getZstd(env: c.napi_env, info: c.napi_callback_info) !c.napi_value {
+    const total_size = 100;
+
+    const args = try napi.getArgs(1, env, info);
+
+    const str = try napi.getBuffer("string", env, args[0]);
+
+    std.debug.print("hello {any}", .{str});
+
+    var data: ?*anyopaque = undefined;
+
+    var result: c.napi_value = undefined;
+    if (c.napi_create_buffer(env, total_size, &data, &result) != c.napi_ok) {
+        return null;
+    }
+    return result;
+}
