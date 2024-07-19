b9f515bde82e69082a664a263c528f23c33cb7c7
Jim de Beer <beerdejim@gmail.com>
2024-07-19 10:04:26 +0200

    fix

--- packages/db/native/query/ctx.zig
+++ packages/db/native/query/ctx.zig
@@ -3,4 +3,8 @@ const std = @import("std");
 const db = @import("../db.zig");
 const results = @import("./results.zig");
 
-pub const QueryCtx = struct { shards: *std.AutoHashMap([5]u8, db.Shard), txn: ?*c.MDB_txn, results: *std.ArrayList(results.Result) };
+pub const QueryCtx = struct {
+    shards: *std.AutoHashMap([5]u8, db.Shard),
+    txn: ?*c.MDB_txn,
+    results: *std.ArrayList(results.Result),
+};
