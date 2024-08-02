376f2d8678843c1e87dcc3ace99b3b18f4ae3ebe
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-08-02 16:42:03 +0200

    Port SDB io init functions
    ref BASED-98

--- packages/db/clibs/lib/selva/io.h
+++ packages/db/clibs/lib/selva/io.h
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2022-2024 SAULX
+ * SPDX-License-Identifier: MIT
+ */
 #pragma once
 
 #define SELVA_DB_VERSION_SIZE   40
@@ -73,4 +77,8 @@ struct selva_io {
 };
 #endif
 
+int selva_io_init_file(struct selva_io *io, const char *filename, enum selva_io_flags flags);
+struct selva_string *selva_io_init_string_write(struct selva_io *io, enum selva_io_flags flags);
+int selva_io_init_string_read(struct selva_io * restrict io, struct selva_string * restrict s, enum selva_io_flags flags);
+void selva_io_end(struct selva_io *io, struct selva_string **filename_out, uint8_t hash_out[restrict SELVA_IO_HASH_SIZE]);
 int io_dump_save_async(struct SelvaDb *db, const char *filename);
