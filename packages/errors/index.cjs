3900fe53d93b1c7cb071024723c12c93d9fa1a77
Nuno Frade <nfrade.tribal@gmail.com>
2024-08-06 19:27:10 +0200

    Don't use default workaround

--- packages/errors/index.cjs
+++
@@ -1,8 +0,0 @@
-// this file is just here for cjs support for node.js
-const lib = require('./src/index.js')
-
-// this makes it that you don't have to do:
-// `const based = require('@based/errors').default`
-// but can do:
-// `const based = require('@based/errors')`
-module.exports = Object.assign(lib.default, lib)
