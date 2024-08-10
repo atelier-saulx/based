726726e77f5cb7fb48125abb04ec40e01c46670f
Jim de Beer <beerdejim@gmail.com>
2024-08-10 08:46:49 +0200

    better results

--- packages/db/native/query/results.zig
+++ packages/db/native/query/results.zig
@@ -5,9 +5,23 @@ const std = @import("std");
 const db = @import("../db/db.zig");
 const QueryCtx = @import("./ctx.zig").QueryCtx;
 
-pub const Result = struct { id: ?u32, field: u8, val: ?[]u8, start: ?u16, includeMain: []u8, refLvl: u8 };
+pub const Result = struct {
+    id: ?u32,
+    field: u8,
+    val: ?[]u8,
+    start: ?u16,
+    includeMain: []u8,
+    refLvl: u8,
+};
 
-pub fn createResultsBuffer(ctx: QueryCtx, env: c.napi_env, total_size: usize, total_results: usize) !c.napi_value {
+const MAX_REF = 65025;
+
+pub fn createResultsBuffer(
+    ctx: QueryCtx,
+    env: c.napi_env,
+    total_size: usize,
+    total_results: usize,
+) !c.napi_value {
     var resultBuffer: ?*anyopaque = undefined;
     var result: c.napi_value = undefined;
     if (c.napi_create_buffer(env, total_size + 4, &resultBuffer, &result) != c.napi_ok) {
@@ -16,54 +30,38 @@ pub fn createResultsBuffer(ctx: QueryCtx, env: c.napi_env, total_size: usize, to
 
     var data = @as([*]u8, @ptrCast(resultBuffer));
 
-    std.mem.writeInt(
-        u32,
-        data[0..][0..4],
-        @truncate(total_results),
-        .little,
-    );
+    std.mem.writeInt(u32, data[0..][0..4], @truncate(total_results), .little);
 
-    var lastPos: usize = 4;
-    var lastSingleRef: [2]u8 = .{ 255, 255 };
+    // Max is 65025
+    var lastRef: u16 = MAX_REF;
     var lastRefLvl: u8 = 0;
+    var i: usize = 4;
 
     for (ctx.results.items) |*item| {
         if (item.start != null) {
-            const start: [2]u8 = @bitCast(item.start.?);
-
-            if (lastSingleRef[0] != start[0] or lastSingleRef[1] != start[1] or lastRefLvl != item.refLvl) {
-                lastSingleRef = start;
+            if (lastRef != item.start.? or lastRefLvl != item.refLvl) {
+                lastRef = item.start.?;
                 lastRefLvl = item.refLvl;
-
-                data[lastPos] = 254;
-                lastPos += 1;
-
+                data[i] = 254;
+                i += 1;
                 if (item.refLvl > 1) {
-                    data[lastPos] = 1;
+                    data[i] = 1;
                 } else {
-                    data[lastPos] = 0;
+                    data[i] = 0;
                 }
-
-                lastPos += 1;
-
-                data[lastPos] = lastSingleRef[0];
-                lastPos += 1;
-                data[lastPos] = lastSingleRef[1];
-                lastPos += 1;
-
-                @memcpy(data[lastPos .. lastPos + 4], @as([*]u8, @ptrCast(&item.id)));
-
-                lastPos += 4;
+                i += 1;
+                std.mem.writeInt(u16, data[i..][0..2], lastRef, .little);
+                i += 2;
+                @memcpy(data[i .. i + 4], @as([*]u8, @ptrCast(&item.id)));
+                i += 4;
             }
         } else {
-            lastSingleRef[0] = 255;
-            lastSingleRef[1] = 255;
-
+            lastRef = MAX_REF;
             if (item.id != null) {
-                data[lastPos] = 255;
-                lastPos += 1;
-                @memcpy(data[lastPos .. lastPos + 4], @as([*]u8, @ptrCast(&item.id)));
-                lastPos += 4;
+                data[i] = 255;
+                i += 1;
+                @memcpy(data[i .. i + 4], @as([*]u8, @ptrCast(&item.id)));
+                i += 4;
             }
         }
 
@@ -71,41 +69,46 @@ pub fn createResultsBuffer(ctx: QueryCtx, env: c.napi_env, total_size: usize, to
             continue;
         }
 
-        @memcpy(data[lastPos .. lastPos + 1], @as([*]u8, @ptrCast(&item.field)));
-        lastPos += 1;
+        @memcpy(data[i .. i + 1], @as([*]u8, @ptrCast(&item.field)));
+        i += 1;
+
+        if (item.val == null) {
+            continue;
+        }
+
+        const val = item.val.?;
 
         if (item.field == 0) {
             if (item.includeMain.len != 0) {
                 var mainPos: usize = 2;
-                var mainU8 = item.val.?;
                 while (mainPos < item.includeMain.len) {
                     const operation = item.includeMain[mainPos..];
                     const start: u16 = std.mem.readInt(u16, operation[0..2], .little);
                     const len: u16 = std.mem.readInt(u16, operation[2..4], .little);
-                    @memcpy(data[lastPos .. lastPos + len], mainU8[start .. start + len]);
-                    lastPos += len;
+                    @memcpy(data[i .. i + len], val[start .. start + len]);
+                    i += len;
                     mainPos += 4;
                 }
             } else {
                 @memcpy(
-                    data[lastPos .. lastPos + item.val.?.len],
-                    item.val.?[0..item.val.?.len],
+                    data[i .. i + val.len],
+                    item.val.?[0..val.len],
                 );
-                lastPos += item.val.?.len;
+                i += val.len;
             }
         } else {
             std.mem.writeInt(
                 u16,
-                data[lastPos..][0..2],
-                @as(u16, @truncate(item.val.?.len)),
+                data[i..][0..2],
+                @as(u16, @truncate(val.len)),
                 .little,
             );
-            lastPos += 2;
+            i += 2;
             @memcpy(
-                data[lastPos .. lastPos + item.val.?.len],
-                item.val.?[0..item.val.?.len],
+                data[i .. i + val.len],
+                val[0..val.len],
             );
-            lastPos += item.val.?.len;
+            i += val.len;
         }
     }
 
