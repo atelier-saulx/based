b63bc3bea4e3e6ff689c30018ae08e2c1c752b4b
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-09-06 13:39:16 +0200

    Add sort

---
+++ packages/db/clibs/lib/selva/sort.c
@@ -0,0 +1,287 @@
+/*
+ * Copyright (c) 2022-2024 SAULX
+ * SPDX-License-Identifier: MIT
+ */
+#include <assert.h>
+#include <string.h>
+#include <sys/types.h>
+#include "jemalloc.h"
+#include "util/funmap.h"
+#include "util/selva_lang.h"
+#include "util/svector.h"
+#include "selva/fields.h"
+#include "selva_error.h"
+#include "selva/sort.h"
+
+#define EN_ASSERT 0
+
+struct SelvaSortCtx {
+    struct SVector out;
+    struct SVectorIterator it;
+    enum SelvaSortOrder order;
+    char lang[8]; /* TODO */
+};
+
+struct SelvaSortItem {
+    const void *p;
+    size_t data_len;
+    union {
+        int64_t i64;
+        double d;
+    };
+    char data[];
+};
+
+typedef int (*orderFunc)(const void ** restrict a_raw, const void ** restrict b_raw);
+
+static int selva_sort_cmp_none(const void ** restrict a_raw __unused, const void ** restrict b_raw __unused)
+{
+    const struct SelvaSortItem *a = *(const struct SelvaSortItem **)a_raw;
+    const struct SelvaSortItem *b = *(const struct SelvaSortItem **)b_raw;
+    ptrdiff_t x = (ptrdiff_t)a->p;
+    ptrdiff_t y = (ptrdiff_t)b->p;
+
+    return x < y ? -1 : x > y ? 1 : 0;
+}
+
+static int selva_sort_cmp_asc_i64(const void ** restrict a_raw, const void ** restrict b_raw)
+{
+    const struct SelvaSortItem *a = *(const struct SelvaSortItem **)a_raw;
+    const struct SelvaSortItem *b = *(const struct SelvaSortItem **)b_raw;
+    int64_t x = a->i64;
+    int64_t y = b->i64;
+
+    return x < y ? -1 : x > y ? 1 : selva_sort_cmp_none(a_raw, b_raw);
+}
+
+static int selva_sort_cmp_desc_i64(const void ** restrict a_raw, const void ** restrict b_raw)
+{
+    return selva_sort_cmp_asc_i64(b_raw, a_raw);
+}
+
+static int selva_sort_cmp_asc_d(const void ** restrict a_raw, const void ** restrict b_raw)
+{
+    const struct SelvaSortItem *a = *(const struct SelvaSortItem **)a_raw;
+    const struct SelvaSortItem *b = *(const struct SelvaSortItem **)b_raw;
+    double x = a->d;
+    double y = b->d;
+
+    return x < y ? -1 : x > y ? 1 : selva_sort_cmp_none(a_raw, b_raw);
+}
+
+static int selva_sort_cmp_desc_d(const void ** restrict a_raw, const void ** restrict b_raw)
+{
+    return selva_sort_cmp_asc_d(b_raw, a_raw);
+}
+
+static int selva_sort_cmp_asc_buffer(const void ** restrict a_raw, const void ** restrict b_raw)
+{
+    const struct SelvaSortItem *a = *(const struct SelvaSortItem **)a_raw;
+    const struct SelvaSortItem *b = *(const struct SelvaSortItem **)b_raw;
+    int r;
+
+    r = memcmp(a->data, b->data, min(a->data_len, b->data_len));
+    if (r != 0) {
+        return r;
+    } else {
+        size_t x = a->data_len;
+        size_t y = b->data_len;
+
+        return x < y ? -1 : x > y ? 1 : selva_sort_cmp_none(a_raw, b_raw);
+    }
+}
+
+static int selva_sort_cmp_desc_buffer(const void ** restrict a_raw, const void ** restrict b_raw)
+{
+    return selva_sort_cmp_asc_buffer(b_raw, a_raw);
+}
+
+static int selva_sort_cmp_asc_text(const void ** restrict a_raw, const void ** restrict b_raw)
+{
+    const struct SelvaSortItem *a = *(const struct SelvaSortItem **)a_raw;
+    const struct SelvaSortItem *b = *(const struct SelvaSortItem **)b_raw;
+    const char *a_str = a->data;
+    const char *b_str = b->data;
+    int res = 0;
+
+    res = strcmp(a_str, b_str);
+
+    return (res != 0) ? res : selva_sort_cmp_none(a_raw, b_raw);
+}
+
+static int selva_sort_cmp_desc_text(const void ** restrict a_raw, const void ** restrict b_raw)
+{
+    return selva_sort_cmp_asc_text(b_raw, a_raw);
+}
+
+static orderFunc order_functions[] = {
+    [SELVA_SORT_ORDER_NONE] = selva_sort_cmp_none,
+    [SELVA_SORT_ORDER_I64_ASC] = selva_sort_cmp_asc_i64,
+    [SELVA_SORT_ORDER_I64_DESC] = selva_sort_cmp_desc_i64,
+    [SELVA_SORT_ORDER_DOUBLE_ASC] = selva_sort_cmp_asc_d,
+    [SELVA_SORT_ORDER_DOUBLE_DESC] = selva_sort_cmp_desc_d,
+    [SELVA_SORT_ORDER_BUFFER_ASC] = selva_sort_cmp_asc_buffer,
+    [SELVA_SORT_ORDER_BUFFER_DESC] = selva_sort_cmp_desc_buffer,
+    [SELVA_SORT_ORDER_TEXT_ASC] = selva_sort_cmp_asc_text,
+    [SELVA_SORT_ORDER_TEXT_DESC] = selva_sort_cmp_desc_text,
+};
+
+GENERATE_STATIC_FUNMAP(get_cmp_fun, order_functions, enum SelvaSortOrder, SELVA_SORT_ORDER_NONE);
+
+struct SelvaSortCtx *selva_sort_init(enum SelvaSortOrder order, size_t initial_len)
+{
+    struct SelvaSortCtx *ctx = selva_malloc(sizeof(*ctx));
+
+    SVector_Init(&ctx->out, initial_len, get_cmp_fun(order));
+    memset(ctx->lang, 0, sizeof(ctx->lang));
+
+    return ctx;
+}
+
+void selva_sort_destroy(struct SelvaSortCtx *ctx)
+{
+    struct SVectorIterator it;
+    struct SelvaSortItem *item;
+
+    SVector_ForeachBegin(&it, &ctx->out);
+    while ((item = SVector_Foreach(&it))) {
+        selva_free(item);
+    }
+
+    SVector_Destroy(&ctx->out);
+    selva_free(ctx);
+}
+
+static struct SelvaSortItem *create_item_empty(const void *p)
+{
+    struct SelvaSortItem *item = selva_calloc(1, sizeof(*item));
+
+    item->p = p;
+
+    return item;
+}
+
+static struct SelvaSortItem *create_item_i64(int64_t v, const void *p)
+{
+    struct SelvaSortItem *item = selva_calloc(1, sizeof(*item));
+
+    item->i64 = v;
+    item->p = p;
+
+    return item;
+}
+
+static struct SelvaSortItem *create_item_d(double d, const void *p)
+{
+    struct SelvaSortItem *item = selva_calloc(1, sizeof(*item));
+
+    item->d = d;
+    item->p = p;
+
+    return item;
+}
+
+static struct SelvaSortItem *create_item_buffer(const void *buf, size_t len, const void *p)
+{
+    struct SelvaSortItem *item = selva_malloc(sizeof_wflex(struct SelvaSortItem, data, len));
+
+    item->data_len = len;
+    memcpy(item->data, buf, len);
+    item->p = p;
+
+    return item;
+}
+
+static size_t calc_final_data_len(const char *data_lang, const char *data, size_t data_len, locale_t *locale_p) {
+    size_t final_data_len = data_len;
+    locale_t locale = 0;
+
+    if (data_len > 0) {
+        /* FIXME locales */
+#if 0
+        locale = selva_lang_getlocale(selva_langs, data_lang, strlen(data_lang));
+#endif
+        final_data_len = strxfrm_l(NULL, data, 0, locale);
+    }
+
+    *locale_p = locale;
+    return final_data_len;
+}
+
+static struct SelvaSortItem *create_item_text(const char *lang, const char *str, size_t len, const void *p)
+{
+    locale_t locale = 0;
+    size_t data_len = calc_final_data_len(lang, str, len, &locale);
+    struct SelvaSortItem *item = selva_malloc(sizeof_wflex(struct SelvaSortItem, data, data_len + 1));
+
+    item->data_len = data_len;
+    if (len > 0) {
+        strxfrm_l(item->data, str, len, locale);
+    }
+    item->p = p;
+
+    return item;
+}
+
+void selva_sort_insert(struct SelvaSortCtx *ctx, const void *p)
+{
+#if EN_ASSERT
+    assert(ctx->order == SELVA_SORT_ORDER_NONE);
+#endif
+
+    (void)SVector_Insert(&ctx->out, create_item_empty(p));
+}
+
+void selva_sort_insert_i64(struct SelvaSortCtx *ctx, int64_t v, const void *p)
+{
+#if EN_ASSERT
+    assert(ctx->order == SELVA_SORT_ORDER_I64_ASC || ctx->order == SELVA_SORT_ORDER_I64_DESC);
+#endif
+
+    (void)SVector_Insert(&ctx->out, create_item_i64(v, p));
+}
+
+void selva_sort_insert_double(struct SelvaSortCtx *ctx, double d, const void *p)
+{
+#if EN_ASSERT
+    assert(ctx->order == SELVA_SORT_ORDER_DOUBLE_ASC || ctx->order SELVA_SORT_ORDER_DOUBLE_DESC);
+#endif
+
+    (void)SVector_Insert(&ctx->out, create_item_d(d, p));
+}
+
+void selva_sort_insert_buf(struct SelvaSortCtx *ctx, const void *buf, size_t len, const void *p)
+{
+#if EN_ASSERT
+    assert(ctx->order == SELVA_SORT_ORDER_BUFFER_ASC || ctx->order SELVA_SORT_ORDER_BUFFER_DESC);
+#endif
+
+    (void)SVector_Insert(&ctx->out, create_item_buffer(buf, len, p));
+}
+
+void selva_sort_insert_text(struct SelvaSortCtx *ctx, const char *str, size_t len, const void *p)
+{
+#if EN_ASSERT
+    assert(ctx->order == SELVA_SORT_ORDER_TEXT_ASC || ctx->order SELVA_SORT_ORDER_TEXT_DESC);
+#endif
+
+    (void)SVector_Insert(&ctx->out, create_item_text(ctx->lang, str, len, p));
+}
+
+void selva_sort_foreach_begin(struct SelvaSortCtx *ctx)
+{
+    SVector_ForeachBegin(&ctx->it, &ctx->out);
+}
+
+const void *selva_sort_foreach(struct SelvaSortCtx *ctx)
+{
+
+    struct SelvaSortItem *item = SVector_Foreach(&ctx->it);
+
+    return item->p;
+}
+
+bool selva_sort_foreach_done(const struct SelvaSortCtx *ctx)
+{
+    return SVector_Done(&ctx->it);
+}
