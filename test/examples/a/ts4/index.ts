35b3af7a7c754bb53f3458e74258005f7b53e443
Luigui Delyer <git@s1x.com.br>
2024-09-10 13:58:19 +0200

    Tweking examples

--- test/examples/a/ts4/index.ts
+++ test/examples/a/ts4/index.ts
@@ -2,18 +2,16 @@ import { BasedAppFunction } from '@based/functions'
 
 export const app: BasedAppFunction = async (
   _based,
-  { css, js, favicon },
+  { html, favicon },
   _ctx,
 ) => {
   return `<html lang="en">
   <head>
     <meta charset="utf-8" />
     <link rel="shortcut icon" href="${favicon.url}"/>
-    <link rel="stylesheet" href="${css.url}" />
   </head>
   <body>
-    <div id="root"></div>
-    <script>${await js.text}</script>
+    <div id="root">${await html.text}</div>
   </body>
 </html>`
 
