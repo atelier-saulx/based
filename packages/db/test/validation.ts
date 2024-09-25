eec528b13a1db7ff8e6954370b0e191a8aabbfe3
youzi <youri@saulx.com>
2024-09-25 19:08:04 +0200

    make it nice with awaitable response

---
+++ packages/db/test/validation.ts
@@ -0,0 +1,28 @@
+import { BasedDb } from '../src/index.js'
+import test from './shared/test.js'
+
+await test('update', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+
+  await db.start({ clean: true })
+
+  t.after(() => db.destroy())
+
+  db.updateSchema({
+    types: {
+      user: {
+        props: {
+          name: { type: 'string' },
+        },
+      },
+    },
+  })
+
+  const user = await db.create('user', {
+    name: 'success',
+  })
+  console.log(+user)
+  console.log('!!!', await user, +user, `${user}`)
+})
