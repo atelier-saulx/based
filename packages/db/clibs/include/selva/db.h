3967442ec17f969e510314dcd972a3a5d60beff7
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-10-23 15:19:39 +0200

    Make more const

--- packages/db/clibs/include/selva/db.h
+++ packages/db/clibs/include/selva/db.h
@@ -296,10 +296,10 @@ SELVA_EXPORT
 const struct SelvaAlias *selva_get_alias_by_dest(struct SelvaAliases *aliases, node_id_t dest);
 
 SELVA_EXPORT
-const struct SelvaAlias *selva_get_next_alias(struct SelvaAlias *alias);
+const struct SelvaAlias *selva_get_next_alias(const struct SelvaAlias *alias);
 
 SELVA_EXPORT
-const char *selva_get_alias_name(struct SelvaAlias *alias, size_t *len) __attribute__((nonnull(1), pure));
+const char *selva_get_alias_name(const struct SelvaAlias *alias, size_t *len) __attribute__((nonnull(1), pure));
 
 SELVA_EXPORT
 struct SelvaAliases *selva_get_aliases(struct SelvaTypeEntry *type, field_t field);
