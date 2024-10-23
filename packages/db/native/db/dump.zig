4b088572356e58feaa666fd414d0c2f611252ad2
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-10-23 18:17:29 +0200

    Expose save/load range to js

--- packages/db/native/db/dump.zig
+++ packages/db/native/db/dump.zig
@@ -34,23 +34,60 @@ pub fn isReady(napi_env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.
     } else if (rc == selva.SELVA_EINPROGRESS) {
         _ = c.napi_get_boolean(napi_env, false, &res);
     } else {
-        napi.jsThrow(napi_env, "dump failed"); // TODO pass better error
+        napi.jsThrow(napi_env, selva.strerror(rc));
     }
     return res;
 }
 
-pub fn save_common(filename: [:0]u8) !void {
-    try errors.selva(selva.selva_dump_save_common(db.ctx.selva, filename.ptr));
+pub fn save_common(napi_env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.napi_value {
+    const args = napi.getArgs(1, napi_env, info) catch return null;
+    const sdb_filename = napi.get([]u8, napi_env, args[0]) catch return null;
+    var res: c.napi_value = null;
+
+    const rc = selva.selva_dump_save_common(db.ctx.selva, sdb_filename.ptr);
+    _ = c.napi_create_int32(napi_env, rc, &res);
+
+    return res;
 }
 
-pub fn save_range(te: db.Type, filename: [:0]u8, start: u32, end: u32) !void {
-    try errors.selva(selva.selva_dump_save_range(db.ctx.selva, te, filename.ptr, start, end));
+pub fn save_range(napi_env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.napi_value {
+    const args = napi.getArgs(3, napi_env, info) catch return null;
+    const sdb_filename = napi.get([]u8, napi_env, args[0]) catch return null;
+    const typeCode = napi.get(u16, napi_env, args[1]) catch return null;
+    const start = napi.get(u32, napi_env, args[1]) catch return null;
+    const end = napi.get(u32, napi_env, args[2]) catch return null;
+    var res: c.napi_value = null;
+
+    const te = selva.selva_get_type_by_index(db.ctx.selva, typeCode);
+    if (te == null) {
+        _ = c.napi_create_int32(napi_env, selva.SELVA_ENOENT, &res);
+        return res;
+    }
+
+    const rc = selva.selva_dump_save_range(db.ctx.selva, te, sdb_filename.ptr, start, end);
+    _ = c.napi_create_int32(napi_env, rc, &res);
+
+    return res;
 }
 
-pub fn load_common(filename: [:0]u8) !void {
-    try errors.selva(selva.selva_dump_load_common(db.ctx.selva, filename.ptr));
+pub fn load_common(napi_env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.napi_value {
+    const args = napi.getArgs(1, napi_env, info) catch return null;
+    const sdb_filename = napi.get([]u8, napi_env, args[0]) catch return null;
+    var res: c.napi_value = null;
+
+    const rc = selva.selva_dump_load_common(db.ctx.selva, sdb_filename.ptr);
+    _ = c.napi_create_int32(napi_env, rc, &res);
+
+    return res;
 }
 
-pub fn load_range(filename: [:0]u8) !void {
-    try errors.selva(selva.selva_dump_load_range(db.ctx.selva, filename.ptr));
+pub fn load_range(napi_env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.napi_value {
+    const args = napi.getArgs(1, napi_env, info) catch return null;
+    const sdb_filename = napi.get([]u8, napi_env, args[0]) catch return null;
+    var res: c.napi_value = null;
+
+    const rc = selva.selva_dump_load_range(db.ctx.selva, sdb_filename.ptr);
+    _ = c.napi_create_int32(napi_env, rc, &res);
+
+    return res;
 }
