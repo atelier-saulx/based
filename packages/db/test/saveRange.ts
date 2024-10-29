52a6ac0271ec92837b0d0f73f30a8fa80a40cc4d
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-10-29 11:09:31 +0100

    Better save range test

--- packages/db/test/saveRange.ts
+++ packages/db/test/saveRange.ts
@@ -2,13 +2,14 @@ import { join as pathJoin } from 'node:path'
 import { copyFile, mkdir, readdir } from 'node:fs/promises'
 import { BasedDb } from '../src/index.js'
 import test from './shared/test.js'
+import { italy } from './shared/examples.js'
 import { deepEqual, equal } from './shared/assert.js'
 
 await test('save simple range', async (t) => {
   const db = new BasedDb({
     path: t.tmp,
   })
-  db.blockSize = 1
+  db.blockSize = 100_000
 
   await db.start({ clean: true })
   t.after(() => {
@@ -23,56 +24,101 @@ await test('save simple range', async (t) => {
           name: { type: 'string' },
           email: { type: 'string' },
           age: { type: 'uint32' },
+          manifesto: { type: 'string' },
         },
       },
     },
   })
 
-  db.create('user', {
-    age: 1337,
-    name: 'mr disaster',
-    email: 'disaster@disaster.co.uk',
-  })
-  db.create('user', {
-    age: 1453,
-    name: 'mr flop',
-    email: 'flop@disaster.co.uk',
-  })
+  //const N = 10_000_000
+  const N = 400_000
+  const slen = 80
+  const xn_len = italy.length / slen
+  for (let i = 1; i <= N; i++) {
+    let xn1 = ((i * slen) / slen | 0) % xn_len
+    let xn2 = (xn1 + 1) % xn_len
 
-  db.drain()
+    if (xn1 > xn2) {
+      xn1 ^= xn2;
+      xn2 ^= xn1;
+      xn1 ^= xn2;
+    }
+
+    db.create('user', {
+      age: i,
+      name: 'mr flop ' + i,
+      email: 'abuse@disaster.co.uk',
+      manifesto: italy.substring(xn1 * slen, xn2 * slen),
+    })
+  }
+
+  const res = db.drain()
+  console.error('created all nodes', res)
+  const save_start = performance.now()
   await db.stop()
+  const save_end = performance.now()
+  console.error('save rdy', save_end - save_start)
 
   const ls = await readdir(t.tmp)
-  equal(ls.includes('common.sdb'), true)
-  equal(ls.includes('65281_1_1.sdb'), true)
-  equal(ls.includes('65281_2_2.sdb'), true)
+  equal(ls.length, N / 100_000 + 4)
+  deepEqual(ls,
+  [
+    '65281_100001_200000.sdb',
+    '65281_1_100000.sdb',
+    '65281_200001_300000.sdb',
+    '65281_300001_400000.sdb',
+    'common.sdb',
+    'data.mdb',
+    'schema.json',
+    'writelog.json'
+  ])
 
-  //db.start()
+  const load_start = performance.now()
   const newDb = new BasedDb({
     path: t.tmp,
   })
-  db.blockSize = 1
+  db.blockSize = 100_000
   await newDb.start()
   t.after(() => {
     return newDb.destroy()
   })
+  const load_end = performance.now()
+  console.log('load rdy', load_end - load_start)
 
   deepEqual(
     newDb
       .query('user')
       .include('name')
-      .sort('name')
       .range(0, 2)
       .get()
       .toObject(),
     [
       {
         id: 1,
-        name: 'mr disaster',
+        name: 'mr flop 1',
       },
       {
         id: 2,
-        name: 'mr flop',
+        name: 'mr flop 2',
+      },
+    ],
+  )
+
+  deepEqual(
+    newDb
+      .query('user')
+      .include('name')
+      .range(200_000, 2)
+      .get()
+      .toObject(),
+    [
+      {
+        id: 200001,
+        name: 'mr flop 200001',
+      },
+      {
+        id: 200002,
+        name: 'mr flop 200002',
       },
     ],
   )
