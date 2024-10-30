8768cb89ac28d742f2d73cfb5e094aab12375c68
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-10-30 16:09:12 +0100

    Doc improvement

--- packages/db/clibs/include/libdeflate_strings.h
+++ packages/db/clibs/include/libdeflate_strings.h
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2024 SAULX
- *
  * SPDX-License-Identifier: MIT
  */
 #pragma once
@@ -20,17 +19,24 @@ struct libdeflate_block_state;
 struct libdeflate_decompressor;
 
 /**
- * Test if the compressed string in_buf includes the string needle_buf.
+ * Test if the compressed string in_buf is equivalent to the buffer poited by ptr2_buf.
+ * First string deflated and second not
  * @return Same as memcmp().
   */
 LIBDEFLATEEXPORT int
 libdeflate_memcmp(struct libdeflate_decompressor *decompressor, struct libdeflate_block_state *state, const char *in_buf, size_t in_len, const void *ptr2_buf, size_t ptr2_len);
 
+/**
+ * Test if the compressed string in_buf includes the string needle_buf.
+ * First string deflated and second not
+ * @return Same as memmem().
+  */
 LIBDEFLATEEXPORT int64_t
 libdeflate_memmem(struct libdeflate_decompressor *decompressor, struct libdeflate_block_state *state, const char *in_buf, size_t in_len, const void *needle_buf, size_t needle_len);
 
 /**
  * Test if the compressed string in_buf includes the string needle_buf.
+ * Similar to libdeflate_memmem() but theoretically slightly faster execution.
  * @return  <0 error;
  *          0 no match;
  *          1 match found.
