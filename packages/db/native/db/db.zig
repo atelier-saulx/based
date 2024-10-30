113d2f4fd701ba12eb83208d5a0962f9c3626c56
Jim de Beer <beerdejim@gmail.com>
2024-10-30 09:07:49 +0100

    fix

--- packages/db/native/db/db.zig
+++ packages/db/native/db/db.zig
@@ -165,23 +165,16 @@ pub fn writeField(ctx: *DbCtx, data: []u8, node: Node, fieldSchema: FieldSchema)
     ));
 }
 
-// int selva_fields_reference_set(
-//           struct SelvaDb *db,
-//           struct SelvaNode * restrict src,
-//           const struct SelvaFieldSchema *fs_src,
-//           struct SelvaNode * restrict dst,
-//           struct SelvaNodeReference **ref_out)
-
-pub fn writeReference(ctx: *DbCtx, value: Node, target: Node, fieldSchema: FieldSchema) !void {
-    // TODO return the ref on this
-
-    try errors.selva(selva.selva_fields_set(
+pub fn writeReference(ctx: *DbCtx, value: Node, target: Node, fieldSchema: FieldSchema) !?*selva.SelvaNodeReference {
+    var ref: ?*selva.SelvaNodeReference = null;
+    try errors.selva(selva.selva_fields_reference_set(
         ctx.selva,
         target,
         fieldSchema,
         value,
-        8, // ptr len
+        @ptrCast(&ref),
     ));
+    return ref;
 }
 
 pub fn writeReferences(ctx: *DbCtx, value: []Node, target: Node, fieldSchema: FieldSchema) !void {
