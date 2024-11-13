5c9fefcd695273e7eb425f52c5100ad2b6b0e55f
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-11-13 16:16:29 +0100

    Add js_mod()

--- packages/db/clibs/include/util/selva_math.h
+++ packages/db/clibs/include/util/selva_math.h
@@ -1,15 +1,17 @@
 /*
- * Copyright (c) 2022-2023 SAULX
+ * Copyright (c) 2022-2024 SAULX
  * SPDX-License-Identifier: MIT
  */
 #pragma once
 #include <math.h>
 
-static inline double nan_undefined(void) {
+static inline double nan_undefined(void)
+{
     return nan("1");
 }
 
-static inline int isnan_undefined(double x) {
+static inline int isnan_undefined(double x)
+{
     long long i;
 
     if (!isnan(x)) {
@@ -19,4 +21,17 @@ static inline int isnan_undefined(double x) {
     __builtin_memcpy(&i, &x, sizeof(i));
 
     return i & 1;
+}
+
+/**
+ * Mod (%) like it is in JS.
+ */
+static inline double js_fmod(double x, double y)
+{
+    double result = remainder(fabs(x), (y = fabs(y)));
+    if (signbit(result)) {
+        result += y;
+    }
+
+    return copysign(result, x);
 }
