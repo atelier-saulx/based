77c527e3af96220b50a26942cd3de17b76f80119
Jim de Beer <beerdejim@gmail.com>
2024-11-15 15:25:33 +0100

    fix

--- packages/db/test/filter.ts
+++ packages/db/test/filter.ts
@@ -783,24 +783,29 @@ await test('variable size (string/binary)', async (t) => {
 
   equal(len, 1, 'has binary (single')
 
-  const size = 1e7 + 2
-  const largeDerp = new Uint8Array(size)
-  let bytes = 0
-  for (let i = 0; i < size; i++) {
-    largeDerp[i] = bytes
-    bytes++
-    if (bytes > 253) {
-      bytes = 0
-    }
-  }
+  // const size = 1e7 + 2
+  // const largeDerp = new Uint8Array(size)
+  // let bytes = 0
+  // for (let i = 0; i < size; i++) {
+  //   largeDerp[i] = bytes
+  //   bytes++
+  //   if (bytes > 253) {
+  //     bytes = 0
+  //   }
+  // }
 
-  const smurpArticle = await db.create('article', {
-    type: 'gossip',
-    code: 'xa',
-    name: 'Smurp',
-    body: 'Derp derp',
-    derp: largeDerp,
-  })
+  const largeDerp = Buffer.from(italy)
+  let smurpArticle
+  for (let i = 0; i < 1e3; i++) {
+    smurpArticle = db.create('article', {
+      type: 'gossip',
+      code: 'xa',
+      name: 'Smurp',
+      body: 'Derp derp',
+      derp: largeDerp,
+    })
+  }
+  await db.drain()
 
   /*
   selva and call uint32_t crc32c(uint32_t crc, void const *buf, size_t len)
@@ -813,19 +818,15 @@ await test('variable size (string/binary)', async (t) => {
   q[250] = 255
   console.log('DERP DERP FLAP')
 
-  db.query('article', smurpArticle)
-    .filter('derp', 'has', new Uint8Array([30, 100, 21, 50]))
-    // .include('id')
+  db.query('article')
+    .filter('derp', 'has', Buffer.from('Italy'))
+    .include('id')
     .get()
-    .inspect(10)
+    .inspect(1)
 
   console.log('2 DERP DERP FLAP')
 
-  db.query('article', smurpArticle)
-    .filter('derp', 'has', q)
-    // .include('id')
-    .get()
-    .inspect(10)
+  db.query('article').filter('derp', 'has', q).include('id').get().inspect(1)
 
   // FIX WITH CRC32 + len (original crc32)
   // add orignal crc32 as last argument after making compression
