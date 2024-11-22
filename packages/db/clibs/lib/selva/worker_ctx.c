eb5b607615415be0bec6bd68123d7fcc71580be7
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-11-22 14:38:20 +0100

    Missing files

---
+++ packages/db/clibs/lib/selva/worker_ctx.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2024 SAULX
+ * SPDX-License-Identifier: MIT
+ */
+#include <stdio.h>
+#include <pthread.h>
+#include "util/selva_string.h"
+#include "selva/worker_ctx.h"
+
+static __thread bool worker_initialized;
+
+void worker_ctx_init()
+{
+    pthread_t x = pthread_self();
+    fprintf(stderr, "tls: %d\n", (int)x);
+
+    selva_string_init_tls();
+    worker_initialized = true;
+}
+
+void worker_ctx_deinit()
+{
+    if (worker_initialized) {
+    }
+
+    selva_string_deinit_tls();
+    worker_initialized = false;
+}
