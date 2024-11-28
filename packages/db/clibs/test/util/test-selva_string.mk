95d0121f8ae1bc7a1177f87578b6a89fda190110
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-11-28 16:31:47 +0100

    Fix tests

--- packages/db/clibs/test/util/test-selva_string.mk
+++ packages/db/clibs/test/util/test-selva_string.mk
@@ -5,7 +5,4 @@
 TEST_SRC += test-selva_string.c
 SRC-selva_string += ../../lib/util/crc32c/crc32c.c
 SRC-selva_string += ../../lib/util/finalizer.c
-SRC-selva_string += ../../lib/util/selva_lang/selva_mbscmp.c
-SRC-selva_string += ../../lib/util/selva_lang/selva_mbsstrstr.c
-SRC-selva_string += ../../lib/util/selva_lang/selva_mbstowc.c
 SRC-selva_string += ../../lib/util/selva_string.c
