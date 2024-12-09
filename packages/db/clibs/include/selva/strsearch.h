0ad397fd81bb1294f27fef59bbb9979f7db7ff6a
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-12-09 21:05:25 +0100

    fix

--- packages/db/clibs/include/selva/strsearch.h
+++ packages/db/clibs/include/selva/strsearch.h
@@ -4,6 +4,7 @@
  */
 #pragma once
 
+#include <stdint.h>
 #if defined(__APPLE__) && __MACH__
 #include <xlocale.h>
 #endif
@@ -13,47 +14,16 @@
 
 #define STRSEARCH_NEEDLE_MAX 39
 
-struct strsearch_needle {
-    int good;
-    char fch;
-    const char *sep;
-    const char *buf;
-    size_t len;
-};
-
 struct strsearch_wneedle {
-    int good;
-    wchar_t fch;
-    const char *sep;
     wchar_t buf[STRSEARCH_NEEDLE_MAX + 1];
     size_t len;
 };
 
 SELVA_EXPORT
-int strsearch_levenshtein_u8(const char * restrict s, size_t m, const char * restrict t, size_t n);
+int strsearch_make_wneedle(struct strsearch_wneedle *wneedle, locale_t loc, wctrans_t trans, const char *needle, size_t needle_len);
 
 SELVA_EXPORT
-int strsearch_init_u8_ctx(struct strsearch_needle *needle, const char *needle_str, size_t needle_len, int good, bool strict_first_char_match);
-
-/**
- * Make needle for strsearch_has_mbs().
- * wneedle is fully allocated by the called.
- * @returns 0 = ok;
- *          SELVA_ENOBUFS = too long needle;
- *          SELVA_EINVAL = needle_len was 0.
- */
-SELVA_EXPORT
-int strsearch_init_mbs_ctx(struct strsearch_wneedle *wneedle, locale_t loc, wctrans_t trans, const char *needle, size_t needle_len, int good, bool strict_first_char_match);
-
-/**
- * Fuzzy substring search.
- * @returns 0 == perfect match, no changes needed to the needle;
- *          0..needle_len - 1 == this many changes needed to make a match;
- *          needle_len == no match/change every character to make a match;
- *          INT_MAX == error or too long needle.
- */
-SELVA_EXPORT
-int strsearch_has_u8(const char *text, size_t text_len, const struct strsearch_needle *needle);
+uint8_t strsearch_levenshtein_u8(const char * restrict s, size_t m, const char * restrict t, size_t n);
 
 SELVA_EXPORT
-int strsearch_has_mbs(locale_t loc, wctrans_t trans, const char *text, size_t text_len, const struct strsearch_wneedle *wneedle);
+uint8_t strsearch_levenshtein_mbs(locale_t loc, wctrans_t trans, const char * restrict s, size_t m, const char * restrict t, size_t n);
--- packages/db/clibs/lib/selva/strsearch.c
+++ packages/db/clibs/lib/selva/strsearch.c
@@ -12,26 +12,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
-#include "jemalloc.h"
 #include "selva/selva_lang.h"
-#include "print_ready.h"
-#include "util/timestamp.h"
 #include "selva_error.h"
 #include "selva/strsearch.h"
 
 #define LEV_MAX (STRSEARCH_NEEDLE_MAX + 1)
 
-static const char *select_separator(const char *needle, size_t len)
+int strsearch_make_wneedle(struct strsearch_wneedle *wneedle, locale_t loc, wctrans_t trans, const char *needle, size_t needle_len)
 {
-    const char * const separators[] = {
-        [0] = " ,.;-\n", /* default */
-        [1] = " ,.;\n", /* contains '-' */
-        [2] = ",.;-\n", /* contains ' ' */
-        [3] = ",.;\n", /* contains '-' and ' ' */
-    };
-    unsigned i = !!memchr(needle, '-', len) | (!!memchr(needle, ' ', len) << 1);
+    mbstate_t ps;
+    size_t i = 0, j = 0;
 
-    return separators[i];
+    if (needle_len == 0) {
+        return SELVA_EINVAL;
+    }
+    if (needle_len > LEV_MAX - 1) {
+        return SELVA_ENOBUFS;
+    }
+
+    memset(&ps, 0, sizeof(ps));
+    while (i < needle_len) {
+        i += selva_mbstowc(wneedle->buf + j++, needle + i, needle_len - i, &ps, trans, loc);
+    }
+    wneedle->buf[j] = '\0';
+    wneedle->len = j;
+
+    return 0;
 }
 
 static int32_t min3(int32_t a, int32_t b, int32_t c)
@@ -39,12 +45,12 @@ static int32_t min3(int32_t a, int32_t b, int32_t c)
     return min(a, min(b, c));
 }
 
-static int32_t levenshtein_u8(const char * restrict s, size_t m, const char * restrict t, size_t n)
+uint8_t strsearch_levenshtein_u8(const char * restrict s, size_t m, const char * restrict t, size_t n)
 {
     if (m == 0) return n;
     if (n == 0) return m;
-    if (/*n > LEV_MAX - 1 || */ m > LEV_MAX - 1) {
-        return INT_MAX;
+    if (n > LEV_MAX - 1 || m > LEV_MAX - 1) {
+        return 255;
     }
 
     int32_t v[2][LEV_MAX];
@@ -65,19 +71,14 @@ static int32_t levenshtein_u8(const char * restrict s, size_t m, const char * re
         v1 = tmp;
     }
 
-    return v0[n];
+    return (uint8_t)v0[n];
 }
 
-int strsearch_levenshtein_u8(const char * restrict s, size_t m, const char * restrict t, size_t n)
-{
-    return (int)levenshtein_u8(s, m, t, n);
-}
-
-static int32_t levenshtein_mbs(locale_t loc, wctrans_t trans, const char * restrict s, size_t m, const wchar_t * restrict t, size_t n)
+uint8_t strsearch_levenshtein_mbs(locale_t loc, wctrans_t trans, const char * restrict s, size_t m, const char * restrict t, size_t n)
 {
     if (m == 0) return n;
-    if (m > LEV_MAX - 1) {
-        return INT_MAX;
+    if (n > LEV_MAX - 1 || m > LEV_MAX - 1) {
+        return 255;
     }
 
     int32_t v[2][LEV_MAX];
@@ -103,254 +104,5 @@ static int32_t levenshtein_mbs(locale_t loc, wctrans_t trans, const char * restr
         i += nbytes1;
     }
 
-    return v0[n];
+    return (uint8_t)v0[n];
 }
-
-/*
- * Copyright (c) 2024 SAULX
- * Copyright (c) 2019 Olli Vanhoja <olli.vanhoja@alumni.helsinki.fi>
- * Copyright (c) 2014, 2015 Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-static const char * strtok2(const char * s, const char * delim, const char ** lasts, size_t left)
-{
-    const char * spanp;
-    const char * tok;
-    uint32_t c, sc;
-
-    /* s may be NULL */
-    if (left == 0 || (!s && !(s = *lasts))) {
-        return NULL;
-    }
-
-    /*
-     * Skip (span) leading delimiters (s += strspn(s, delim), sort of).
-     */
-    left++;
-cont:
-    c = *s++;
-    if (--left == 0) {
-        *lasts = NULL;
-        return NULL;
-    }
-    for (spanp = delim; (sc = *spanp++) != 0;) {
-        if (c == sc) {
-            goto cont;
-        }
-    }
-    tok = s - 1;
-
-    /*
-     * Scan token (scan for delimiters: s += strcspn(s, delim), sort of).
-     * Note that delim must have one NUL; we stop if we see that, too.
-     */
-    while (left > 0) {
-        c = *s++;
-        left--;
-        spanp = delim;
-        do {
-            if ((sc = *spanp++) == c) {
-                goto out;
-            }
-        } while (sc != 0);
-    }
-
-out:
-    *lasts = (left == 0) ? NULL : s;
-    return tok;
-}
-
-int strsearch_init_u8_ctx(struct strsearch_needle *needle, const char *needle_str, size_t needle_len, int good, bool strict_first_char_match)
-{
-    char fch;
-
-    if (needle_len == 0) {
-        return SELVA_EINVAL;
-    }
-    if (needle_len > LEV_MAX - 1) {
-        return SELVA_ENOBUFS;
-    }
-
-    if (strict_first_char_match) {
-        if (isalpha(needle_str[0])) {
-            fch = tolower(needle_str[0]);
-        } else {
-            fch = needle_str[0];
-        }
-    } else {
-        fch = '\0';
-    }
-
-    *needle = (struct strsearch_needle){
-        .good = good,
-        .fch = fch,
-        .sep = select_separator(needle_str, needle_len),
-        .buf = needle_str,
-        .len = needle_len,
-    };
-
-    return 0;
-}
-
-int strsearch_init_mbs_ctx(struct strsearch_wneedle *wneedle, locale_t loc, wctrans_t trans, const char *needle, size_t needle_len, int good, bool strict_first_char_match)
-{
-    mbstate_t ps;
-    size_t i = 0, j = 0;
-
-    if (needle_len == 0) {
-        return SELVA_EINVAL;
-    }
-    if (needle_len > LEV_MAX - 1) {
-        return SELVA_ENOBUFS;
-    }
-
-    memset(&ps, 0, sizeof(ps));
-    while (i < needle_len) {
-        i += selva_mbstowc(wneedle->buf + j++, needle + i, needle_len - i, &ps, trans, loc);
-    }
-    wneedle->buf[j] = '\0';
-    wneedle->len = j;
-
-    wneedle->good = good;
-    wneedle->fch = strict_first_char_match && iswalpha(wneedle->buf[0]) ? wneedle->buf[0] : L'\0';
-    wneedle->sep = select_separator(needle, needle_len);
-
-    return 0;
-}
-
-int strsearch_has_u8(const char *text, size_t text_len, const struct strsearch_needle *needle)
-{
-    const char *sep = needle->sep;
-    const char fch = needle->fch;
-    const int32_t good = needle->good;
-    const char *word;
-    const char *brkt;
-    int32_t d = INT_MAX;
-
-    for (word = strtok2(text, sep, &brkt, text_len);
-         word;
-         word = strtok2(NULL, sep, &brkt, text_len - (brkt - text))) {
-        size_t len = (brkt) ? brkt - word - 1 : strlen(word);
-
-        if (fch != '\0' && tolower(word[0]) != fch) {
-            continue;
-        }
-
-        int32_t d2 = levenshtein_u8(word, len, needle->buf, needle->len);
-        d = min(d, d2);
-        if (d <= good) {
-            break;
-        }
-    }
-
-    return d;
-}
-
-int strsearch_has_mbs(locale_t loc, wctrans_t trans, const char *text, size_t text_len, const struct strsearch_wneedle *wneedle)
-{
-    const char *sep = wneedle->sep;
-    const char fch = wneedle->fch;
-    const int32_t good = wneedle->good;
-    const char *word;
-    const char *brkt;
-    int32_t d = INT_MAX;
-
-    for (word = strtok2(text, sep, &brkt, text_len);
-         word;
-         word = strtok2(NULL, sep, &brkt, text_len - (brkt - text))) {
-        size_t len = (brkt) ? brkt - word - 1 : strlen(word);
-
-        if (fch != L'\0') {
-            wchar_t wc;
-            mbstate_t ps;
-
-            memset(&ps, 0, sizeof(ps));
-            (void)selva_mbstowc(&wc, word, len, &ps, trans, loc);
-            if (wc != fch) {
-                continue;
-            }
-        }
-
-        int32_t d2 = levenshtein_mbs(loc, trans, word, len, wneedle->buf, wneedle->len);
-        d = min(d, d2);
-        if (d <= good) {
-            break;
-        }
-    }
-
-    return d;
-}
-
-#if 0
-#define TEST_U8 0
-#include <stdio.h>
-__constructor static void test(void)
-{
-    locale_t loc = newlocale(LC_ALL_MASK, "fi_FI", 0);
-    wctrans_t trans = wctrans_l("tolower", loc);
-    FILE *fp = fopen("test/shared/bible.txt", "r");
-    long fsize;
-
-    if (!fp) {
-        abort();
-    }
-    fseek(fp, 0, SEEK_END);
-    fsize = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-    char *book = malloc(fsize + 1);
-    fread(book, fsize, 1, fp);
-    fclose(fp);
-    book[fsize] = 0;
-#if 0
-    const char book[] = "Not a very long book really good tho";
-    fsize = strlen(book);
-#endif
-
-    static const char * const patterns[] = { "fod", "god", "G0d", "good", "GOD", "göd", "thuogh", "eegrergerg", "{{{{{{{{" };
-    struct timespec ts_start, ts_end;
-
-    ts_monotime(&ts_start);
-    for (size_t i = 0; i < num_elem(patterns); i++) {
-        const char *pattern = patterns[i];
-        size_t len = strlen(pattern);
-
-        fprintf(stderr, "pattern: %s | ", pattern);
-#if TEST_U8 == 1
-        struct strsearch_needle needle;
-        (void)strsearch_init_u8_ctx(&needle, pattern, len, 1, true);
-        fprintf(stderr, "%d\n", strsearch_has_u8(book, fsize, &needle));
-#else
-        struct strsearch_wneedle wneedle;
-        (void)strsearch_init_mbs_ctx(&wneedle, loc, trans, pattern, len, 1, true);
-        fprintf(stderr, "%d\n", strsearch_has_mbs(0, trans, book, fsize, &wneedle));
-#endif
-    }
-    ts_monotime(&ts_end);
-    print_ready("search", &ts_start, &ts_end, "");
-}
-#endif
