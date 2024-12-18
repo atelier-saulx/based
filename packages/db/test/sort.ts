c45a6db8c2807a4a1fe86b688f2e34538f055be9
Jim de Beer <beerdejim@gmail.com>
2024-12-18 12:30:28 +0100

    add 1M sort test

--- packages/db/test/sort.ts
+++ packages/db/test/sort.ts
@@ -2,6 +2,82 @@ import { BasedDb } from '../src/index.js'
 import test from './shared/test.js'
 import { deepEqual, equal } from './shared/assert.js'
 
+await test('1M', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+
+  await db.start({ clean: true })
+
+  t.after(() => {
+    return db.destroy()
+  })
+
+  db.putSchema({
+    types: {
+      user: {
+        props: {
+          name: { type: 'string' },
+          email: { type: 'string' },
+          age: { type: 'uint32' },
+        },
+      },
+    },
+  })
+
+  for (let i = 0; i < 1e6; i++) {
+    const mrZ = db.create('user', {
+      name: 'mr z',
+      age: 1 + i,
+      email: i + '@z.z',
+    })
+  }
+
+  const dbTime = db.drain()
+  console.log('db modify', dbTime, 'ms')
+  equal(dbTime < 1000, true, 'db modify should not take longer then 1s')
+
+  let d = Date.now()
+  db.server.createSortIndex('user', 'email')
+  let siTime = Date.now() - d
+  console.log('create sort index (string)', siTime, 'ms')
+  equal(
+    siTime < 500,
+    true,
+    'creating string sort index should not take longer then 500ms',
+  )
+
+  const r = await db
+    .query('user')
+    .include('age', 'name', 'email')
+    .range(0, 1e5)
+    .sort('email')
+    .filter('age', '>', 1e6 - 1e5)
+    .get()
+    .then((v) => v.inspect())
+
+  deepEqual(
+    r.node(0),
+    {
+      id: 900001,
+      age: 900001,
+      name: 'mr z',
+      email: '900000@z.z',
+    },
+    'first node is correct',
+  )
+
+  d = Date.now()
+  db.server.createSortIndex('user', 'age')
+  siTime = Date.now() - d
+  console.log('create sort index (uint32)', siTime, 'ms')
+  equal(
+    siTime < 250,
+    true,
+    'creating string sort index should not take longer then 250s',
+  )
+})
+
 await test('basic', async (t) => {
   const db = new BasedDb({
     path: t.tmp,
