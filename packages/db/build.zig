de7e7d6be12d61cb83c7b9c3163041e4cbcbc95f
Marco Vieira <maovieira@gmail.com>
2025-01-17 18:32:46 -0300

    temp zig building

--- packages/db/build.zig
+++ packages/db/build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    const node_versions = [_][]const u8{ "v20.11.1", "v22.13.0" };
+    const node_versions = [_][]const u8{ "v20.11.1", "v20.18.1", "v22.13.0" };
 
     const target = b.standardTargetOptions(.{});
 
@@ -64,7 +64,7 @@ pub fn build(b: *std.Build) !void {
         // Yes! that is uggly as hell but apparently zig expects that .dest_sub_path to be a pre-generated static string.
         // Since there is no better solution for that, we will just hardcode the values here.
         // TODO this code will be ported to TS to avoid this kind of problem.
-        const dest_sub_path = if (std.mem.eql(u8, version, "v20.11.1")) "lib_node-v20.11.1.node" else if (std.mem.eql(u8, version, "v22.13.0")) "lib_node-v22.13.0.node" else return error.InvalidNodeVersion;
+        const dest_sub_path = if (std.mem.eql(u8, version, "v20.11.1")) "lib.node-v20.11.1" else if (std.mem.eql(u8, version, "v20.18.1")) "lib.node-v20.18.1" else if (std.mem.eql(u8, version, "v22.13.0")) "lib.node-v22.13.0" else return error.InvalidNodeVersion;
 
         std.debug.print("Building {s}...\n", .{dest_sub_path});
 
@@ -76,7 +76,40 @@ pub fn build(b: *std.Build) !void {
         });
 
         b.getInstallStep().dependOn(&install_lib.step);
-        b.installArtifact(lib);
     }
+
+    // another ugly stuff here, but we need to do that because the dynamic lib is created in zig-out anyway.
+    var lib_suffix: []const u8 = undefined;
+    var lib_prefix: []const u8 = undefined;
+    var arch: []const u8 = undefined;
+    if (target.result.os.tag == .linux) {
+        lib_suffix = ".so";
+        lib_prefix = "linux_";
+    } else if (target.result.os.tag == .macos) {
+        lib_suffix = ".dylib";
+        lib_prefix = "darwin_";
+    } else {
+        return error.UnsupportedOs;
+    }
+    // we are not following the system notation so .aarch64 will be arm64 why K.I.S.S.?
+    if (target.result.cpu.arch == .aarch64) {
+        arch = "arm64";
+    } else {
+        arch = @tagName(target.result.cpu.arch);
+    }
+    const resolved_source_path = try std.fmt.allocPrint(allocator, "zig-out/lib/libbased-db-zig{s}", .{lib_suffix});
+    const resolved_install_dir = try std.fmt.allocPrint(allocator, "dist/lib/{s}{s}/", .{ lib_prefix, arch });
+
+    const install_step = b.addSystemCommand(&[_][]const u8{
+        "mv",
+        resolved_source_path,
+        resolved_install_dir,
+    });
+    b.getInstallStep().dependOn(&install_step.step);
+
+    defer allocator.free(resolved_source_path);
+    defer allocator.free(resolved_install_dir);
+
+    b.installArtifact(lib);
     std.debug.print("Done.\n", .{});
 }
