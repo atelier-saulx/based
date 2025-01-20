0fdc1bc62be23eea0c8f672ad6c6d347b38d073b
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-01-20 18:40:42 +0100

    Remove old scripts

--- scripts/build-c.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Copyright (c) 2023 SAULX
-# SPDX-License-Identifier: MIT
-
-rm -rf packages/server/selvad/local/lib packages/server/selvad/local/modules packages/server/selvad/local/selvad
-cd selvad
-make
-INSTALL_DIR=../packages/server/selvad/local make install
