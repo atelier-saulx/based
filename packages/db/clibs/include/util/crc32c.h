cbe8d50209375f8a751119c87bda4ca6e15e693d
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-01-27 10:41:49 +0100

    Fix export

--- packages/db/clibs/include/util/crc32c.h
+++ packages/db/clibs/include/util/crc32c.h
@@ -1,15 +1,15 @@
 /*
+ * Copyright (c) 2025 SAULX
  * Copyright (C) 2013, 2021 Mark Adler <madler@alumni.caltech.edu>
  * SPDX-License-Identifier: Zlib
  */
 #pragma once
-#ifndef CRC32C_H
-#define CRC32C_H
+
+#include "selva/_export.h"
 
 /**
  * Compute CRC-32C.
  */
+SELVA_EXPORT
 uint32_t crc32c(uint32_t crc, void const *buf, size_t len)
     __attribute__((pure, access(read_only, 2, 3)));
-
-#endif /* CRC32C_H */
