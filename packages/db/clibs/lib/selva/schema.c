b9134d11410c0166f988e0db45970b1c6bcfd490
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-01-28 10:47:26 +0100

    Make code more clear

--- packages/db/clibs/lib/selva/schema.c
+++ packages/db/clibs/lib/selva/schema.c
@@ -206,7 +206,7 @@ static int type2fs_refs(struct schemabuf_parser_ctx *ctx, struct SelvaFieldsSche
         node_type_t dst_node_type;
         field_t inverse_field;
         uint32_t schema_len;
-        /* uint8_t schema[]; */
+        uint8_t schema[] __counted_by(schema_len);
     } __packed constraints;
 
     static_assert(sizeof(constraints) == 8);
