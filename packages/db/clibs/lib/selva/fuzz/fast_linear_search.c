3d95f01bd6e2111867255c54991aa238d265eaea
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-01-29 07:02:08 +0100

    Remove unused code

--- packages/db/clibs/lib/selva/fuzz/fast_linear_search.c
+++ packages/db/clibs/lib/selva/fuzz/fast_linear_search.c
@@ -16,32 +16,6 @@ static inline uint64_t next(void)
     return (seed = 214013 * seed + 2531011);
 }
 
-ssize_t sch(const uint32_t arr[], size_t len, uint32_t x)
-{
-    size_t mid = len / 2;
-    for (size_t i = 0, j = len - 1, k = mid, l = mid; i <= mid; i++, j--, k--, l++) {
-        if (arr[i] == x) return i;
-        if (arr[j] == x) return j;
-        if (arr[k] == x) return k;
-        if (arr[l] == x) return l;
-    }
-    return -1;
-}
-#define MAKE_FUN(TYPE, NAME) \
-    ssize_t NAME(const TYPE arr[], size_t len, TYPE x) \
-    { \
-        size_t mid = len / 2; \
-        for (size_t i = 0, j = len - 1, k = mid, l = mid; i <= mid; i++, j--, k--, l++) { \
-            if (arr[i] == x) return i; \
-            if (arr[j] == x) return j; \
-            if (arr[k] == x) return k; \
-            if (arr[l] == x) return l; \
-        } \
-        return -1; \
-    }
-
-MAKE_FUN(uint32_t, fast_linear_search_uint32_b)
-
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
     if (Size % sizeof(uint32_t) != 0) {
@@ -53,8 +27,6 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
     uint32_t x = len == 0 ? 0 : next() % len;
 
     (void)fast_linear_search_uint32(arr, len, x);
-    //(void)fast_linear_search_uint32_b(arr, len, x);
-    //(void)sch(arr, len, x);
 
     return 0;
 }
