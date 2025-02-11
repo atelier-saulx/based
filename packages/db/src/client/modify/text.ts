052a3143b6b8eb3499bdd2e685521bdf37f7cf95
Jim de Beer <beerdejim@gmail.com>
2025-02-11 20:46:21 +0100

    fix

--- packages/db/src/client/modify/text.ts
+++ packages/db/src/client/modify/text.ts
@@ -3,22 +3,37 @@ import { ModifyCtx } from '../../index.js'
 import { ModifyOp, ModifyErr } from './types.js'
 import { SchemaTypeDef, PropDef } from '../../server/schema/types.js'
 import { writeString } from './string.js'
+import { ModifyState } from './ModifyRes.js'
 
 export function writeText(
   value: { [k: string]: Parameters<typeof writeString>[1] },
   ctx: ModifyCtx,
   def: SchemaTypeDef,
   t: PropDef,
-  parentId: number,
+  res: ModifyState,
   modifyOp: ModifyOp,
 ): ModifyErr {
-  for (const lang in value) {
-    const langCode: LangCode = langCodesMap.get(lang) || langCodesMap.get('en') // TODO use proper fallback
-    const s = value[lang]
+  // todo proper fallback as well
+  if (value && typeof value !== 'object') {
+    const err = writeString(
+      res.i18n ?? langCodesMap.get('en'),
+      value,
+      ctx,
+      def,
+      t,
+      res.tmpId,
+      modifyOp,
+    )
+  } else {
+    for (const lang in value) {
+      const langCode: LangCode =
+        langCodesMap.get(lang) || langCodesMap.get('en') // TODO use proper fallback
+      const s = value[lang]
 
-    const err = writeString(langCode, s, ctx, def, t, parentId, modifyOp)
-    if (err) {
-      return err
+      const err = writeString(langCode, s, ctx, def, t, res.tmpId, modifyOp)
+      if (err) {
+        return err
+      }
     }
   }
 }
