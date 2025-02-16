25464bf3809d3abbd3c662323268404dd0c70a2d
Marco Vieira <maovieira@gmail.com>
2025-02-16 14:27:35 +0100

    crc32 in JS

---
+++ packages/db/test/crc32.ts
@@ -0,0 +1,75 @@
+import { BasedDb, crc32 } from '../src/index.js'
+import test from './shared/test.js'
+import { equal, deepEqual } from './shared/assert.js'
+
+await test('simple', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+
+  await db.start({ clean: true })
+
+  t.after(() => {
+    return db.destroy()
+  })
+
+  await db.putSchema({
+    types: {
+      transaction: {
+        props: {
+          myHash: 'number',
+        },
+      },
+    },
+  })
+
+  const transaction = await db.create('transaction', {
+    myHash: crc32(Buffer.from('oid123')),
+  })
+
+  equal(
+    await db.query('transaction').include('id', 'myHash').get().toObject(),
+    [
+      {
+        id: 1,
+        myHash: 2628032717,
+      },
+    ],
+  )
+
+  let lastId = 0
+  const m: number[] = []
+  for (let i = 0; i < 1e7; i++) {
+    lastId = db.create('transaction', {
+      myHash: crc32(Buffer.from(`oid${i}`)),
+    }).tmpId
+    if (i % 2) {
+      m.push(lastId)
+    }
+  }
+
+  db.update('transaction', transaction, {
+    myHash: m,
+  })
+
+  await db.drain()
+
+  equal(
+    await db
+      .query('transaction')
+      .include('id', 'myHash')
+      .range(813, 2)
+      .get()
+      .toObject(),
+    [
+      {
+        id: 814,
+        myHash: 3342344172,
+      },
+      {
+        id: 815,
+        myHash: 894672111,
+      },
+    ],
+  )
+})
