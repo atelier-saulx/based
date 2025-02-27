ebba00629fe52e3c65762566031dfedb972b51e9
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-02-27 14:21:59 +0100

    inline schema funcs when possible (in C)
    Zig doesn't like this code so it can't be inlined there.

--- packages/db/clibs/include/selva/db.h
+++ packages/db/clibs/include/selva/db.h
@@ -75,16 +75,44 @@ struct SelvaTypeEntry *selva_get_type_by_node(const struct SelvaDb *db, struct S
  * Get the node schema for type.
  */
 SELVA_EXPORT
-const struct SelvaNodeSchema *selva_get_ns_by_te(const struct SelvaTypeEntry *te) __attribute__((nonnull, pure));
+__attribute__((nonnull, pure))
+inline const struct SelvaNodeSchema *selva_get_ns_by_te(const struct SelvaTypeEntry *te)
+#if !__zig
+{
+    return &te->ns;
+}
+#else
+;
+#endif
 
 SELVA_EXPORT
-const struct SelvaFieldSchema *get_fs_by_fields_schema_field(const struct SelvaFieldsSchema *fields_schema, field_t field) __attribute__((nonnull, pure));
+__attribute__((nonnull, pure))
+inline const struct SelvaFieldSchema *get_fs_by_fields_schema_field(const struct SelvaFieldsSchema *fields_schema, field_t field)
+#if !__zig
+{
+    if (field >= fields_schema->nr_fields) {
+        return nullptr;
+    }
+
+    return &fields_schema->field_schemas[field];
+}
+#else
+;
+#endif
 
 /**
  * Get the field schema for field.
  */
 SELVA_EXPORT
-const struct SelvaFieldSchema *selva_get_fs_by_ns_field(const struct SelvaNodeSchema *ns, field_t field) __attribute__((nonnull, pure));
+__attribute__((nonnull, pure))
+inline const struct SelvaFieldSchema *selva_get_fs_by_ns_field(const struct SelvaNodeSchema *ns, field_t field)
+#if !__zig
+{
+    return get_fs_by_fields_schema_field(&ns->fields_schema, field);
+}
+#else
+;
+#endif
 
 /**
  * Get the field schema for field.
@@ -96,7 +124,14 @@ SELVA_EXPORT
 #if __has_c_attribute(reproducible)
 [[reproducible]]
 #endif
-enum SelvaFieldType selva_get_fs_type(const struct SelvaFieldSchema *fs);
+inline enum SelvaFieldType selva_get_fs_type(const struct SelvaFieldSchema *fs)
+#if !__zig
+{
+    return fs->type;
+}
+#else
+;
+#endif
 
 /**
  * Get the EdgeFieldConstraint from a ref field schema.
--- packages/db/clibs/lib/selva/db.c
+++ packages/db/clibs/lib/selva/db.c
@@ -359,24 +359,11 @@ struct SelvaTypeEntry *selva_get_type_by_node(const struct SelvaDb *db, struct S
     return te;
 }
 
-const struct SelvaNodeSchema *selva_get_ns_by_te(const struct SelvaTypeEntry *te)
-{
-    return &te->ns;
-}
+extern inline const struct SelvaNodeSchema *selva_get_ns_by_te(const struct SelvaTypeEntry *te);
 
-const struct SelvaFieldSchema *get_fs_by_fields_schema_field(const struct SelvaFieldsSchema *fields_schema, field_t field)
-{
-    if (field >= fields_schema->nr_fields) {
-        return nullptr;
-    }
+extern inline const struct SelvaFieldSchema *get_fs_by_fields_schema_field(const struct SelvaFieldsSchema *fields_schema, field_t field);
 
-    return &fields_schema->field_schemas[field];
-}
-
-const struct SelvaFieldSchema *selva_get_fs_by_ns_field(const struct SelvaNodeSchema *ns, field_t field)
-{
-    return get_fs_by_fields_schema_field(&ns->fields_schema, field);
-}
+extern inline const struct SelvaFieldSchema *selva_get_fs_by_ns_field(const struct SelvaNodeSchema *ns, field_t field);
 
 const struct SelvaFieldSchema *selva_get_fs_by_node(struct SelvaDb *db, struct SelvaNode *node, field_t field)
 {
@@ -390,10 +377,7 @@ const struct SelvaFieldSchema *selva_get_fs_by_node(struct SelvaDb *db, struct S
     return selva_get_fs_by_ns_field(&type->ns, field);
 }
 
-enum SelvaFieldType selva_get_fs_type(const struct SelvaFieldSchema *fs)
-{
-    return fs->type;
-}
+extern inline enum SelvaFieldType selva_get_fs_type(const struct SelvaFieldSchema *fs);
 
 const struct EdgeFieldConstraint *selva_get_edge_field_constraint(const struct SelvaFieldSchema *fs)
 {
