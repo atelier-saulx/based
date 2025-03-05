eb46c5f00f72a689c377f530e7229df0d4f61894
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-03-05 17:08:32 +0100

    Add support for high bits ptagging

--- packages/db/clibs/lib/selva/include/ptag.h
+++ packages/db/clibs/lib/selva/include/ptag.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2024 SAULX
+ * Copyright (c) 2021-2025 SAULX
  * SPDX-License-Identifier: MIT
  */
 #pragma once
@@ -9,12 +9,22 @@
 /**
  * @addtogroup ptag
  * Tagged pointers.
- * We can add tags to the lowest bits on pointers because we know that the
- * architecture will never use those bits. The tag bits needs to be
+ * We can add tags to the lowest or highest bits on pointers because we know
+ * that the architecture will never use those bits. The tag bits needs to be
  * removed before using the pointer.
+ * - high: 16-bits on aarch and x64 except with Intel 5-level paging when it becomes 7-bits
+ * - low: 2-bits in any 4-byte aligned pointer
  * @{
  */
 
+/**
+ * Select low or high bits mode.
+ * - 0: low
+ * - 1: high
+ */
+#define PTAG_MODE 0
+
+#if PTAG_MODE == 0
 #define PTAG_MASK 0x03ull
 
 /**
@@ -25,7 +35,7 @@
                   __builtin_types_compatible_p(typeof(tag), int) || \
                   __builtin_types_compatible_p(typeof(tag), unsigned)); \
     ((typeof (value))(((uintptr_t)(value) & ~PTAG_MASK) | ((tag) & PTAG_MASK))); \
-    })
+})
 
 /**
  * Get the tag value from a tagged pointer.
@@ -38,6 +48,29 @@
  */
 #define PTAG_GETP(ptag) \
     (void *)((uintptr_t)(ptag) & ~PTAG_MASK)
+#else
+#define PTAG_MASK 0xFFFF000000000000ull
+#define PTAG_VADDR 48
+
+#define PTAG(value, tag) ({\
+    static_assert(__builtin_types_compatible_p(typeof(tag), short) || \
+                  __builtin_types_compatible_p(typeof(tag), int) || \
+                  __builtin_types_compatible_p(typeof(tag), unsigned)); \
+    ((typeof (value))(((uintptr_t)(value) & ~PTAG_MASK) | ((uintptr_t)(tag) << PTAG_VADDR))); \
+})
+
+#define PTAG_GETTAG(ptag) \
+    ((int)(((uintptr_t)(ptag) & PTAG_MASK) >> PTAG_VADDR))
+
+#ifdef __aarch64__
+#define PTAG_GETP(ptag) \
+    (void *)((uintptr_t)(ptag) & ~PTAG_MASK)
+#else
+#define PTAG_GETP(ptag) \
+    (void *)(((intptr_t)(ptag) << 16) >> 16)
+#endif
+
+#endif
 
 /**
  * @}
