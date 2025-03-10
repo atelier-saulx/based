62a32936ec09aa3087eed230f5116751fb248db4
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-03-10 18:42:51 +0100

    Make playground unmanaged or managed again

--- packages/db/test/shared/playground.ts
+++ packages/db/test/shared/playground.ts
@@ -170,7 +170,7 @@ const runSimple = async (path: string) => {
   const db = new BasedDb({
     path,
   })
-  await db.start({ clean: true, managed: true })
+  await db.start({ clean: true, managed: false })
 
   const profession = ['developer', 'baker', '???']
 
