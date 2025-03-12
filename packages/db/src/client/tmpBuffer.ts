cfc535f05295eec2c943055274b640fbc71fb522
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-03-12 16:25:50 +0100

    Support max len buffer

--- packages/db/src/client/tmpBuffer.ts
+++ packages/db/src/client/tmpBuffer.ts
@@ -3,10 +3,13 @@ export default function makeTmpBuffer(initialSize: number) {
 
   return {
     getUint8Array: (size: number): Uint8Array => {
+      const opts = {
+        maxByteLength: Math.min(Math.round(1.5 * size), 274877906944)
+      }
       // @ts-ignore
       if (tmpBuffer.maxByteLength < size) {
         // @ts-ignore
-        tmpBuffer = new ArrayBuffer(size, { maxByteLength: 1.5 * size | 0 });
+        tmpBuffer = new ArrayBuffer(size, opts)
       }
       // @ts-ignore
       tmpBuffer.resize(size)
