bce025556a550528b2b8ccf4ef66786fe325d471
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-03-14 15:57:23 +0100

    Add bufToHex()

--- packages/db/src/utils.ts
+++ packages/db/src/utils.ts
@@ -24,3 +24,19 @@ export const equals = (aB: Uint8Array, bB: Uint8Array): boolean => {
     return native.equals(aB, bB)
   }
 }
+
+const charMap = new TextEncoder().encode("0123456789abcdef");
+const DECODER = new TextDecoder()
+
+export const bufToHex = (a: Uint8Array): string => {
+  const tmp = new Uint8Array(2 * a.byteLength)
+  const n = charMap.length
+
+  let j = 0;
+  for (let i = 0; i < a.byteLength; i++) {
+    tmp[j++] = charMap[a[i] >>> 4 % n]
+    tmp[j++] = charMap[a[i] & 0x0f % n]
+  }
+
+  return DECODER.decode(tmp)
+}
