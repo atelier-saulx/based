3c4cefe4926ca7c0971898fb0ffa25edc3b13b46
Marco Vieira <maovieira@gmail.com>
2025-03-17 19:58:36 -0300

    agg kickoff

---
+++ packages/db/test/branchedCount.ts
@@ -0,0 +1,78 @@
+import { BasedDb } from '../src/index.js'
+import test from './shared/test.js'
+import { deepEqual } from './shared/assert.js'
+
+await test('branchedCount', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+
+  await db.start({ clean: true })
+
+  t.after(() => {
+    return db.destroy()
+  })
+
+  await db.putSchema({
+    types: {
+      user: {
+        props: {
+          flap: { type: 'uint32' },
+          name: { type: 'string' },
+          articles: {
+            items: {
+              ref: 'article',
+              prop: 'contributors',
+            },
+          },
+        },
+      },
+      article: {
+        props: {
+          name: { type: 'string' },
+          contributors: {
+            items: {
+              ref: 'user',
+              prop: 'articles',
+            },
+          },
+        },
+      },
+    },
+  })
+
+  const mrSnurp = db.create('user', {
+    name: 'Mr snurp',
+    flap: 10,
+  })
+
+  const flippie = db.create('user', {
+    name: 'Flippie',
+    flap: 20,
+  })
+
+  const derpie = db.create('user', {
+    name: 'Derpie',
+    flap: 30,
+  })
+
+  const dinkelDoink = db.create('user', {
+    name: 'Dinkel Doink',
+    flap: 40,
+  })
+
+  const strudelArticle = db.create('article', {
+    name: 'The wonders of Strudel',
+    contributors: [mrSnurp, flippie, derpie, dinkelDoink],
+  })
+
+  await db.drain()
+
+  await db.query('article', strudelArticle).include('*', '**').get().inspect()
+
+  await db
+    .query('article', strudelArticle)
+    .include((q) => q('contributors').include('name').filter('flap', '>', 25))
+    .get()
+    .inspect()
+})
