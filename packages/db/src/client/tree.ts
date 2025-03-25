e2de812759582f3fc0345375e1004e46b6dcad1d
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-03-25 21:27:46 +0100

    cleanup

--- packages/db/src/client/tree.ts
+++
@@ -1,8 +0,0 @@
-export const makeCsmtKeyFromNodeId = (
-  typeId: number,
-  blockCapacity: number,
-  nodeId: number,
-) => {
-  const tmp = nodeId - +!(nodeId % blockCapacity)
-  return typeId * 4294967296 + ((tmp / blockCapacity) | 0) * blockCapacity + 1
-}
