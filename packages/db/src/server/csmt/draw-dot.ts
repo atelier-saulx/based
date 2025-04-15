7d6d1805c814a98ab94abde26b86cab3ead06074
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-04-15 11:45:00 +0200

    fmt

--- packages/db/src/server/csmt/draw-dot.ts
+++ packages/db/src/server/csmt/draw-dot.ts
@@ -1,13 +1,19 @@
 import { encodeBase64 } from '@saulx/utils'
 import { Csmt, TreeNode } from './index.js'
 
+type DataFormatter<T> = (data: T) => string
+
 function makeLabel(node: TreeNode<any>) {
   return `${node.key}\n${encodeBase64(node.hash).substring(0, 5)}`
 }
 
-// This can be visualized with Graphviz dot
+function wrapFormatter<T>(dataFormatter: DataFormatter<T>, node: TreeNode<T>) {
+    return `\n${dataFormatter(node.data).replace('"', '\\"')}`
+}
+
+// This can be visualized with Graphviz dot.
 // Online: https://dreampuf.github.io/GraphvizOnline/?engine=dot
-export default function draw<T = any>(csmt: Csmt<T>, dataFormatter?: (data: T) => string) {
+export default function draw<T = any>(csmt: Csmt<T>, dataFormatter?: DataFormatter<T>) {
   const root = csmt.getRoot()
   const lines: string[] = []
   const nodes: string[] = []
@@ -20,7 +26,7 @@ export default function draw<T = any>(csmt: Csmt<T>, dataFormatter?: (data: T) =
     const isLeaf = !left && !right
 
     nodes.push(
-      `n${cur} [label="${makeLabel(node) + ((dataFormatter && node.data) ? '\n' + dataFormatter(node.data) : '')}"${isLeaf ? ' shape=box' : ''}];`,
+      `n${cur} [label="${makeLabel(node) + ((dataFormatter && node.data) ? wrapFormatter<T>(dataFormatter, node) : '')}"${isLeaf ? ' shape=box' : ''}];`,
     )
     if (cur > 0) {
       lines.push(`n${prev} -- n${cur}`)
