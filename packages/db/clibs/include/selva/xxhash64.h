00b4d2de559c6985501605326963aacd9d7607b2
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-04-15 10:17:05 +0200

    Cleanup

--- packages/db/clibs/include/selva/xxhash64.h
+++ packages/db/clibs/include/selva/xxhash64.h
@@ -1,12 +1,13 @@
+/*
+ * Copyright (c) 2025 SAULX
+ * SPDX-License-Identifier: MIT
+ */
 #pragma once
 #ifndef XXHASH64_H
 #define XXHASH64_H
 
-#include <stdlib.h>
 #include <stdint.h>
 #include "selva/_export.h"
-#include "cdefs.h"
-
 
 /**
  * @brief Wrapper function that computes the 64-bit hash of a given input string using the xxHash algorithm.
@@ -16,7 +17,6 @@
  * @param s The input string to be hashed.
  * @return A 64-bit hash value of the input string.
  */
-
 SELVA_EXPORT
 uint64_t xxHash64(const char *s, size_t len);
 
