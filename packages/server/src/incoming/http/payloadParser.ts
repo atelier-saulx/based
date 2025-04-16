d37d8e4ad9a2b9705c9f154ab045ac665a616726
youzi <youri@saulx.com>
2025-04-16 20:44:40 +0200

    make it work

--- packages/server/src/incoming/http/payloadParser.ts
+++ packages/server/src/incoming/http/payloadParser.ts
@@ -2,13 +2,10 @@ import { Context, HttpSession, BasedRouteComplete } from '@based/functions'
 import { parseAuthState } from '../../auth/index.js'
 import { pathExtractor } from './pathMatcher.js'
 
-export default (
-  ctx: Context<HttpSession>,
-  route: BasedRouteComplete
-) => {
+export default (ctx: Context<HttpSession>, route: BasedRouteComplete) => {
   let url = ctx.session.url
   const query = ctx.session.query ? '?' + ctx.session.query : ''
-  
+
   if (url.charCodeAt(url.length - 1) !== 47 && query) {
     url += '/'
   }
@@ -18,12 +15,12 @@ export default (
   if (!url.startsWith(`/${route.name}`)) {
     url = `/${route.name}${url}`
   }
-  
-  const payload = pathExtractor(route.tokens || [], Buffer.from(url))  
 
-  if ('token' in payload) {
+  const payload = pathExtractor(route.tokens, Buffer.from(url))
+
+  if (payload && 'token' in payload) {
     ctx.session.authState = parseAuthState(payload.token)
-    
+
     if (route.type === 'query') {
       delete payload.token
     }
