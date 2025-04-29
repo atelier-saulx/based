c38763f179e4865b042ab85ffe71a307180100e4
Marco Vieira <maovieira@gmail.com>
2025-04-29 16:24:35 -0300

    comptime stats

--- packages/db/native/query/aggregates/statistics.zig
+++ packages/db/native/query/aggregates/statistics.zig
@@ -1,106 +1,111 @@
 const std = @import("std");
 const AggFn = @import("../../types.zig").AggFn;
 
-pub const statType = enum { Population, Sample };
+pub const setType = enum { Population, Sample };
 
-pub const aggAccumulator = struct {
+const accumulator = struct {
     count: usize = 0,
     sum: f64 = 0.0,
     sum_sq: f64 = 0.0,
-    min: f64 = std.math.inf(f64),
-    max: f64 = -std.math.inf(f64),
-
-    // TODO: functions should go here? have to test allocation usage
-    // TODO: test computation options for performance. bit opertions, divisions, Welford's alg (sxx)
-    // TODO: valition to gyarantee usage only with number fields
-
-    const Self = @This();
-
-    pub fn update(self: *Self, value: f64, aggFn: AggFn) void {
-        switch (aggFn) {
-            .max => self.max = @max(self.max, value),
-            .min => self.min = @min(self.min, value),
-            .stddev => self.sum_sq += value * value,
-            .variance => self.sum_sq += value * value,
-        }
-        self.count += 1;
-        self.sum += value;
-    }
-
-    pub fn getCount(self: Self) usize {
-        return self.count;
-    }
-
-    pub fn getSum(self: Self) f64 {
-        return self.sum;
-    }
-
-    pub fn getMin(self: Self) ?f64 {
-        if (self.count == 0) return null;
-        return self.min;
-    }
-
-    pub fn getMax(self: Self) ?f64 {
-        if (self.count == 0) return null;
-        return self.max;
-    }
-
-    pub fn getAvg(self: Self) ?f64 {
-        if (self.count == 0) return null;
-        const n = @as(f64, @floatFromInt(self.count));
-        return self.sum / n;
-    }
-
-    pub fn getVariancePop(self: Self) ?f64 {
-        if (self.count == 0) return null;
-        const n = @as(f64, @floatFromInt(self.count));
-        if (n == 0.0) return null;
-
-        const mean = self.sum / n;
-        const variance = (self.sum_sq / n) - (mean * mean);
-
-        if (variance < 0.0 and variance > -1e-12) return 0.0;
-        if (variance < 0.0) return error.NegativeVariance;
-
-        return variance;
-    }
-
-    pub fn getVarianceSample(self: Self) ?f64 {
-        if (self.count < 2) return null;
-        const n = @as(f64, @floatFromInt(self.count));
-        const numerator = self.sum_sq - (self.sum * self.sum) / n;
-        const denominator = n - 1.0;
-        const variance = numerator / denominator;
-        if (variance < 0.0 and variance > -1e-12) return 0.0;
-        if (variance < 0.0) return error.NegativeVariance;
-        return variance;
-    }
-
-    pub fn getVariance(self: Self, kind: statType) ?f64 {
-        const varianceOpt: ?f64 = switch (kind) {
-            .Population => self.getVariancePop(),
-            .Sample => self.getVarianceSample(),
-        };
-
-        if (varianceOpt) |variance| {
-            return variance;
-        } else {
-            return null;
-        }
-    }
-
-    pub fn getStdDev(self: Self, kind: statType) ?f64 {
-        const varianceOpt: ?f64 = switch (kind) {
-            .Population => self.getVariancePop(),
-            .Sample => self.getVarianceSample(),
-        };
-
-        if (varianceOpt) |variance| {
-            return std.math.sqrt(variance);
-        } else {
-            return null;
-        }
-    }
+    min: f64,
+    max: f64,
 };
 
-// MV: Should add another struct to ordered sets after define allocation method
+fn accumulate(comptime data: []const f64) accumulator {
+    comptime var _count: usize = 0;
+    comptime var _sum: f64 = 0.0;
+    comptime var _sum_sq: f64 = 0.0;
+    comptime var _min: f64 = std.math.inf(f64);
+    comptime var _max: f64 = -std.math.inf(f64);
+
+    inline for (data) |value| {
+        comptime std.debug.assert(std.math.isFinite(value)); // f64!
+
+        _count += 1;
+        _sum += value;
+        _sum_sq += value * value;
+        _min = @min(min, value);
+        _max = @max(max, value);
+    }
+
+    return .{
+        .count = _count,
+        .sum = _sum,
+        .sum_sq = _sum_sq,
+        .min = _min, // remains Infiity if count == 0
+        .max = _max,
+    };
+}
+
+pub fn count(comptime data: []const f64) usize {
+    return accumulate(data).count;
+}
+
+pub fn sum(comptime data: []const f64) f64 {
+    return accumulate(data).sum;
+}
+
+pub fn min(comptime data: []const f64) f64 {
+    const state = accumulate(data);
+    if (state.count == 0) return null;
+    return state.min;
+}
+
+pub fn max(comptime data: []const f64) f64 {
+    const state = accumulate(data);
+    if (state.count == 0) return null;
+    return state.max;
+}
+
+pub fn avg(comptime data: []const f64) f64 {
+    const state = accumulate(data);
+    if (state.count == 0) return null;
+    const n = @as(f64, @floatFromInt(state.count));
+    return state.sum / n;
+}
+
+pub fn variancePop(comptime data: []const f64) f64 {
+    const state = accumulate(data);
+    if (state.count == 0) return null;
+    const n = @as(f64, @floatFromInt(state.count));
+
+    const _avg = state.sum / n;
+    const _variance = (state.sum_sq / n) - (_avg * _avg);
+
+    if (_variance < 0.0 and _variance > -1e-12) return 0.0;
+    if (_variance < 0.0) return null;
+
+    return variance;
+}
+
+pub fn varianceSample(comptime data: []const f64) f64 {
+    const state = accumulate(data);
+    if (state.count < 2) return null;
+    const n = @as(f64, @floatFromInt(state.count));
+    const numerator = state.sum_sq - (state.sum * state.sum) / n;
+    const denominator = n - 1.0;
+    const _variance = numerator / denominator;
+
+    if (_variance < 0.0 and _variance > -1e-12) return 0.0;
+    if (_variance < 0.0) return null;
+
+    return _variance;
+}
+
+pub fn variance(comptime data: []const f64, kind: setType) ?f64 {
+    const state = accumulate(data);
+    return switch (kind) {
+        .Population => state.variancePop(),
+        .Sample => state.varianceSample(),
+    };
+}
+
+pub fn stdDev(comptime data: []const f64, kind: setType) ?f64 {
+    const _variance: ?f64 = variance(data, kind);
+
+    if (_variance) |v| {
+        return std.math.sqrt(v);
+    } else {
+        return null;
+    }
+}
