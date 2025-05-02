c3fe3d586bd301ca809f1848c076262f61d56074
Marco Vieira <maovieira@gmail.com>
2025-05-01 22:25:26 -0300

    added performance tests for delete

--- packages/db/test/delete.ts
+++ packages/db/test/delete.ts
@@ -223,3 +223,110 @@ await test('save', async (t) => {
   deepEqual(await db2.query('user').include('id').get().toObject(), [{ id: 2 }])
   deepEqual(await db.query('user').include('id').get().toObject(), [{ id: 2 }])
 })
+
+await test('delete performance', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+  await db.start({ clean: true })
+  t.after(() => t.backup(db))
+
+  await db.setSchema({
+    types: {
+      user: {
+        props: {
+          flap: { type: 'uint32' },
+          name: { type: 'string' },
+          articles: {
+            items: {
+              ref: 'article',
+              prop: 'contributors',
+            },
+          },
+        },
+      },
+      article: {
+        props: {
+          name: { type: 'string' },
+          contributors: {
+            items: {
+              ref: 'user',
+              prop: 'articles',
+            },
+          },
+        },
+      },
+    },
+  })
+  const amount = 1e6
+  const users = []
+
+  console.time('create 1M users')
+  for (let i = 0; i < amount; i++) {
+    users.push(db.create('user', { name: `user_${i}`, flap: i }))
+  }
+  await db.drain()
+  console.timeEnd('create 1M users')
+
+  console.time('delete 1M users')
+  for (const user of users) {
+    db.delete('user', user)
+  }
+  await db.drain()
+  console.timeEnd('delete 1M users')
+
+  deepEqual((await db.query('user').get()).toObject(), [])
+
+  const amountArticles = 1e6
+  const articles = []
+
+  console.time('create 1M articles')
+  for (let i = 0; i < amountArticles; i++) {
+    articles.push(db.create('article', { name: `article_${i}` }))
+  }
+  await db.drain()
+  console.timeEnd('create 1M articles')
+
+  console.time('delete 1M articles')
+  for (const article of articles) {
+    db.delete('article', article)
+  }
+  await db.drain()
+  console.timeEnd('delete 1M articles')
+
+  deepEqual((await db.query('article').get()).toObject(), [])
+
+  const articles2 = []
+
+  console.time('create 1M articles - drain interleaved at 10k')
+  for (let i = 0; i < amountArticles; i++) {
+    articles2.push(db.create('article', { name: `article_${i}` }))
+    if (i % 1e5 === 0) {
+      await db.drain()
+    }
+  }
+  await db.drain()
+  console.timeEnd('create 1M articles - drain interleaved at 10k')
+
+  console.log(
+    'if you interleave drain in each batch of 10K deletes you come up if +2min',
+    'this test was commented/skipped to not pollute the others',
+  )
+  // console.time('delete 1M articles - drain interleaved at 10k')
+  // for (const article of articles2) {
+  //   db.delete('article', article)
+  //   if (articles.indexOf(article) % 1e5 === 0) {
+  //     await db.drain()
+  //   }
+  // }
+  // await db.drain()
+  // console.timeEnd('delete 1M articles - drain interleaved at 10k')
+
+  console.time('delete 1M articles - again')
+  for (const article of articles2) {
+    db.delete('article', article)
+  }
+  await db.drain()
+  console.timeEnd('delete 1M articles - again')
+  deepEqual((await db.query('article').get()).toObject(), [])
+})
