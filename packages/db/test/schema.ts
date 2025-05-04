e00a00e15c9c5ffb9896442b594c6b3580d1b2ed
Francesco Marciani <marciani3@gmail.com>
2025-05-04 16:54:36 +0200

    add schema max fields test

---
+++ packages/db/test/schema.ts
@@ -0,0 +1,38 @@
+import test from './shared/test.js'
+import { BasedDb } from '../src/index.js'
+
+await test('support many fields on root', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+  await db.start({ clean: true })
+  t.after(() => t.backup(db))
+
+  const props = {}
+  for (let i = 0; i < 254; i++) {
+    props['myProp' + i] = 'string'
+  }
+
+  await db.setSchema({
+    props,
+  })
+})
+
+await test('support many fields on type', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+  await db.start({ clean: true })
+  t.after(() => t.backup(db))
+
+  const props = {}
+  for (let i = 0; i < 254; i++) {
+    props['myProp' + i] = 'string'
+  }
+
+  await db.setSchema({
+    types: {
+      flurp: props,
+    },
+  })
+})
