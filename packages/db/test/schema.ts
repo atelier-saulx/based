596246df9235323a8eb9d4f85013e28d4bfb751c
youzi <youri@saulx.com>
2025-05-06 19:00:58 +0200

    add test for schema.hash

--- packages/db/test/schema.ts
+++ packages/db/test/schema.ts
@@ -36,3 +36,36 @@ await test('support many fields on type', async (t) => {
     },
   })
 })
+
+await test('schema hash', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+  await db.start({ clean: true })
+  t.after(() => db.destroy())
+
+  await db.setSchema({
+    types: {
+      flurp: {
+        name: 'string',
+      },
+    },
+  })
+
+  const hash1 = db.server.schema.hash
+
+  await db.setSchema({
+    types: {
+      flurp: {
+        name: 'string',
+        title: 'string',
+      },
+    },
+  })
+
+  const hash2 = db.server.schema.hash
+
+  if (!hash1 || !hash2 || hash1 === hash2) {
+    throw new Error('Incorrect hash')
+  }
+})
