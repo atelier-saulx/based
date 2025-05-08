3e998b80df8e0f0779882438c2c5bcc5a0ae262d
Marco Vieira <maovieira@gmail.com>
2025-05-08 13:31:28 -0300

    added sum aggregation and groupBy tests

--- packages/db/test/aggregate.ts
+++ packages/db/test/aggregate.ts
@@ -1,4 +1,4 @@
-import { equal } from 'node:assert'
+import { equal, deepEqual } from 'node:assert'
 import { BasedDb } from '../src/index.js'
 import { allCountryCodes } from './shared/examples.js'
 import test from './shared/test.js'
@@ -31,143 +31,130 @@ await test('aggregate', async (t) => {
             ref: 'sequence',
             prop: 'votes',
           },
-          // flap: {
-          //   props: {
-          //     hello: 'uint32',
-          //   },
-          // },
           country: { type: 'string', maxBytes: 2 },
-          // AL: 'uint8',
-          // AM: 'uint8',
-          // AT: 'uint8',
           AU: 'uint8',
-          // AZ: 'uint8',
-          // BE: 'uint8',
-          // CH: 'uint8',
-          // CY: 'uint8',
-          // CZ: 'uint8',
-          // DE: 'uint8',
-          // DK: 'uint8',
-          // EE: 'uint8',
-          // ES: 'uint8',
-          // FI: 'uint8',
-          // FR: 'uint8',
-          // GB: 'uint8',
-          // GE: 'uint8',
-          // GR: 'uint8',
-          // HR: 'uint8',
-          // IE: 'uint8',
-          // IL: 'uint8',
-          // IS: 'uint8',
-          // IT: 'uint8',
-          // LT: 'uint8',
-          // LU: 'uint8',
-          // LV: 'uint8',
-          // MD: 'uint8',
-          // MT: 'uint8',
           NL: 'uint8',
-          // NO: 'uint8',
-          // PL: 'uint8',
-          // PT: 'uint8',
-          // RS: 'uint8',
-          // SE: 'uint8',
-          // SI: 'uint8',
-          // SM: 'uint8',
-          // UA: 'uint8',
         },
       },
     },
   })
-
-  // const countries = Object.keys(db.client.schema.types.vote.props).filter(
-  //   (v) => v !== 'flap' && v !== 'country',
-  // )
-
-  // for (let i = 0; i < 1; i++) {
-  //   const x = {
-  //     country: allCountryCodes[~~(Math.random() * allCountryCodes.length)],
-  //     flap: {
-  //       hello: 1,
-  //     },
-  //   }
-  //   for (const key of countries) {
-  //     x[key] = ~~(Math.random() * 20)
-  //   }
-  //   db.create('vote', x)
-  // }
-
   const nl1 = db.create('vote', {
     country: 'bb',
     NL: 10,
-    // flap: {
-    //   hello: 813,
-    // },
   })
   const nl2 = db.create('vote', {
     country: 'aa',
     NL: 20,
-    // flap: {
-    //   hello: 100,
-    // },
   })
   const au1 = db.create('vote', {
     country: 'aa',
     AU: 15,
-    // flap: {
-    //   hello: 900,
-    // },
   })
   const s = db.create('sequence', { votes: [nl1, nl2, au1] })
 
-  // const q2 = await db
-  //   .query('sequence')
-  //   // .include('votes.NL')
-  //   // don't break line
-  //   .sum('votes')
-  //   .get()
+  deepEqual(
+    await db.query('vote').sum('NL').get().toObject(),
+    { NL: 30 },
+    'sum, top level, single prop',
+  )
 
-  // console.log(q2.toJSON()) // ok
-  // q2.inspect(10) // buggy
+  deepEqual(
+    await db
+      .query('vote')
+      .filter('country', '=', 'aa')
+      .sum('NL')
+      .get()
+      .toObject(),
+    { NL: 20 },
+    'sum with filter',
+  )
 
-  //--------------------------------------------------------
-  const q2 = await db
-    .query('sequence')
-    .include((select) => {
-      select('votes')
-        // don't break line
-        // .groupBy('sequence')
-        .sum('NL', 'AU') // 'AU'
-      // .include('id')
-    })
-    .get()
-  q2.debug() // ok
-  q2.inspect()
-  // console.info(q2.toObject())
+  // top level  ----------------------------------
+  deepEqual(
+    await db.query('vote').sum('NL', 'AU').get().toObject(),
+    { NL: 30, AU: 15 },
+    'sum, top level, multiple props',
+  )
 
-  //--------------------------------------------------------
+  deepEqual(
+    await db.query('vote').sum().get().toObject(),
+    {},
+    'sum() returning nothing',
+  )
 
-  const q3 = await db
-    .query('sequence')
-    .include((select) => {
-      select('votes')
-        // don't break line
-        .groupBy('country')
-        .sum('NL', 'AU') // 'AU'
-    })
-    .get()
-  // console.log('----------')
-  q3.debug() // ok
-  q3.inspect()
+  deepEqual(
+    await db
+      .query('vote')
+      .filter('country', '=', 'zz')
+      .sum('NL')
+      .get()
+      .toObject(),
+    { NL: 0 },
+    'sum with empty result set',
+  )
 
-  // await db.query('vote').sum('NL').get().inspect()
+  // groupBy  ----------------------------------
 
-  // console.log(await db.drain())
+  deepEqual(
+    await db.query('vote').sum('NL', 'AU').groupBy('country').get().toObject(),
+    { bb: { NL: 10, AU: 0 }, aa: { NL: 20, AU: 15 } },
+    'sum, top level, groupBy',
+  )
 
-  // // db.query('NL').sum('NL').get().inspect() // field error
+  deepEqual(
+    await db.query('vote').groupBy('country').get().toObject(),
+    { bb: {}, aa: {} },
+    'groupBy with no aggregation function',
+  )
 
-  //--------------------------------------------------------
-  // await db.query('vote').sum('NL').get().inspect()
-  //--------------------------------------------------------
+  deepEqual(
+    await db
+      .query('vote')
+      .filter('country', '=', 'bb')
+      .groupBy('country')
+      .sum('NL', 'AU')
+      .get()
+      .toObject(),
+    { bb: { NL: 10, AU: 0 } },
+    'filter, groupBy on single distinct value',
+  )
+
+  // branched includes ----------------------------------
+  deepEqual(
+    await db
+      .query('sequence')
+      .include((select) => {
+        select('votes').sum('NL', 'AU')
+      })
+      .get()
+      .toObject(),
+    [{ id: 1, votes: { NL: 30, AU: 15 } }],
+    'brached include, sum, references',
+  )
+
+  deepEqual(
+    await db
+      .query('sequence')
+      .include((select) => {
+        select('votes').groupBy('country').sum('NL', 'AU')
+      })
+      .get()
+      .toObject(),
+    [{ id: 1, votes: { aa: { AU: 15, NL: 20 }, bb: { AU: 0, NL: 10 } } }],
+    'branched include, references, groupBy',
+  )
+
+  deepEqual(
+    await db
+      .query('sequence')
+      .include((select) => {
+        select('votes').filter('country', '=', 'aa').sum('NL', 'AU')
+      })
+      .get()
+      .toObject(),
+    [{ id: 1, votes: { NL: 20, AU: 15 } }],
+    'branched include, references, filtered, groupBy',
+  )
 
   // await db.query('vote').count().get().inspect()
   // // ;(await db.query('vote').count().get()).debug()
@@ -181,60 +168,112 @@ await test('aggregate', async (t) => {
   // // count is going to be a seperate aggregate (like group)
   // // count is very different in that it does not require a field
 
-  // // const q = await db
-  // //   .query('vote')
-  // //   .groupBy('country')
-  // //   .sum(countries, 'flap.hello')
-  // //   .get()
-  // //   .inspect()
-  // // console.log(q.execTime, q.size, '?')
-
-  // const q2 = await db
-  //   .query('vote')
-  //   .groupBy('country')
-  //   .sum('NL', 'AU')
-  //   .get()
-  //   .inspect()
-  // const q3 = await db.query('vote').groupBy('country').count().get()
-
   // console.log('count + groupBy + inspect')
   // q3.inspect()
   // console.log('count + groupBy + toObject')
   // console.log(q3.toObject())
 
-  // // this is your goal
-  // db.query('sequence')
-  //   .include((select) => {
-  //     select('votes').groupBy('country').sum('NL', 'AU')
-  //   })
-  //   .get()
-
-  // // db.query('sequence', seqId)
-  // // .include((select) => {
-  // //   select('votes').groupBy('country').sum('NL', 'AU')
-  // // })
-  // // .get()
-
-  // // console.log(q.execTime, q2.size, '?')
-  // // q.debug()
-
-  // // add count!
-
   // // ;(await db.query('vote').sum('flap.hello', 'SM').get()).debug()
 
-  // // console.log(await db.query('vote').sum(countries).get().toObject())
-
   // // console.log((await db.query('vote').sum(countries).get()).execTime)
-  // // ;(await db.query('vote').sum('SM', 'UA').get()).debug()
 
-  // // step 1 make this work
-  // // group('country')
-  // // ;(await db.query('vote').sum(countries).get()).debug()
-
-  // // console.log((await db.query('vote').sum(countries).get()).toObject())
   // // .mean('ddi1', 'ddi2', 'ddi3', 'ddi4')
+})
 
-  // // console.log((await db.query('vote').sum(countries).get()).execTime, 'ms')
+await test('batch', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+    maxModifySize: 1e6,
+  })
+
+  await db.start({ clean: true })
+  // t.after(() => t.backup(db))
+  t.after(() => db.stop())
+
+  await db.setSchema({
+    types: {
+      sequence: {
+        props: {
+          votes: {
+            items: {
+              ref: 'vote',
+              prop: 'sequence',
+            },
+          },
+        },
+      },
+      vote: {
+        props: {
+          sequence: {
+            ref: 'sequence',
+            prop: 'votes',
+          },
+          flap: {
+            props: {
+              hello: 'uint32',
+            },
+          },
+          country: { type: 'string', maxBytes: 2 },
+          AL: 'uint8',
+          AM: 'uint8',
+          AT: 'uint8',
+          AU: 'uint8',
+          AZ: 'uint8',
+          BE: 'uint8',
+          CH: 'uint8',
+          CY: 'uint8',
+          CZ: 'uint8',
+          DE: 'uint8',
+          DK: 'uint8',
+          EE: 'uint8',
+          ES: 'uint8',
+          FI: 'uint8',
+          FR: 'uint8',
+          GB: 'uint8',
+          GE: 'uint8',
+          GR: 'uint8',
+          HR: 'uint8',
+          IE: 'uint8',
+          IL: 'uint8',
+          IS: 'uint8',
+          IT: 'uint8',
+          LT: 'uint8',
+          LU: 'uint8',
+          LV: 'uint8',
+          MD: 'uint8',
+          MT: 'uint8',
+          NL: 'uint8',
+          NO: 'uint8',
+          PL: 'uint8',
+          PT: 'uint8',
+          RS: 'uint8',
+          SE: 'uint8',
+          SI: 'uint8',
+          SM: 'uint8',
+          UA: 'uint8',
+        },
+      },
+    },
+  })
+
+  const countries = Object.keys(db.client.schema.types.vote.props).filter(
+    (v) => v !== 'flap' && v !== 'country',
+  )
+
+  for (let i = 0; i < 1; i++) {
+    const x = {
+      country: allCountryCodes[~~(Math.random() * allCountryCodes.length)],
+      flap: {
+        hello: 1,
+      },
+    }
+    for (const key of countries) {
+      x[key] = ~~(Math.random() * 20)
+    }
+    db.create('vote', x)
+  }
+
+  console.log(await db.drain())
 })
 
 await test('top level count', async (t) => {
