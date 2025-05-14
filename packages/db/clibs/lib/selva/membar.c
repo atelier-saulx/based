dd596fa9688682800058a7a7c6861a900da9fa27
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-05-14 16:08:11 +0200

    Improve membar

--- packages/db/clibs/lib/selva/membar.c
+++ packages/db/clibs/lib/selva/membar.c
@@ -2,18 +2,29 @@
  * Copyright (c) 2025 SAULX
  * SPDX-License-Identifier: MIT
  */
+#include <stdatomic.h>
 #include "selva/membar.h"
 
 void membar_sync_read(void)
 {
-    __sync_synchronize();
+#if defined(__arm__) || defined(__aarch64__)
+    __asm__ volatile (
+            "dmb ishld"
+            ::: "memory");
+#else
+    atomic_thread_fence(memory_order_acquire);
+#endif
 }
 
 void membar_sync_write(void)
 {
 #if defined(__arm__) || defined(__aarch64__)
-    __asm__ volatile ("isb" ::: "memory");
+    __asm__ volatile (
+            "isb\n\t"
+            "dmb ishst"
+            ::: "memory");
 #else
-    __sync_synchronize();
+    atomic_thread_fence(memory_order_release);
 #endif
+
 }
