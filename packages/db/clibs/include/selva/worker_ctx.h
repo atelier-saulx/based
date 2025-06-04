4d49c14d1dda44e96ad6d989c64a18ca49743ac2
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-06-04 13:51:47 +0200

    Init decompressor in worker_ctx

--- packages/db/clibs/include/selva/worker_ctx.h
+++ packages/db/clibs/include/selva/worker_ctx.h
@@ -1,13 +1,26 @@
 /*
- * Copyright (c) 2024 SAULX
+ * Copyright (c) 2024-2025 SAULX
  * SPDX-License-Identifier: MIT
  */
 #pragma once
 
 #include "selva/_export.h"
+#include "libdeflate.h"
 
 SELVA_EXPORT
-void worker_ctx_init();
+void worker_ctx_init(void);
 
 SELVA_EXPORT
-void worker_ctx_deinit();
+void worker_ctx_deinit(void);
+
+SELVA_EXPORT
+enum libdeflate_result worker_ctx_libdeflate_decompress(
+			      const void *in, size_t in_nbytes,
+			      void *out, size_t out_nbytes_avail,
+			      size_t *actual_out_nbytes_ret);
+
+SELVA_EXPORT
+enum libdeflate_result worker_ctx_libdeflate_decompress_stream(
+        const char *in_buf, size_t in_len,
+        libdeflate_decompress_stream_cb_t cb, void *ctx,
+        int *result);
--- packages/db/clibs/lib/selva/worker_ctx.c
+++ packages/db/clibs/lib/selva/worker_ctx.c
@@ -2,26 +2,51 @@
  * Copyright (c) 2024-2025 SAULX
  * SPDX-License-Identifier: MIT
  */
+#include <stdint.h>
 #include <pthread.h>
 #include "selva/selva_string.h"
+#include "libdeflate.h"
 #include "selva/worker_ctx.h"
 
 static __thread bool worker_initialized;
+static __thread struct libdeflate_decompressor *libdeflate_decompressor;
+static __thread struct libdeflate_block_state libdeflate_block_state;
 
-void worker_ctx_init()
+__constructor void worker_ctx_init(void)
 {
 #if 0
     pthread_t x = pthread_self();
 #endif
 
     selva_string_init_tls();
+    libdeflate_decompressor = libdeflate_alloc_decompressor();
+    libdeflate_block_state = libdeflate_block_state_init(305000);
     worker_initialized = true;
 }
 
-void worker_ctx_deinit()
+__destructor void worker_ctx_deinit(void)
 {
     if (worker_initialized) {
         selva_string_deinit_tls();
+        libdeflate_block_state_deinit(&libdeflate_block_state);
+        libdeflate_free_decompressor(libdeflate_decompressor);
+        libdeflate_decompressor = nullptr;
         worker_initialized = false;
     }
+}
+
+enum libdeflate_result worker_ctx_libdeflate_decompress(
+			      const void *in, size_t in_nbytes,
+			      void *out, size_t out_nbytes_avail,
+			      size_t *actual_out_nbytes_ret)
+{
+    return libdeflate_decompress(libdeflate_decompressor, in, in_nbytes, out, out_nbytes_avail, actual_out_nbytes_ret);
+}
+
+enum libdeflate_result worker_ctx_libdeflate_decompress_stream(
+        const char *in_buf, size_t in_len,
+        libdeflate_decompress_stream_cb_t cb, void *ctx,
+        int *result)
+{
+    return libdeflate_decompress_stream(libdeflate_decompressor, &libdeflate_block_state, in_buf, in_len, cb, ctx, result);
 }
