65b7e469a3362d5b7155ebc0e0465b0ed1a2231c
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-06-30 15:07:43 +0200

    Improve worker response handling

--- packages/db/src/server/IoWorker.ts
+++ packages/db/src/server/IoWorker.ts
@@ -1,6 +1,7 @@
 import { DbWorker } from './workers/DbWorker.js'
 import { DbServer } from './index.js'
 import { IoJob} from './workers/io_worker_types.js'
+import { DECODER, readInt32 } from '@saulx/utils'
 
 
 export class IoWorker extends DbWorker {
@@ -14,21 +15,34 @@ export class IoWorker extends DbWorker {
   override handleMsg(_buf: any): void {
   }
 
-  loadBlock(filepath: string): Promise<void> {
+  async loadBlock(filepath: string): Promise<void> {
     const job: IoJob = {
       type: 'load',
       filepath
     }
-    return this.call(job)
+
+    const resBuf = await this.call(job)
+    if (resBuf.length) {
+      throw new Error(DECODER.decode(resBuf))
+    }
   }
 
-  unloadBlock(filepath: string, typeId: number, start: number): Promise<void> {
+  async unloadBlock(filepath: string, typeId: number, start: number): Promise<Uint8Array> {
     const job: IoJob = {
       type: 'unload',
       filepath,
       typeId,
       start
     }
-    return this.call(job)
+
+    const resBuf = await this.call(job)
+    const err = readInt32(resBuf, 0)
+    if (err) {
+      throw new Error(`selva error: ${err}`)
+    }
+
+    // Note that this shares the original buffer which may not be 100% optimal,
+    // as the first 4 bytes are no longer needed.
+    return new Uint8Array(resBuf.buffer, 4)
   }
 }
