bd53ab4858b231dacab7404fe4b6f53ae65e9a09
youzi <youri@saulx.com>
2025-07-03 16:59:19 +0200

    debugging schema

--- packages/db/src/types.ts
+++ packages/db/src/types.ts
@@ -1,3 +1,4 @@
+export const SCHEMA_FILE_DEPRECATED = 'schema.json'
 export const SCHEMA_FILE = 'schema.bin'
 export const WRITELOG_FILE = 'writelog.json'
 export const COMMON_SDB_FILE = 'common.sdb'
