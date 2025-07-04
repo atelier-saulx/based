dd67d751bf2cfbe51ee00f1e912a1c024d749a73
Marco Vieira <maovieira@gmail.com>
2025-07-04 10:15:04 -0300

    building the test

--- packages/db/test/aggregate.ts
+++ packages/db/test/aggregate.ts
@@ -1785,3 +1785,78 @@ await test.skip('edges agregation', async (t) => {
   // edges unreacheable
   db.query('movie').max('actors.$rating').get().inspect(10)
 })
+
+await test('dev', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+  await db.start({ clean: true })
+  t.after(() => db.stop())
+
+  await db.setSchema({
+    types: {
+      lunch: {
+        week: 'uint8',
+        Mon: 'cardinality',
+        Tue: 'cardinality',
+        Wed: 'cardinality',
+        Thu: 'cardinality',
+        Fri: 'cardinality',
+      },
+    },
+  })
+
+  const week27 = {
+    week: 27,
+    Mon: ['Tom', 'youzi', 'jimdebeer', 'Victor', 'Luca'],
+    Tue: ['Nuno', 'Tom', 'Alex', 'Niels', 'jimdebeer', 'Francesco', 'Victor'],
+    Wed: ['Nuno', 'youzi', 'Francesco', 'Victor', 'Luca'],
+    Thu: [
+      'Nuno',
+      'yves',
+      'Fulco',
+      'Tom',
+      'Sara',
+      'Felix',
+      'Thomas',
+      'Sebastian',
+      'jimdebeer',
+      'youzi',
+      'Francesco',
+      'Victor',
+      'sandor',
+      'Fabio',
+      'Luca',
+    ],
+    Fri: [
+      'Nuno',
+      'yves',
+      'Tom',
+      'youzi',
+      'jimdebeer',
+      'Francesco',
+      'Victor',
+      'sandor',
+      'Luca',
+    ],
+  }
+  await db.create('lunch', week27)
+
+  const eaters = await db.query('lunch').get()
+  eaters.inspect()
+
+  // knwon from raw data:
+  const days = Object.keys(week27).filter((k) => k !== 'week')
+  const meals = days.map((k) => week27[k]).flat()
+  const totalEaters = new Set(meals)
+  console.log(
+    `From raw data. Total eaters: ${totalEaters.size}, Total meals: ${meals.length}`,
+  )
+
+  console.log(
+    'Total meals from query: ',
+    Object.entries(eaters.toObject()[0])
+      .filter(([key]) => days.includes(key))
+      .reduce((sum, el: [string, number]) => sum + el[1], 0),
+  )
+})
