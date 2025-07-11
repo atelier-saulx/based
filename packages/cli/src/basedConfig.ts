13da7264d2456813e3734ef0851d150b6a7239b2
youzi <youri@saulx.com>
2025-07-11 15:35:05 +0200

    dynamic import

--- packages/cli/src/basedConfig.ts
+++ packages/cli/src/basedConfig.ts
@@ -1,30 +1,33 @@
 import { findUp } from 'find-up'
-import { build, BuildResult, context } from 'esbuild'
+import { build } from 'esbuild'
 import { readJSON } from 'fs-extra/esm'
 
 export const getBasedConfig = async () => {
   const basedFile = await findUp(['based.ts', 'based.js', 'based.json'])
 
-  if (basedFile) {
-    if (/\.(?:ts|js)$/.test(basedFile)) {
-      try {
-        const basedConfigCtx = await build({
-          entryPoints: [basedFile],
-          bundle: true,
-          write: false,
-          platform: 'node',
-          metafile: true,
-        })
-        const code = basedConfigCtx.outputFiles[0].text
-        console.log(code)
-        const config = eval(code)
-        console.log(config)
-        return config
-      } catch (error) {
-        console.error(error)
-      }
-    } else {
-      return readJSON(basedFile)
+  if (!basedFile) {
+    console.info('no based file')
+    process.exit()
+  }
+
+  if (/\.(?:ts|js)$/.test(basedFile)) {
+    try {
+      const basedConfigCtx = await build({
+        entryPoints: [basedFile],
+        bundle: true,
+        write: false,
+        platform: 'node',
+        format: 'esm',
+      })
+      const config = await import(
+        `data:text/javascript;base64,${Buffer.from(basedConfigCtx.outputFiles[0].contents).toString('base64')}`
+      )
+      console.log(config.default)
+      return config.default
+    } catch (error) {
+      console.error(error)
     }
+  } else {
+    return readJSON(basedFile)
   }
 }
