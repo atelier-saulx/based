6498480cde0b88ce0ecdd4498a5572c243010e43
Jim de Beer <beerdejim@gmail.com>
2025-07-15 16:20:00 +0200

    fix

--- packages/cli/src/cli.tsx
+++ packages/cli/src/cli.tsx
@@ -11,21 +11,32 @@ const cli = meow(
 	  $ @based/cli [options]
 
   Commands
-    dev       Run in development mode
-    init      Initialize a new project
-    status    Show status of the project
-    logout    Logout from the project
+    deploy     Deploy to the online environment
+    dev        Run in local development mode
+    init       Initialize a new project
+    status     Show status & logs
+    logout     Logout from the cloud
 
   Options
     --no-cloud  Do not use cloud
+    --watch     Watch for changes and redeploy (only for deploy)
+    --force     Force a reload of the app functions (only for deploy)
+    --env       Set the environment
+    --cluster   Set the cluster
+    --project   Set the project
+    --token     User api token
+    --url       Use a custom discovery url
 
   Examples
-    $ @based/cli --no-cloud
+    $ @based/cli deploy --watch --force
 `,
   {
     importMeta: import.meta,
     flags: {
       noCloud: {
+        type: 'boolean',
+      },
+      watch: {
         type: 'boolean',
       },
       dev: {
@@ -37,6 +48,24 @@ const cli = meow(
       logout: {
         type: 'boolean',
       },
+      force: {
+        type: 'boolean',
+      },
+      env: {
+        type: 'string',
+      },
+      cluster: {
+        type: 'string',
+      },
+      project: {
+        type: 'string',
+      },
+      token: {
+        type: 'string',
+      },
+      url: {
+        type: 'string',
+      },
     },
   },
 )
@@ -45,6 +74,13 @@ const command: Props['command'] = cli.input[0] as Props['command']
 
 const opts: Opts = {
   noCloud: cli.flags.noCloud,
+  env: cli.flags.env,
+  cluster: cli.flags.cluster,
+  project: cli.flags.project,
+  token: cli.flags.token,
+  url: cli.flags.url,
+  force: cli.flags.force,
+  watch: cli.flags.watch,
 }
 
 render(<App opts={opts} command={command ?? 'status'} />)
--- packages/cli/src/types.ts
+++ packages/cli/src/types.ts
@@ -1,3 +1,10 @@
 export type Opts = {
   noCloud: boolean
+  env: string
+  cluster: string
+  project: string
+  token: string
+  url: string
+  force: boolean
+  watch: boolean
 }
