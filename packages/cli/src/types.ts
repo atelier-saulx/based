93735a1b78fb94ce2721287a229db924492ecd74
Jim de Beer <beerdejim@gmail.com>
2025-07-17 10:28:47 +0200

    fix

--- packages/cli/src/types.ts
+++ packages/cli/src/types.ts
@@ -18,3 +18,15 @@ export type Props = {
   opts: Opts
   command: 'dev' | 'deploy' | 'secrets' | 'init' | 'status' | 'logout'
 }
+
+export type LogType = {
+  id: number
+  msg: string
+  type: 'info' | 'error' | 'warn' | 'debug' | 'log' | 'trace'
+  function: {
+    name: string
+    checksum: number
+  }
+  createdAt: number
+  lines: number
+}
