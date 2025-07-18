e0816c6b6f51fec557b9d41eb671e1da91d9d091
Jim de Beer <beerdejim@gmail.com>
2025-07-18 11:56:55 +0200

    fix

--- packages/db/test/textFilter.ts
+++ packages/db/test/textFilter.ts
@@ -126,20 +126,80 @@ await test('compressionFilter', async (t) => {
 
   await db.setSchema({
     types: {
-      x: {
-        props: {
-          bla: ['x', 'y', 'z'],
-          y: { type: 'string' },
+      function: {
+        name: 'alias',
+        uniqueVisitors: 'cardinality',
+        totalRequests: 'uint32',
+        totalErrors: 'uint32',
+        checksum: 'uint32',
+        connections: 'uint32',
+        errorOnInitialization: 'boolean',
+
+        // temp
+        execTime: 'uint32',
+
+        events: {
+          items: {
+            ref: 'event',
+            prop: 'function',
+            dependent: true,
+          },
         },
+        measurements: {
+          props: {
+            current: {
+              ref: 'measurement',
+              prop: 'current',
+              dependent: true,
+            },
+            history: {
+              items: {
+                ref: 'measurement',
+                prop: 'function',
+                dependent: true,
+              },
+            },
+          },
+        },
+      },
+      measurement: {
+        execTime: 'uint32',
+        calls: 'uint32',
+        errors: 'uint32',
+        uniqueVisitors: 'cardinality',
+        maxConcurrentConnections: 'uint32',
+        current: {
+          ref: 'function',
+          prop: 'measurements.current',
+        },
+        function: {
+          ref: 'function',
+          prop: 'measurements.history',
+        },
+        createdAt: { type: 'timestamp', on: 'create' },
+        lastUpdated: { type: 'timestamp', on: 'update' },
+      },
+      event: {
+        msg: { type: 'string' },
+        function: {
+          ref: 'function',
+          prop: 'events',
+        },
+        createdAt: { type: 'timestamp', on: 'create' },
+        meta: {
+          type: 'string',
+        },
+        type: ['init', 'deploy', 'runtime', 'security'],
+        level: ['info', 'error', 'warn', 'debug'],
       },
     },
   })
 
   const derp = `Lorem ipsum dol89258, consectetur adipisci, nisi nisl aliquam enim, eget facilisis enim nisl nec elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse potenti. Etiam euismod, urna eu tincidunt consectetur, nisi nisl aliquam enim, eget facilisis enim nisl nec elit. Pellentesque habitant mor bi.`
 
-  await db.create('x', {
-    y: derp,
+  await db.create('event', {
+    msg: derp,
   })
 
-  deepEqual(await db.query('x').filter('y', 'has', 'derp').get(), [])
+  deepEqual(await db.query('event').filter('msg', 'has', 'derp').get(), [])
 })
