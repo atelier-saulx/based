7e51200bb55a287aae39dd97c4f0bc3e32135420
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-08-23 09:24:31 +0200

    Add funny bench

---
+++ packages/db/test/bench.ts
@@ -0,0 +1,98 @@
+import { BasedDb } from '../src/index.js'
+import test from './shared/test.js'
+import { start  as startMulti } from './shared/multi.js'
+import assert from 'node:assert'
+
+const N = 1e7 // Nodes
+const N2 = 1e3 // nr filter queries
+const N3 = 1e6 // nr simple gets
+
+const schema = {
+  types: {
+    test: {
+      props: {
+        x: 'uint32',
+        s: 'string',
+      },
+    },
+  },
+} as const
+
+const toxpsNum = (n, t) => (n / (t / 1000))
+const toxps = (n, t) => (n / (t / 1000)).toFixed(2)
+
+await test('test embedded', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+  await db.start({ clean: true })
+  t.after(() => t.backup(db))
+
+  await db.setSchema(schema)
+
+  let start = performance.now()
+
+  let i = N
+  while (i--) {
+    db.create('test', {
+        x: i % 100,
+        s: `hello ${i}`
+    })
+  }
+
+  await db.drain()
+  const ctime = performance.now() - start
+  console.log(ctime, 'ms', toxps(N, ctime), 'c/s')
+
+  start = performance.now()
+  let res = (await Promise.all(Array.from({ length: N2 }).map(() => db.query('test').filter('x', '=', 0).range(1, 10_001).get()))).reduce((prev, cur) => prev + cur.length, 0)
+  assert(res === 10000000)
+  const qtime = performance.now() - start
+  console.log(qtime, 'ms', toxps(N2, qtime), 'q/s')
+
+  start = performance.now()
+  res = (await Promise.all(Array.from({ length: N2 }).map((_, i) => db.query('test', i + 1).get()))).reduce((prev, cur) => prev + cur.length, 0)
+  const qtime1 = performance.now() - start
+  console.log(qtime1, 'ms', toxps(N3, qtime1), 'q/s')
+
+  assert(toxpsNum(N, ctime) > 1000000)
+  assert(toxpsNum(N2, qtime) > 500)
+  assert(toxpsNum(N3, qtime1) > 60000000)
+})
+
+await test('test client-server', async (t) => {
+  const {
+    clients: [client1],
+  } = await startMulti(t)
+
+  await client1.setSchema(schema)
+
+  let start = performance.now()
+
+  let i = N
+  while (i--) {
+    client1.create('test', {
+        x: i % 100,
+        s: `hello ${i}`
+    })
+  }
+
+  await client1.drain()
+  const ctime = performance.now() - start
+  console.log(ctime, 'ms', toxps(N, ctime), 'c/s')
+
+  start = performance.now()
+  let res = (await Promise.all(Array.from({ length: N2 }).map(() => client1.query('test').filter('x', '=', 0).range(1, 10_001).get()))).reduce((prev, cur) => prev + cur.length, 0)
+  assert(res === 10000000)
+  const qtime = performance.now() - start
+  console.log(qtime, 'ms', toxps(N2, qtime), 'q/s')
+
+  start = performance.now()
+  res = (await Promise.all(Array.from({ length: N2 }).map((_, i) => client1.query('test', i + 1).get()))).reduce((prev, cur) => prev + cur.length, 0)
+  const qtime1 = performance.now() - start
+  console.log(qtime1, 'ms', toxps(N3, qtime1), 'q/s')
+
+  assert(toxpsNum(N, ctime) > 1000000)
+  assert(toxpsNum(N2, qtime) > 500)
+  assert(toxpsNum(N3, qtime1) > 7000000)
+})
