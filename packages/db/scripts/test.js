5bd7df4daae41d3afa19b3de844639f3e0048a9f
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-08-23 19:24:01 +0200

    Replace picocolors with native styleText
    Ref FDN-1508

--- packages/db/scripts/test.js
+++ packages/db/scripts/test.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs/promises'
+import { styleText } from 'node:util'
 import { fileURLToPath } from 'url'
 import { join, dirname } from 'path'
-import picocolors from 'picocolors'
 import { wait } from '@based/utils'
 
 import { printSummary } from '../dist/test/shared/test.js'
@@ -61,11 +61,7 @@ const walk = async (dir = p) => {
 await walk(p)
 
 console.log('\n\n')
-console.log(
-  picocolors.bgWhite(
-    ` RUN ${testsToRun.length} file${testsToRun.length == 1 ? '' : 's'} `,
-  ),
-)
+console.log(styleText('bgWhite', ` RUN ${testsToRun.length} file${testsToRun.length == 1 ? '' : 's'} `))
 console.log('')
 
 let cnt = 0
@@ -78,9 +74,7 @@ for (let i = 0; i < repeat; i++) {
   for (const test of testsToRun) {
     const fullPath = test[0]
     const relPath = relative(p, fullPath)
-    console.log(
-      picocolors.bgBlue(` ${picocolors.bold(picocolors.black(relPath))} `),
-    )
+    console.log(styleText('bgBlue', ` ${styleText('bold', styleText('black', relPath))} `))
 
     if (test[1]) {
       process.env.TEST_TO_RUN = test[1]
@@ -95,7 +89,7 @@ for (let i = 0; i < repeat; i++) {
     await import(fullPath + `?_=${++cnt}`)
       .catch((err) => {
         console.log('')
-        console.log(picocolors.bgRed(` Err: ${relPath} `))
+        console.log(styleText('bgRed', ` Err: ${relPath} `))
         console.error(err)
       })
       .then(() => {})
