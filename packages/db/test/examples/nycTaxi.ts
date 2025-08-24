d1979dcf6e432461b9ebea7f62cdcd36f6d62e30
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-08-24 08:29:40 +0200

    logMemoryUsage() in test/shared

--- packages/db/test/examples/nycTaxi.ts
+++ packages/db/test/examples/nycTaxi.ts
@@ -5,6 +5,7 @@ import { readdir, readFile } from 'node:fs/promises'
 import { promisify } from 'node:util'
 import { gunzip as _gunzip } from 'zlib'
 import { Sema } from 'async-sema'
+import {logMemoryUsage} from '../shared/index.js'
 
 const gunzip = promisify(_gunzip)
 
@@ -1482,26 +1483,5 @@ await test('taxi', async (t) => {
     .get()
     .inspect()
 
-  const formatBytes = (bytes) => {
-    if (bytes < 1024) {
-      return bytes + ' Bytes'
-    } else if (bytes < 1024 * 1024) {
-      return (bytes / 1024).toFixed(2) + ' KB'
-    } else if (bytes < 1024 * 1024 * 1024) {
-      return (bytes / (1024 * 1024)).toFixed(2) + ' MB'
-    } else {
-      return (bytes / (1024 * 1024 * 1024)).toFixed(2) + ' GB'
-    }
-  }
-
-  const memoryUsage = process.memoryUsage()
-
-  console.log('Memory Usage:')
-  console.log(`  rss: ${formatBytes(memoryUsage.rss)}`)
-  console.log(`  heapTotal: ${formatBytes(memoryUsage.heapTotal)}`)
-  console.log(`  heapUsed: ${formatBytes(memoryUsage.heapUsed)}`)
-  console.log(`  external: ${formatBytes(memoryUsage.external)}`)
-  if (memoryUsage.arrayBuffers !== undefined) {
-    console.log(`  arrayBuffers: ${formatBytes(memoryUsage.arrayBuffers)}`)
-  }
+  logMemoryUsage()
 })
