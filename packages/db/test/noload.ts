eac6de57c6b8f4a758de0b41b23613185f77fee8
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-08-24 16:43:47 +0200

    Test partial refs

--- packages/db/test/noload.ts
+++ packages/db/test/noload.ts
@@ -1,8 +1,20 @@
+import assert from 'node:assert'
+import { fastPrng } from '@based/utils'
 import { BasedDb } from '../src/index.js'
 import { deepEqual } from './shared/assert.js'
 import test from './shared/test.js'
 import NAMES from './shared/names.js'
-import {makeTreeKey} from '../src/server/tree.js'
+import { makeTreeKey, VerifBlock } from '../src/server/tree.js'
+
+function makeEmployee(i: number) {
+  const name = NAMES[i % NAMES.length]
+  const email = `${name.toLowerCase().replace(' ', '.')}@yari.yo`
+  return {
+    name,
+    email,
+    reportsTo: null,
+  }
+}
 
 await test('noLoadDumps', async (t) => {
   const db = new BasedDb({
@@ -34,22 +46,18 @@ await test('noLoadDumps', async (t) => {
     reportsTo: 1,
   })
   for (let i = 0; i < 1500; i++) {
-    const name = NAMES[i % NAMES.length]
-    const email = `${name.toLowerCase().replace(' ', '.')}@yari.yo`
-    db.create('employee', {
-      name,
-      email,
-      reportsTo: i % 2 + 1,
-    })
+    const employee = makeEmployee(i)
+    employee.reportsTo = i % 2 + 1
+    db.create('employee', employee)
   }
 
   deepEqual(await db.query('employee').include('*').range(0, 2).get().toObject(), [
     { id: 1, name: 'youzi', email: 'youzi@yazi.yo' },
     { id: 2, name: 'doug', email: 'doug@yari.yo' }
   ])
+
   await db.drain()
   await db.save()
-
   const db2 = new BasedDb({
     path: t.tmp,
   })
@@ -85,4 +93,82 @@ await test('noLoadDumps', async (t) => {
   //     console.log(block)
   //   }
   // }
+})
+
+await test('references', async (t) => {
+  let db = new BasedDb({
+    path: t.tmp,
+  })
+  await db.start({ clean: true })
+  t.after(() => db.destroy())
+  const reload = async () => {
+    await db.drain()
+    await db.save()
+    const db2 = new BasedDb({
+      path: t.tmp,
+    })
+    await db2.start({ noLoadDumps: true })
+    t.after(() => db2.destroy())
+    db.destroy()
+    db = db2
+  }
+
+  await db.setSchema({
+    types: {
+      employee: {
+        blockCapacity: 1025,
+        props: {
+          name: { type: 'string', maxBytes: 25 },
+          email: { type: 'string', maxBytes: 40 },
+          reportsTo: { ref: 'employee', prop: 'subordinates' },
+        },
+      },
+      project: {
+        props: {
+          name: { type: 'string', maxBytes: 10 },
+          manager: { ref: 'employee', prop: 'project' },
+        },
+      },
+    },
+  })
+
+  const prng = fastPrng()
+  const rnd = () => prng(1, NAMES.length + 1)
+
+  for (let i = 0; i < 2000; i++) {
+    const employee = makeEmployee(i)
+    employee.reportsTo = rnd()
+    db.create('employee', employee, { unsafe: true })
+  }
+  for (let i = 0; i < 500; i++) {
+    db.create('project', {
+      name: NAMES[rnd()].split(' ')[0],
+      manager: rnd(),
+    })
+  }
+
+  await reload()
+  for (const type of db.server.verifTree.types()) {
+    for (const block of db.server.verifTree.blocks(type)) {
+      deepEqual(block.inmem, false)
+    }
+  }
+
+  // Test that a single block of a partially loaded type with references has
+  // the same hash as it originally had when the type was fully loaded.
+  // There is no assert for that directly because loadBlock would throw.
+  const p = db.server.loadBlock('employee', 1)
+  let block1: VerifBlock
+  for (const type of db.server.verifTree.types()) {
+    for (const block of db.server.verifTree.blocks(type)) {
+      if (block.key === 8589934593) {
+        assert(block.loadPromise)
+        deepEqual(block.inmem, false)
+        block1 = block
+      }
+    }
+  }
+  await p
+  assert(!block1.loadPromise)
+  deepEqual(block1.inmem, true)
 })
