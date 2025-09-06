9a953cbec92d148d11add89313e62a94c8d2e35c
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-09-06 09:13:16 +0200

    Should use mempool_return() to free a te

--- packages/db/clibs/lib/selva/db.c
+++ packages/db/clibs/lib/selva/db.c
@@ -344,7 +344,7 @@ int selva_db_create_type(struct SelvaDb *db, node_type_t type, const uint8_t *sc
     te->type = type;
     err = schemabuf_parse_ns(&te->ns, schema_buf, schema_len);
     if (err) {
-        selva_free(te);
+        mempool_return(&db->types.pool, te);
         return err;
     }
 
