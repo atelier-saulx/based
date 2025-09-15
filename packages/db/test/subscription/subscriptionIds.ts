6bd0b6950a9a95da17e04ee4241ccd4ee180053a
Jim de Beer <beerdejim@gmail.com>
2025-09-15 08:12:08 +0200

    sub ids

---
+++ packages/db/test/subscription/subscriptionIds.ts
@@ -0,0 +1,225 @@
+import { wait } from '@based/utils'
+import { DbClient } from '../../src/client/index.js'
+import { DbServer } from '../../src/server/index.js'
+import test from '../shared/test.js'
+import { equal } from '../shared/assert.js'
+import { italy } from '../shared/examples.js'
+import { getDefaultHooks } from '../../src/hooks.js'
+
+const start = async (t, clientsN = 2) => {
+  const server = new DbServer({
+    path: t.tmp,
+  })
+  const clients = Array.from({ length: clientsN }).map(
+    () =>
+      new DbClient({
+        hooks: getDefaultHooks(server, 100),
+      }),
+  )
+  await server.start({ clean: true })
+  t.after(() => server.destroy())
+  return { clients, server }
+}
+
+await test('subscription', async (t) => {
+  const clientsN = 2
+  const { clients } = await start(t, clientsN)
+
+  await clients[0].setSchema({
+    types: {
+      user: {
+        derp: 'uint8',
+        location: 'string',
+        lang: 'string',
+      },
+    },
+  })
+
+  const x = await clients[0].create('user', {
+    derp: 1,
+  })
+
+  let cnt = 0
+
+  const close = clients[1]
+    .query('user')
+    .include('derp')
+    .subscribe((q) => {
+      cnt++
+    })
+
+  let setCnt = 0
+  let interval = setInterval(async () => {
+    await clients[0].update('user', x, {
+      derp: { increment: 1 },
+    })
+    setCnt++
+  }, 200)
+
+  t.after(() => {
+    clearInterval(interval)
+  })
+
+  await wait(500)
+  clearInterval(interval)
+  close()
+
+  equal(cnt - 1, setCnt, 'Incoming subs is equal to sets')
+
+  await clients[1].create('user', {
+    lang: 'de',
+    location: 'hello',
+  })
+
+  const l = await clients[1].create('user', {
+    lang: 'en',
+    location: 'flap',
+  })
+
+  const close2 = clients[1]
+    .query('user', l)
+    .include('lang')
+    .subscribe((q) => {
+      cnt++
+    })
+
+  const langs = ['aa', 'bb', 'cc']
+
+  setCnt = 0
+  interval = setInterval(async () => {
+    await clients[0].update('user', l, {
+      lang: langs[setCnt % langs.length],
+    })
+    setCnt++
+  }, 200)
+
+  await wait(1000)
+  equal(setCnt > 2, true, 'Incoming subs fired 1 ')
+
+  clearInterval(interval)
+  close2()
+
+  setCnt = 0
+  let lastSet = 'flap'
+  interval = setInterval(async () => {
+    lastSet = italy.slice(setCnt, setCnt + 4)
+    await clients[0].update('user', l, {
+      location: lastSet,
+    })
+    setCnt++
+  }, 200)
+
+  const close3 = clients[1]
+    .query('user', l)
+    .include('location')
+    .subscribe((q) => {
+      equal(lastSet, q.node(0).location, 'equals to last set')
+      cnt++
+    })
+
+  await wait(1000)
+  equal(setCnt > 3, true, 'Incoming subs fired 2')
+
+  clearInterval(interval)
+  close3()
+
+  await wait(1000)
+})
+
+await test('subscription error', async (t) => {
+  const clientsN = 2
+  const { clients } = await start(t, clientsN)
+
+  await clients[0].setSchema({
+    types: {
+      user: {
+        derp: 'uint8',
+        location: 'string',
+        lang: 'string',
+      },
+    },
+  })
+
+  const x = await clients[0].create('user', {
+    derp: 1,
+  })
+
+  const close = clients[1]
+    .query('user')
+    .include('derp')
+    .subscribe(
+      (q) => {
+        // @ts-ignore
+        console.log(q.flap.x)
+      },
+      (err) => {
+        console.error(err)
+      },
+    )
+
+  await wait(1000)
+
+  close()
+})
+
+await test('subscribe to refs', async (t) => {
+  const clientsN = 2
+  const { clients } = await start(t, clientsN)
+
+  await clients[0].setSchema({
+    types: {
+      queue: {
+        props: {
+          name: 'string',
+        },
+      },
+      queueItem: {
+        props: {
+          queue: {
+            ref: 'queue',
+            prop: 'items',
+          },
+          data: 'string',
+        },
+      },
+    },
+  })
+
+  const queueId = await clients[0].create('queue', { name: 'test' })
+
+  let updatesReceived = 0
+  let size = 0
+  const close = clients[1]
+    .query('queue', queueId)
+    .include('items')
+    .subscribe((q) => {
+      updatesReceived++
+      const res = q.toObject()
+      console.log(res.items.length)
+      size = res.items.length
+      const n = performance.now()
+      console.log(updatesReceived, 'update received after', n - d)
+    })
+
+  const d = performance.now()
+
+  for (let i = 0; i < 1e6; i++) {
+    await clients[0].create('queueItem', { data: `Item-${i}`, queue: queueId })
+  }
+
+  // console.log(
+  //   await clients[1]
+  //     .query('queue', queueId)
+  //     .include('items')
+  //     .count('items')
+  //     .get()
+  //     // .inspect(),
+  //   .toObject(),
+  // )
+
+  // { items: 1 }
+
+  await wait(700)
+
+  close()
+})
