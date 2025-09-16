4b9f00782fcf74290ccfcfdee629fa5a879ddb2e
Jim de Beer <beerdejim@gmail.com>
2025-09-16 09:36:43 +0200

    fix

--- packages/db/test/subscription/subscriptionIds.ts
+++ packages/db/test/subscription/subscriptionIds.ts
@@ -119,17 +119,13 @@ await test('subscription perf', async (t) => {
 
   const p = []
   let cnt = 0
-  for (let i = 0; i < 12; i++) {
-    // clientWorker()
-
+  for (let i = 0; i < 10; i++) {
     p.push(
       clientWorker(
         t,
         db,
         async (client, { ctx }, { native, utils }) => {
-          // address: BigInt ctx = BigInt
           const dbCtx = native.externalFromInt(ctx)
-
           await client.schemaIsSet()
           const q = client.query('user', 1)
           const y = await q.get()
@@ -144,14 +140,6 @@ await test('subscription perf', async (t) => {
         { ctx: native.intFromExternal(db.server.dbCtxExternal) },
       ),
     )
-
-    // const address: BigInt = native.intFromExternal(db.dbCtxExternal)
-
-    // writeUint32(q.buffer, i + 1, 4)
-    // native.getQueryBuf(q.buffer, db.server.dbCtxExternal)
-    // db.server.getQueryBuf(q.buffer).then((v) => {
-    //   cnt++
-    // }))
   }
 
   await Promise.all(p)
