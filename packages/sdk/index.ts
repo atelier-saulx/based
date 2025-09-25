4ac9bc682a6b60725ca1e80cd903f34f797b3605
Marco Vieira <maovieira@gmail.com>
2025-09-25 16:49:01 -0300

    barrelTest

--- packages/sdk/index.ts
+++ packages/sdk/index.ts
@@ -1,5 +1,12 @@
+export * from '@based/db'
+export * from '@based/schema'
+
 import * as db from '@based/db'
 import * as schema from '@based/schema'
-import * as utils from '@based/utils'
 
-export { db, schema, utils }
+export { db, schema }
+
+export default {
+  db,
+  schema,
+}
