a62ee675b6fe314dac4d3ca8bea1128af190a273
Marco Vieira <maovieira@gmail.com>
2025-09-25 20:17:15 -0300

    added sdk pkg

---
+++ packages/sdk/src/index.ts
@@ -0,0 +1,12 @@
+export * from '@based/db'
+export * from '@based/schema'
+
+import * as db from '@based/db'
+import * as schema from '@based/schema'
+
+export { db, schema }
+
+export default {
+  db,
+  schema,
+}
