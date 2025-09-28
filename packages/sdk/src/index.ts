552f0a8ea447d9cf39b4524b618ad2d42b4bf5a7
Marco Vieira <maovieira@gmail.com>
2025-09-27 21:05:48 -0300

    added hash, protocol and utils to sdk

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,15 +1,24 @@
 export * from '@based/db'
 export * from '@based/schema'
 export * from '@based/utils'
+export * from '@based/hash'
+export * from '@based/protocol'
 
 import * as db from '@based/db'
 import * as schema from '@based/schema'
 import * as utils from '@based/utils'
+import * as hash from '@based/hash'
+import * as protocol from '@based/protocol'
 
-export { db, schema, utils }
+export * from '@based/hash'
+export * from '@based/protocol'
+
+export { db, schema, utils, hash, protocol }
 
 export default {
   db,
   schema,
   utils,
+  hash,
+  protocol,
 }
