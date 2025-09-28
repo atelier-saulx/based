991df2e80a8abd2c12f21b3db67c02481e56b23c
Marco Vieira <maovieira@gmail.com>
2025-09-27 22:44:01 -0300

    added errors to sdk

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -4,6 +4,7 @@ export * from '@based/utils'
 export * from '@based/hash'
 export * from '@based/protocol'
 export * from '@based/functions'
+export * from '@based/errors'
 
 import * as db from '@based/db'
 import * as schema from '@based/schema'
@@ -11,8 +12,9 @@ import * as utils from '@based/utils'
 import * as hash from '@based/hash'
 import * as protocol from '@based/protocol'
 import * as functions from '@based/functions'
+import * as errors from '@based/errors'
 
-export { db, schema, utils, hash, protocol, functions }
+export { db, schema, utils, hash, protocol, functions, errors }
 
 export default {
   db,
@@ -21,4 +23,5 @@ export default {
   hash,
   protocol,
   functions,
+  errors,
 }
