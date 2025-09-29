25216ddd3de802950784efa02a4f16c7b0359fe0
Marco Vieira <maovieira@gmail.com>
2025-09-29 14:38:59 -0300

    adding server, client and react

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -5,6 +5,9 @@ export * from '@based/hash'
 export * from '@based/protocol'
 export * from '@based/functions'
 export * from '@based/errors'
+export * from '@based/server'
+export * from '@based/type-gen'
+export * from '@based/client'
 export * from '@based/react'
 
 import * as db from '@based/db'
@@ -14,9 +17,24 @@ import * as hash from '@based/hash'
 import * as protocol from '@based/protocol'
 import * as functions from '@based/functions'
 import * as errors from '@based/errors'
+import * as server from '@based/server'
+import * as typeGen from '@based/type-gen'
+import * as client from '@based/client'
 import * as react from '@based/react'
 
-export { db, schema, utils, hash, protocol, functions, errors, react }
+export {
+  db,
+  schema,
+  utils,
+  hash,
+  protocol,
+  functions,
+  errors,
+  server,
+  typeGen,
+  client,
+  react,
+}
 
 export default {
   db,
@@ -26,5 +44,8 @@ export default {
   protocol,
   functions,
   errors,
+  server,
+  typeGen,
+  client,
   react,
 }
