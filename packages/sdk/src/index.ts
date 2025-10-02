b93f864b8cfbb73c15a9c7da9793f57f16ca5f18
Marco Vieira <maovieira@gmail.com>
2025-10-02 11:15:22 -0300

    fix exporting

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -11,45 +11,45 @@ export * from '@based/client'
 export * from '@based/react'
 export * from '@based/schema-diagram'
 
-import * as db from '@based/db'
-import * as schema from '@based/schema'
-import * as utils from '@based/utils'
-import * as hash from '@based/hash'
-import * as protocol from '@based/protocol'
-import * as functions from '@based/functions'
-import * as errors from '@based/errors'
-import * as server from '@based/server'
-import * as typeGen from '@based/type-gen'
-import * as client from '@based/client'
-import * as react from '@based/react'
-import * as schemaDiagram from '@based/schema-diagram'
+// import * as db from '@based/db'
+// import * as schema from '@based/schema'
+// import * as utils from '@based/utils'
+// import * as hash from '@based/hash'
+// import * as protocol from '@based/protocol'
+// import * as functions from '@based/functions'
+// import * as errors from '@based/errors'
+// import * as server from '@based/server'
+// import * as typeGen from '@based/type-gen'
+// import * as client from '@based/client'
+// import * as react from '@based/react'
+// import * as schemaDiagram from '@based/schema-diagram'
 
-export {
-  db,
-  schema,
-  utils,
-  hash,
-  protocol,
-  functions,
-  errors,
-  server,
-  typeGen,
-  client,
-  react,
-  schemaDiagram,
-}
+// export {
+//   db,
+//   schema,
+//   utils,
+//   hash,
+//   protocol,
+//   functions,
+//   errors,
+//   server,
+//   typeGen,
+//   client,
+//   react,
+//   schemaDiagram,
+// }
 
-export default {
-  db,
-  schema,
-  utils,
-  hash,
-  protocol,
-  functions,
-  errors,
-  server,
-  typeGen,
-  client,
-  react,
-  schemaDiagram,
-}
+// export default {
+//   db,
+//   schema,
+//   utils,
+//   hash,
+//   protocol,
+//   functions,
+//   errors,
+//   server,
+//   typeGen,
+//   client,
+//   react,
+//   schemaDiagram,
+// }
