1af6311e02a2fd0444f7961f57c7115f32256ccb
youzi <youri@saulx.com>
2025-10-02 16:48:13 +0200

    updates towards browser compat

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,55 +1,13 @@
-export * from '@based/db'
+// export * from '@based/db'
+// export * from '@based/server'
 export * from '@based/schema'
 export * from '@based/utils'
 export * from '@based/hash'
 export * from '@based/protocol'
 export * from '@based/functions'
 export * from '@based/errors'
-export * from '@based/server'
 export * from '@based/type-gen'
 export * from '@based/client'
 export * from '@based/react'
 export * from '@based/schema-diagram'
-
-// import * as db from '@based/db'
-// import * as schema from '@based/schema'
-// import * as utils from '@based/utils'
-// import * as hash from '@based/hash'
-// import * as protocol from '@based/protocol'
-// import * as functions from '@based/functions'
-// import * as errors from '@based/errors'
-// import * as server from '@based/server'
-// import * as typeGen from '@based/type-gen'
-// import * as client from '@based/client'
-// import * as react from '@based/react'
-// import * as schemaDiagram from '@based/schema-diagram'
-
-// export {
-//   db,
-//   schema,
-//   utils,
-//   hash,
-//   protocol,
-//   functions,
-//   errors,
-//   server,
-//   typeGen,
-//   client,
-//   react,
-//   schemaDiagram,
-// }
-
-// export default {
-//   db,
-//   schema,
-//   utils,
-//   hash,
-//   protocol,
-//   functions,
-//   errors,
-//   server,
-//   typeGen,
-//   client,
-//   react,
-//   schemaDiagram,
-// }
+export { default as based } from '@based/client'
