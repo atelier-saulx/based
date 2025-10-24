e925b4bb4a34381d5568fc8ac4b64388d3e52b08
Marco Vieira <maovieira@gmail.com>
2025-10-24 18:59:56 -0300

    added test for json on edge

--- packages/db/test/edges/edgeType.ts
+++ packages/db/test/edges/edgeType.ts
@@ -1,6 +1,6 @@
 import { BasedDb } from '../../src/index.js'
 import test from '../shared/test.js'
-import { deepEqual } from '../shared/assert.js'
+import { deepEqual, equal } from '../shared/assert.js'
 
 await test('single reference', async (t) => {
   const db = new BasedDb({
@@ -38,8 +38,8 @@ await test('single reference', async (t) => {
   })
 
   const mr = await Promise.all([
-    db.create('user', { name: 'Mr Drol', }),
-    db.create('user', { name: 'Mr Derp', }),
+    db.create('user', { name: 'Mr Drol' }),
+    db.create('user', { name: 'Mr Derp' }),
   ])
 
   await db.create('article', {
@@ -52,6 +52,78 @@ await test('single reference', async (t) => {
 
   await db.query('article').include('*', '**').get().inspect()
   await db.query('_article:author').include('*').get().inspect()
-  deepEqual([...db.server.verifTree.types()].map((type) => type.typeId), [2, 3, 4])
+  deepEqual(
+    [...db.server.verifTree.types()].map((type) => type.typeId),
+    [2, 3, 4],
+  )
   deepEqual(db.server.dirtyRanges.size, 3)
+})
+
+await test('json type edge', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+
+  await db.start({ clean: true })
+  t.after(() => db.stop())
+
+  await db.setSchema({
+    types: {
+      workspace: {
+        name: 'string',
+      },
+      serviceAccount: {
+        username: 'alias',
+        passwordHash: 'string',
+        expiresAt: 'timestamp',
+
+        workspaces: {
+          items: {
+            ref: 'workspace',
+            prop: 'serviceAccounts',
+            $permissionsString: 'string',
+            $permissionsJson: 'json',
+          },
+        },
+      },
+    },
+  })
+
+  const workspaceId = await db.create('workspace', { name: 'Workspace A' })
+  const serviceAccountId = await db.create('serviceAccount', {
+    username: 'service-account-1',
+    workspaces: [
+      {
+        id: workspaceId,
+        $permissionsString: JSON.stringify({ key: 'value' }),
+        $permissionsJson: { key: 'value' },
+      },
+    ],
+  })
+  const retrieved = await db
+    .query('serviceAccount', serviceAccountId)
+    .include(
+      'id',
+      'workspaces.id',
+      'workspaces.$permissionsString',
+      'workspaces.$permissionsJson',
+    )
+    .get()
+    .toObject()
+
+  equal(retrieved?.workspaces.length, 1, 'Expected to have length 1')
+  deepEqual(
+    retrieved,
+    {
+      id: 1,
+      workspaces: [
+        {
+          id: 1,
+          $permissionJson: '{"key":"value"}',
+          $permissionsString: '{"key":"value"}',
+        },
+      ],
+    },
+    'Expected json + string to be equal',
+  )
 })
