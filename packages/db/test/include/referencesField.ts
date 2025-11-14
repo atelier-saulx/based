84ebdf2df46427b6238e3fe228cd27d75c1437ff
Jim de Beer <beerdejim@gmail.com>
2025-11-14 14:40:37 +0100

    references index in filter

---
+++ packages/db/test/include/referencesField.ts
@@ -0,0 +1,49 @@
+import test from '../shared/test.js'
+import { BasedDb } from '../../src/index.js'
+import { deepEqual } from '../shared/assert.js'
+
+await test('references shortcut', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+  await db.start({ clean: true })
+  t.after(() => t.backup(db))
+
+  await db.setSchema({
+    types: {
+      user: {
+        props: {
+          name: 'string',
+          age: 'uint8',
+          friends: {
+            items: {
+              ref: 'user',
+              prop: 'friends',
+            },
+          },
+        },
+      },
+    },
+  })
+
+  const mrA = await db.create('user', {
+    name: 'Mr a',
+    age: 50,
+    friends: [db.create('user', { name: 'Mr b', age: 25 })],
+  })
+
+  for (let i = 0; i < 10; i++) {
+    db.create('user', { name: 'Mr ' + i, age: 92 + i, friends: [mrA] })
+  }
+
+  deepEqual(
+    await db.query('user', mrA).include('name', 'age', 'friends[0]').get(),
+    {
+      id: 2,
+      age: 50,
+      name: 'Mr a',
+      friends: [{ id: 1, age: 25, name: 'Mr b' }],
+    },
+    '[0]',
+  )
+})
