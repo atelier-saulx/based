74d5a68ee5c234f954cc355756bebc5909e9e927
Marco Vieira <maovieira@gmail.com>
2025-11-20 17:53:48 +0100

    more extracts to perfs

--- packages/db/test/crc32c.ts
+++ packages/db/test/crc32c.ts
@@ -7,21 +7,6 @@ import { langCodesMap } from '@based/schema'
 
 const ENCODER = new TextEncoder()
 
-await test('Hash generation PERF', async (t) => {
-  const val = ENCODER.encode(`oid${1e6}`)
-  console.time('1E6 CRC32c TS')
-  for (let i = 0; i < 1e6; i++) {
-    crc32c(val)
-  }
-  console.timeEnd(`1E6 CRC32c TS`)
-
-  console.time('1E6 CRC32c Native')
-  for (let i = 0; i < 1e6; i++) {
-    nativeCrc32(val)
-  }
-  console.timeEnd(`1E6 CRC32c Native`)
-})
-
 await test('Comparing hash generation collision', async (t) => {
   let crc32set = new Set()
   let collision = 0
