378a848cffd9a083cbc9448000b469e90fcfb23d
Marco Vieira <maovieira@gmail.com>
2025-11-20 12:30:45 +0100

    alias perf tests

---
+++ packages/db/test/alias/aliasOps.perf.ts
@@ -0,0 +1,60 @@
+import { BasedDb } from '../../src/index.js'
+import test from '../shared/test.js'
+import { deepEqual, equal } from '../shared/assert.js'
+
+await test('await updates', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+  await db.start({ clean: true })
+  t.after(() => t.backup(db))
+
+  const status = ['a', 'b', 'c', 'd', 'e', 'f']
+
+  await db.setSchema({
+    types: {
+      user: {
+        props: {
+          externalId: { type: 'alias' },
+          status,
+        },
+      },
+    },
+  })
+
+  const total = 10e6
+
+  for (let i = 0; i < total; i++) {
+    db.create('user', {
+      externalId: i + '-alias',
+      status: 'a',
+    })
+  }
+
+  await db.isModified()
+
+  let totalAlias = 0
+
+  const updateAlias = async () => {
+    const externalId = Math.ceil(Math.random() * total) + '-alias'
+    const id = Math.ceil(Math.random() * total)
+    await db.update('user', id, {
+      externalId,
+    })
+    totalAlias++
+  }
+
+  const start = performance.now()
+  //let lastMeasure = performance.now()
+  for (let i = 0; i < 100000; i++) {
+    await updateAlias()
+    if (!(i % 10000)) {
+      //const opsPerS = totalAlias / ((performance.now() - lastMeasure) / 1e3)
+      //console.log(`${~~opsPerS} per sec`)
+      //lastMeasure = performance.now()
+      totalAlias = 0
+    }
+  }
+
+  equal(performance.now() - start < 5e3, true, 'should be smaller then 5s')
+})
