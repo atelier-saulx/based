84c1504dbb4273168292d99147167fdc1614f14b
Marco Vieira <maovieira@gmail.com>
2025-11-20 12:31:14 +0100

    fix import

--- packages/db/test/aggregate/overall.perf.ts
+++ packages/db/test/aggregate/overall.perf.ts
@@ -2,6 +2,7 @@ import { BasedDb } from '../../src/index.js'
 import test from '../shared/test.js'
 import { deepEqual } from '../shared/assert.js'
 import { fastPrng } from '@based/utils'
+import { equal } from 'node:assert'
 
 await test('overall performance', async (t) => {
   const db = new BasedDb({
