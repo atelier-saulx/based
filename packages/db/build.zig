ba84ad723bda39d52757dca3b099a0cf1649077c
youzi <youri@saulx.com>
2025-11-24 17:23:37 +0100

    added files for zls

--- packages/db/build.zig
+++ packages/db/build.zig
@@ -25,14 +25,7 @@ pub fn build(b: *std.Build) void {
     });
 
     lib.linker_allow_shlib_undefined = true;
-
     lib.root_module.addOptions("config", options);
-    //     {
-    //   target: 'aarch64-macos',
-    //   nodeHeadersPath: 'deps/node-v22.21.1',
-    //   rpath: '@loader_path',
-    //   libSelvaPath: 'dist/lib/darwin_aarch64'
-    // }
     const node_hpath = b.option([]const u8, "node_hpath", "Path to the Node.js headers") orelse "deps/node-v22.21.1/include/node";
     lib.addIncludePath(b.path(node_hpath));
 
@@ -41,11 +34,9 @@ pub fn build(b: *std.Build) void {
     const headers_selva_path = b.option([]const u8, "headersselvapath", "Path to the Selva Headers") orelse "dist/lib/darwin_aarch64/include";
 
     lib.root_module.addRPathSpecial(rpath);
-
     lib.addIncludePath(b.path(headers_selva_path));
     lib.addLibraryPath(b.path(lib_selva_path));
     lib.linkSystemLibrary("selva");
-
     lib.linkLibC();
 
     const install_lib = b.addInstallArtifact(lib, .{
