02aed2770ba635e6ab9cc06369c137193bc115a9
Marco Vieira <maovieira@gmail.com>
2025-11-24 11:20:38 +0100

    fix perf funny import

--- packages/db/test/shared/perf.ts
+++ packages/db/test/shared/perf.ts
@@ -2,7 +2,6 @@ import * as fs from 'fs'
 import * as path from 'path'
 import { performance } from 'perf_hooks'
 import { styleText } from 'util'
-import { difference } from '../../../schema/dist/parse/semver/difference.js'
 
 const MEASURES_PER_TEST = 10
 
