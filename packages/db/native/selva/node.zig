1fcf906a099b1468b2b1abd28386853231dd7ba7
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-11-24 20:49:34 +0100

    Cleanup imports

--- packages/db/native/selva/node.zig
+++ packages/db/native/selva/node.zig
@@ -1,54 +1,53 @@
 const std = @import("std");
-const selva = @import("selva.zig").c;
-const st = @import("selva.zig");
+const selva = @import("selva.zig");
 const errors = @import("../errors.zig");
 const utils = @import("../utils.zig");
 const Modify = @import("../modify/common.zig");
-const DbCtx = @import("../db/ctx.zig").DbCtx;
+const Db = @import("../db/ctx.zig");
 const t = @import("../types.zig");
 
-pub const Type = st.Type;
-pub const Node = st.Node;
+pub const Type = selva.Type;
+pub const Node = selva.Node;
 
 pub inline fn getNodeId(node: Node) u32 {
     return utils.read(u32, @as([*]u8, @ptrCast(node))[0..4], 0);
 }
 
 pub inline fn getNodeTypeId(node: Node) t.TypeId {
-    return selva.selva_get_node_type(node);
+    return selva.c.selva_get_node_type(node);
 }
 
-pub fn upsertNode(ctx: *Modify.ModifyCtx, typeEntry: st.Type, id: u32) !Node {
-    const node = selva.selva_upsert_node(ctx.db.selva, typeEntry, id);
+pub fn upsertNode(ctx: *Modify.ModifyCtx, typeEntry: selva.Type, id: u32) !Node {
+    const node = selva.c.selva_upsert_node(ctx.db.selva, typeEntry, id);
     if (node == null) {
         return errors.SelvaError.SELVA_CANNOT_UPSERT;
     }
     return node.?;
 }
 
-pub fn getNode(typeEntry: st.Type, id: u32) ?Node {
-    return selva.selva_find_node(typeEntry, id);
+pub fn getNode(typeEntry: selva.Type, id: u32) ?Node {
+    return selva.c.selva_find_node(typeEntry, id);
 }
 
-pub inline fn getFirstNode(typeEntry: st.Type) ?Node {
-    return selva.selva_min_node(typeEntry);
+pub inline fn getFirstNode(typeEntry: selva.Type) ?Node {
+    return selva.c.selva_min_node(typeEntry);
 }
 
-pub inline fn getLastNode(typeEntry: st.Type) ?Node {
-    return selva.selva_max_node(typeEntry);
+pub inline fn getLastNode(typeEntry: selva.Type) ?Node {
+    return selva.c.selva_max_node(typeEntry);
 }
 
-pub inline fn getNextNode(typeEntry: st.Type, node: Node) ?Node {
-    return selva.selva_next_node(typeEntry, node);
+pub inline fn getNextNode(typeEntry: selva.Type, node: Node) ?Node {
+    return selva.c.selva_next_node(typeEntry, node);
 }
 
-pub inline fn getPrevNode(typeEntry: st.Type, node: Node) ?Node {
-    return selva.selva_prev_node(typeEntry, node);
+pub inline fn getPrevNode(typeEntry: selva.Type, node: Node) ?Node {
+    return selva.c.selva_prev_node(typeEntry, node);
 }
 
 pub const TypeIterator = struct {
     comptime desc: bool = false,
-    typeEntry: st.Type,
+    typeEntry: selva.Type,
     node: ?Node = null,
     fn init(s: *const TypeIterator) TypeIterator {
         if (s.desc) {
@@ -78,20 +77,20 @@ pub const TypeIterator = struct {
     }
 };
 
-pub inline fn getNodeFromReference(dstType: st.Type, ref: anytype) ?Node {
-    if (comptime @TypeOf(ref) == st.ReferenceSmall or
-        @TypeOf(ref) == st.ReferenceLarge or
-        @TypeOf(ref) == *allowzero selva.SelvaNodeSmallReference or
-        @TypeOf(ref) == *allowzero selva.SelvaNodeLargeReference)
+pub inline fn getNodeFromReference(dstType: selva.Type, ref: anytype) ?Node {
+    if (comptime @TypeOf(ref) == selva.ReferenceSmall or
+        @TypeOf(ref) == selva.ReferenceLarge or
+        @TypeOf(ref) == *allowzero selva.c.SelvaNodeSmallReference or
+        @TypeOf(ref) == *allowzero selva.c.SelvaNodeLargeReference)
     {
-        return selva.selva_find_node(dstType, ref.*.dst);
-    } else if (comptime @TypeOf(ref) == ?st.ReferenceSmall or
-        @TypeOf(ref) == ?st.ReferenceLarge or
-        @TypeOf(ref) == ?*selva.SelvaNodeSmallReference or
-        @TypeOf(ref) == ?*selva.SelvaNodeLargeReference)
+        return selva.c.selva_find_node(dstType, ref.*.dst);
+    } else if (comptime @TypeOf(ref) == ?selva.ReferenceSmall or
+        @TypeOf(ref) == ?selva.ReferenceLarge or
+        @TypeOf(ref) == ?*selva.c.SelvaNodeSmallReference or
+        @TypeOf(ref) == ?*selva.c.SelvaNodeLargeReference)
     {
         if (ref) |r| {
-            return selva.selva_find_node(dstType, r.*.dst);
+            return selva.c.selva_find_node(dstType, r.*.dst);
         }
     } else {
         @compileLog("Invalid type: ", @TypeOf(ref));
@@ -100,8 +99,8 @@ pub inline fn getNodeFromReference(dstType: st.Type, ref: anytype) ?Node {
     return null;
 }
 
-pub fn ensureRefEdgeNode(ctx: *Modify.ModifyCtx, node: Node, efc: st.EdgeFieldConstraint, ref: st.ReferenceLarge) !Node {
-    const edgeNode = selva.selva_fields_ensure_ref_edge(ctx.db.selva, node, efc, ref, 0, st.markDirtyCb, ctx);
+pub fn ensureRefEdgeNode(ctx: *Modify.ModifyCtx, node: Node, efc: selva.EdgeFieldConstraint, ref: selva.ReferenceLarge) !Node {
+    const edgeNode = selva.c.selva_fields_ensure_ref_edge(ctx.db.selva, node, efc, ref, 0, selva.markDirtyCb, ctx);
     if (edgeNode) |n| {
         Modify.markDirtyRange(ctx, efc.edge_node_type, getNodeId(n));
         return n;
@@ -110,11 +109,11 @@ pub fn ensureRefEdgeNode(ctx: *Modify.ModifyCtx, node: Node, efc: st.EdgeFieldCo
     }
 }
 
-pub fn getEdgeNode(db: *DbCtx, efc: st.EdgeFieldConstraint, ref: st.ReferenceLarge) ?Node {
+pub fn getEdgeNode(db: *Db.DbCtx, efc: selva.EdgeFieldConstraint, ref: selva.ReferenceLarge) ?Node {
     if (ref.*.edge == 0) {
         return null;
     }
 
-    const edge_type = selva.selva_get_type_by_index(db.selva, efc.*.edge_node_type);
-    return selva.selva_find_node(edge_type, ref.*.edge);
+    const edge_type = selva.c.selva_get_type_by_index(db.selva, efc.*.edge_node_type);
+    return selva.c.selva_find_node(edge_type, ref.*.edge);
 }
