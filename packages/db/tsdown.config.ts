eaecbe7fdd36e9e2babe2f76ce90d74427f11b1d
youzi <youri@saulx.com>
2025-11-25 23:03:45 +0100

    wip

--- packages/db/tsdown.config.ts
+++ packages/db/tsdown.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   clean: ['dist/**', '!dist/lib/**'],
+  minify: true,
+  fixedExtension: false,
   entry: {
     sdk: './src/sdk.ts',
     db: './src/index.ts',
