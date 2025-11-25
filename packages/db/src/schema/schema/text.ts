fd0d8cad5c1b5d7f30f84164919882c908d1c1b1
youzi <youri@saulx.com>
2025-11-25 18:04:42 +0100

    add compression and format to text

--- packages/db/src/schema/schema/text.ts
+++ packages/db/src/schema/schema/text.ts
@@ -1,7 +1,14 @@
 import { parseBase, type Base } from './base.js'
 import type { SchemaOut } from './schema.js'
 import { assert, isRecord, isString } from './shared.js'
-import type { StringCompression, StringFormat } from './string.js'
+import {
+  isCompression,
+  isFormat,
+  stringCompressions,
+  stringFormats,
+  type StringCompression,
+  type StringFormat,
+} from './string.js'
 
 export type SchemaText = Base & {
   type: 'text'
@@ -18,6 +25,14 @@ export const parseText = (
     isRecord(locales) && Object.keys(locales).length > 0,
     'Locales should be defined',
   )
+  assert(
+    def.compression === undefined || isCompression(def.compression),
+    `Compression should be one of ${stringCompressions.join(', ')}`,
+  )
+  assert(
+    def.format === undefined || isFormat(def.format),
+    `Format should be one of ${stringFormats.join(', ')}`,
+  )
   if (def.default) {
     assert(isRecord(def.default), 'Default should be record of strings')
     assert(
@@ -28,5 +43,7 @@ export const parseText = (
   return parseBase<SchemaText>(def, {
     type: 'text',
     default: def.default as SchemaText['default'],
+    compression: def.compression,
+    format: def.format,
   })
 }
