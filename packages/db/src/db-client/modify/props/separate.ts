bc49812b36bcd9d998c5ea671f453d20f6f08be6
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-11-25 21:53:49 +0100

    Remove default alias support

--- packages/db/src/db-client/modify/props/separate.ts
+++ packages/db/src/db-client/modify/props/separate.ts
@@ -1,10 +1,8 @@
 import { Ctx } from '../Ctx.js'
 import { writeReference } from './reference.js'
-import { writeString } from './string.js'
 import { writeText } from './text.js'
 import { writeReferences } from './references.js'
 import { writeBinary } from './binary.js'
-import { writeAlias } from './alias.js'
 import { writeCardinality } from './cardinality.js'
 import { writeVector } from './vector.js'
 import { writeJson } from './json.js'
@@ -22,8 +20,6 @@ export const writeSeparate = (ctx: Ctx, def: PropDef, val: any) => {
     writeReferences(ctx, def, val)
   } else if (type === PropType.binary) {
     writeBinary(ctx, def, val)
-  } else if (type === PropType.alias) {
-    writeAlias(ctx, def, val)
   } else if (type === PropType.cardinality) {
     writeCardinality(ctx, def, val)
   } else if (type === PropType.vector || type === PropType.colVec) {
