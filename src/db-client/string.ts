4a0d27be3a9a22d099e39ff07ad5dc8281f712a6
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-11-26 13:46:47 +0100

    Move string writing

--- src/db-client/string.ts
+++ src/db-client/string.ts
@@ -7,6 +7,46 @@ import { LangCodeEnum } from '../zigTsExports.js'
 
 const { getUint8Array: getTmpBuffer } = makeTmpBuffer(4096) // the usual page size?
 
+export function writeRaw(dst: Uint8Array, s: Uint8Array | string, offset: number, lang: LangCodeEnum, noCompression: boolean): number {
+  if (s instanceof Uint8Array) {
+    dst.set(s, offset)
+    return dst.byteLength
+  } else if (noCompression) {
+    const value = s.normalize('NFKD')
+    dst[offset] = lang
+    dst[offset + 1] = NOT_COMPRESSED
+    const l = native.stringToUint8Array(value, dst, offset + 2)
+    let crc = native.crc32(dst.subarray(offset + 2, offset + 2 + l))
+    writeUint32(dst, crc, offset + 2 + l)
+    return l + 6;
+  } else { // Try to compress
+    const value = s.normalize('NFKD')
+    const l = native.stringByteLength(value)
+
+    if (l <= 200 || dst.byteLength - offset < 2 * l + 10) {
+      return writeRaw(dst, s, offset, lang, true)
+    }
+
+    dst[offset] = lang
+    dst[offset + 1] = COMPRESSED
+
+    const insertPos = offset + 6 + l
+    const endPos = insertPos + l
+    native.stringToUint8Array(value, dst, insertPos)
+    const crc = native.crc32(dst.subarray(insertPos, endPos))
+    const size = native.compress(dst, offset + 6, l)
+    if (size == 0) {
+      // Didn't compress
+      return writeRaw(dst, s, offset, lang, true)
+    }
+
+    writeUint32(dst, l, offset + 2) // uncompressed size
+    writeUint32(dst, crc, offset + size + 6)
+    return size + 10
+  }
+}
+
+// TODO use writeRAw() here
 export const write = (
   ctx: Ctx,
   value: string,
