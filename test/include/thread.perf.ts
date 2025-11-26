48b16314c5756494419a9ddf266ec450acbd578b
Jim de Beer <beerdejim@gmail.com>
2025-11-26 12:33:19 +0100

    fix text fallback

--- test/include/thread.perf.ts
+++ test/include/thread.perf.ts
@@ -59,27 +59,27 @@ await test('include', async (t) => {
 
   // await db.query('user').include('name', 'body').range(0, 1).get().inspect()
 
-  // await perf(
-  //   async () => {
-  //     const q: any[] = []
-  //     for (let i = 0; i < 1000; i++) {
-  //       q.push(
-  //         db
-  //           .query('user')
-  //           .locale('de')
-  //           .include('name', 'body')
-  //           // .include('name', 'body', { end: 2 })
-  //           .range(0, 1000 + i)
-  //           .get(),
-  //         // .inspect(),
-  //         // .inspect(),
-  //       )
-  //     }
-  //     await Promise.all(q)
-  //   },
-  //   'Nodes',
-  //   { repeat: 10 },
-  // )
+  await perf(
+    async () => {
+      const q: any[] = []
+      for (let i = 0; i < 1000; i++) {
+        q.push(
+          db
+            .query('user')
+            .locale('de', [])
+            .include('name', 'body')
+            // .include('name', 'body', { end: 2 })
+            .range(0, 1000 + i)
+            .get(),
+          // .inspect(),
+          // .inspect(),
+        )
+      }
+      await Promise.all(q)
+    },
+    'Nodes',
+    { repeat: 10 },
+  )
 
   console.log('done')
 
