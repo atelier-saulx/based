2111ef5ad4393df3047636b9d53b5a8c2092321d
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-11-26 14:18:37 +0100

    Replace string writing with more native

--- src/db-client/string.ts
+++ src/db-client/string.ts
@@ -3,7 +3,7 @@ import { Ctx } from './modify/Ctx.js'
 import { resize } from './modify/resize.js'
 import { ENCODER, makeTmpBuffer, writeUint32 } from '../utils/uint8.js'
 import { COMPRESSED, NOT_COMPRESSED } from '../protocol/index.js'
-import { LangCodeEnum } from '../zigTsExports.js'
+import { LangCode, LangCodeEnum } from '../zigTsExports.js'
 
 const { getUint8Array: getTmpBuffer } = makeTmpBuffer(4096) // the usual page size?
 
@@ -46,7 +46,6 @@ export function writeRaw(dst: Uint8Array, s: Uint8Array | string, offset: number
   }
 }
 
-// TODO use writeRAw() here
 export const write = (
   ctx: Ctx,
   value: string,
@@ -54,48 +53,14 @@ export const write = (
   noCompression: boolean,
   lang?: LangCodeEnum,
 ): number | null => {
-  const buf = ctx.buf
-
-  value = value.normalize('NFKD')
-  buf[offset] = lang || 0
-  const { written: l } = ENCODER.encodeInto(value, buf.subarray(offset + 2))
-  let crc = native.crc32(buf.subarray(offset + 2, offset + 2 + l))
-
-  // 50 len maybe if lvl 1
-  if (value.length > 200 && !noCompression) {
-    const insertPos = offset + 6 + l
-    const startPos = offset + 2
-    const endPos = offset + 2 + l
-    const willEnd = insertPos + l
-    resize(ctx, willEnd)
-    buf.copyWithin(insertPos, startPos, endPos)
-    const size = native.compress(buf, offset + 6, l)
-
-    if (size === 0) {
-      resize(ctx, l + 6)
-      buf[offset + 1] = NOT_COMPRESSED
-      ENCODER.encodeInto(value, buf.subarray(offset + 2))
-      writeUint32(buf, crc, offset + l + 2)
-      return l + 6
-    } else {
-      resize(ctx, size + 10)
-      let len = l
-      buf[offset + 1] = COMPRESSED
-      writeUint32(buf, len, offset + 2)
-      writeUint32(buf, crc, offset + size + 6)
-      return size + 10
-    }
-  } else {
-    buf[offset + 1] = NOT_COMPRESSED
-    writeUint32(buf, crc, offset + 2 + l)
-    return l + 6
-  }
+  resize(ctx, native.stringByteLength(value) * 2 + 10)
+  return writeRaw(ctx.buf, value, offset, lang || LangCode.none, noCompression)
 }
 
 export const stringCompress = (str: string): Uint8Array => {
   const len = ENCODER.encode(str).byteLength
   const tmpCompressBlock = getTmpBuffer(len * 3)
-  const l = write({ buf: tmpCompressBlock } as Ctx, str, 0, false)!
+  const l = writeRaw(tmpCompressBlock, str, 0, LangCode.none, false)
   const nBuffer = new Uint8Array(l)
   nBuffer.set(tmpCompressBlock.subarray(0, l))
   return nBuffer
