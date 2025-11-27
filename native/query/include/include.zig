44d6faf37e4162ed447552d2705b1e8341889794
Jim de Beer <beerdejim@gmail.com>
2025-11-27 11:53:43 +0100

    fix

--- native/query/include/include.zig
+++ native/query/include/include.zig
@@ -137,8 +137,11 @@ pub fn include(
                     t.PropType.binary, t.PropType.string, t.PropType.json => {
                         try append.stripCrc32(ctx.thread, header.prop, value);
                     },
+                    t.PropType.microBuffer, t.PropType.vector, t.PropType.colVec => {
+                        try ctx.thread.query.append(header.prop);
+                        try ctx.thread.query.append(value);
+                    },
                     else => {
-                        std.debug.print("yo? {any} \n", .{value});
                         try append.default(ctx.thread, header.prop, value);
                     },
                 }
