184434470d12455128556e0e89f570d4284b9483
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-11-27 08:27:09 +0100

    z for real

--- src/db-client/string.ts
+++ src/db-client/string.ts
@@ -7,79 +7,19 @@ import { LangCode, LangCodeEnum } from '../zigTsExports.js'
 
 const { getUint8Array: getTmpBuffer } = makeTmpBuffer(4096) // the usual page size?
 
-// export const write = (
-//   ctx: Ctx,
-//   value: string,
-//   offset: number,
-//   noCompression: boolean,
-//   lang?: LangCodeEnum,
-// ): number | null => {
-//   const buf = ctx.buf
-//   value = value.normalize('NFKD')
-//   buf[offset] = lang || 0
-//   const { written: l } = ENCODER.encodeInto(value, buf.subarray(offset + 2))
-//   let crc = native.crc32(buf.subarray(offset + 2, offset + 2 + l))
-//   // 50 len maybe if lvl 1
-//   if (value.length > 200 && !noCompression) {
-//     const insertPos = offset + 6 + l
-//     const startPos = offset + 2
-//     const endPos = offset + 2 + l
-//     const willEnd = insertPos + l
-//     resize(ctx, willEnd)
-//     buf.copyWithin(insertPos, startPos, endPos)
-//     const size = native.compress(buf, offset + 6, l)
-//     if (size === 0) {
-//       resize(ctx, l + 6)
-//       buf[offset + 1] = NOT_COMPRESSED
-//       ENCODER.encodeInto(value, buf.subarray(offset + 2))
-//       writeUint32(buf, crc, offset + l + 2)
-//       return l + 6
-//     } else {
-//       resize(ctx, size + 10)
-//       let len = l
-//       buf[offset + 1] = COMPRESSED
-//       writeUint32(buf, len, offset + 2)
-//       writeUint32(buf, crc, offset + size + 6)
-//       return size + 10
-//     }
-//   } else {
-//     buf[offset + 1] = NOT_COMPRESSED
-//     writeUint32(buf, crc, offset + 2 + l)
-//     return l + 6
-//   }
-// }
-
-// TODO olli - this is broken does not compress big strings
-export function writeRaw(
-  dst: Uint8Array,
-  s: Uint8Array | string,
-  offset: number,
-  lang: LangCodeEnum,
-  noCompression: boolean,
-): number {
-  if (s instanceof Uint8Array) {
-    dst.set(s, offset)
-    return dst.byteLength
-  } else if (noCompression) {
-    const value = s.normalize('NFKD')
+function writeRawInternal(dst: Uint8Array, value: string, offset: number, lang: LangCodeEnum, noCompression: boolean): number {
+  if (noCompression) {
     dst[offset] = lang
     dst[offset + 1] = NOT_COMPRESSED
     const l = native.stringToUint8Array(value, dst, offset + 2)
     let crc = native.crc32(dst.subarray(offset + 2, offset + 2 + l))
     writeUint32(dst, crc, offset + 2 + l)
-    return l + 6
-  } else {
-    // Try to compress
-    const value = s.normalize('NFKD')
+    return l + 6;
+  } else { // Try to compress
     const l = native.stringByteLength(value)
 
-    console.log('COMPRESS', l, dst.byteLength - offset < 2 * l + 10)
-
-    // this is all broken
     if (l <= 200 || dst.byteLength - offset < 2 * l + 10) {
-      console.log('writeRaw ?', l)
-
-      return writeRaw(dst, s, offset, lang, true)
+      return writeRaw(dst, value, offset, lang, true)
     }
 
     const insertPos = offset + 6 + l
@@ -89,7 +29,7 @@ export function writeRaw(
     const size = native.compress(dst, offset + 6, l)
     if (size == 0) {
       // Didn't compress
-      return writeRaw(dst, s, offset, lang, true)
+      return writeRaw(dst, value, offset, lang, true)
     }
 
     dst[offset] = lang
@@ -100,6 +40,15 @@ export function writeRaw(
   }
 }
 
+export function writeRaw(dst: Uint8Array, value: Uint8Array | string, offset: number, lang: LangCodeEnum, noCompression: boolean): number {
+  if (value instanceof Uint8Array) {
+    dst.set(value, offset)
+    return dst.byteLength
+  } else {
+    return writeRawInternal(dst, value.normalize('NFKD'), offset, lang, noCompression)
+  }
+}
+
 export const write = (
   ctx: Ctx,
   value: string,
@@ -107,15 +56,16 @@ export const write = (
   noCompression: boolean,
   lang?: LangCodeEnum,
 ): number | null => {
-  //
-  resize(ctx, native.stringByteLength(value) * 2 + 10)
-  return writeRaw(ctx.buf, value, offset, lang || LangCode.none, noCompression)
+  const s = value.normalize('NFKD')
+  resize(ctx, native.stringByteLength(s) * 2 + 10)
+  return writeRawInternal(ctx.buf, s, offset, lang || LangCode.none, noCompression)
 }
 
 export const stringCompress = (str: string): Uint8Array => {
-  const len = ENCODER.encode(str).byteLength
-  const tmpCompressBlock = getTmpBuffer(len * 3)
-  const l = writeRaw(tmpCompressBlock, str, 0, LangCode.none, false)
+  const s = str.normalize('NFKD')
+  const len = ENCODER.encode(s).byteLength
+  const tmpCompressBlock = getTmpBuffer(2 * len + 10)
+  const l = writeRawInternal(tmpCompressBlock, str, 0, LangCode.none, false)
   const nBuffer = new Uint8Array(l)
   nBuffer.set(tmpCompressBlock.subarray(0, l))
   return nBuffer
