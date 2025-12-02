5d9966d3aef0d4cfd772f4e82763668d505ac275
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-12-02 11:40:39 +0100

    Build locale into dist

--- locale/build.sh
+++ locale/build.sh
@@ -8,6 +8,5 @@ fi
 if test "$OSTYPE" == "linux-gnu" && test "$1" != "release"; then
     make -j4
 else
-    podman run --rm -v "$PWD/../..:/usr/src/based-db" based-db-clibs-build-linux_aarch64 /bin/bash -c "cd /usr/src/based-db/packages/locale && make -j4"
-    podman run --rm -v "$PWD/../..:/usr/src/based-db" based-db-clibs-build-linux_x86_64 /bin/bash -c "cd /usr/src/based-db/packages/locale && make -j4"
+    podman run --rm -v "$PWD/..:/usr/src/based-db" based-db-clibs-build-linux_aarch64 /bin/bash -c "cd /usr/src/based-db/locale && make -j4"
 fi
