3c03091214fefa7950a2fbf2987080f7aea018de
Olli Vanhoja <olli.vanhoja@gmail.com>
2025-12-16 12:49:16 +0100

    data -> resp

--- native/selva/info.zig
+++ native/selva/info.zig
@@ -8,7 +8,7 @@ const DbCtx = @import("../db/ctx.zig").DbCtx;
 
 pub fn blockHash(thread: *Thread.Thread, ctx: *DbCtx, q: []u8, op: t.OpType) !void {
     const id = utils.read(u32, q, 0);
-    const data = try thread.query.result(20, id, op);
+    const resp = try thread.query.result(20, id, op);
     const start = utils.read(u32, q, 5);
     const typeCode = utils.read(u16, q, 9);
     const typeEntry = selva.c.selva_get_type_by_index(ctx.selva.?, typeCode);
@@ -16,7 +16,7 @@ pub fn blockHash(thread: *Thread.Thread, ctx: *DbCtx, q: []u8, op: t.OpType) !vo
     if (typeEntry) |te| {
         var hash: SelvaHash128 = 0;
         err = Node.getNodeBlockHash(ctx, te, start, &hash);
-        utils.byteCopy(data, &hash, 4);
+        utils.byteCopy(resp, &hash, 4);
     }
-    utils.write(data, err, 0);
+    utils.write(resp, err, 0);
 }
