b759149e723ee2c599b4ded43ad150707b8ed781
Olli Vanhoja <olli.vanhoja@gmail.com>
2026-01-29 15:18:05 +0100

    Now it works??

--- test/text/textMany.ts
+++ test/text/textMany.ts
@@ -1,11 +1,8 @@
 import { BasedDb } from '../../src/index.js'
 import test from '../shared/test.js'
-import { deepEqual } from '../shared/assert.js'
-import { notEqual } from 'node:assert'
 import { fastPrng } from '../../src/utils/fastPrng.js'
 
-//const N_PROPS = 248
-const N_PROPS = 127
+const N_PROPS = 248
 
 const languages = [ 'de', 'en', 'fi', 'it', 'aa', 'ab', 'af', 'ak', 'sq', 'am', 'ar', 'an', 'hy', 'as', 'av', 'ae', 'ay', 'az', 'eu', 'be', 'bn', 'bi', 'bs', 'br', 'bg', 'my', 'ca', 'km', 'ce', 'zh', 'cv', 'kw', 'co', 'hr', 'cs', 'da', 'dv', 'nl', 'dz', 'et', 'fo', 'fr', 'ff', 'gd', 'gl', 'gsw', 'el', 'kl', 'gu', 'ht', 'ha', 'he', 'hi', 'hu', 'is', 'ig', 'id', 'ia', 'iu', 'ik', 'ga', 'it', 'ja', 'kn', 'ks', 'kk', 'rw', 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'lb', 'li', 'ln', 'lt', 'mk', 'mg', 'ms', 'ml', 'mt', 'gv', 'mi', 'ro', 'mn', 'ne', 'se', 'no', 'nb', 'nn', 'oc', 'or', 'om', 'os', 'pa', 'ps', 'fa', 'pl', 'pt', 'qu', 'rm', 'ru', 'sm', 'sa', 'sc', 'sr', 'sd', 'si', 'sk', 'sl', 'so', 'st', 'nr', 'es', 'sw', 'ss', 'sv', 'tl', 'tg', 'ta', 'tt', 'te', 'th', 'bo', 'ti', 'to', 'ts', 'tn', 'tr', 'tk', 'ug', 'uk', 'ur', 'uz', 've', 'vi', 'wa', 'cy', 'fy', 'wo', 'xh', 'yi', 'yo', 'zu', 'ka', 'cnr', ] as const // prettier-ignore
 
@@ -68,6 +65,8 @@ await test('Many text props', async (t) => {
     },
   })
 
+  //console.log(JSON.stringify(db.server.schemaTypesParsed, null, 2))
+
   await db.create('dialog', setTextProps())
-  console.log(await db.query('dialog').include('*').get())
+  //console.log(await db.query('dialog').include('*').get())
 })
