3a0cb551591cbe0bb8d20de36ca0d2629b0e5072
youzi <youri@saulx.com>
2026-02-03 14:30:16 +0100

    init aggregate ast update

--- test/query-ast/aggregate.ts
+++ test/query-ast/aggregate.ts
@@ -1,12 +1,4 @@
-import { getTypeDefs } from '../../dist/schema/defs/getTypeDefs.js'
 import { QueryAst } from '../../src/db-query/ast/ast.js'
-import { astToQueryCtx } from '../../src/db-query/ast/toCtx.js'
-import {
-  resultToObject,
-  serializeReaderSchema,
-} from '../../src/protocol/index.js'
-import { BasedDb, debugBuffer } from '../../src/sdk.js'
-import { AutoSizedUint8Array } from '../../src/utils/AutoSizedUint8Array.js'
 import { testDb } from '../shared/index.js'
 
 import test from '../shared/test.js'
@@ -23,4 +15,11 @@ await test('aggregate', async (t) => {
   const a = client.create('user', {
     age: 18,
   })
+
+  const ast: QueryAst = {
+    type: 'user',
+    // aggregate: {
+    //   props: {},
+    // },
+  }
 })
