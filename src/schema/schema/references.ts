630a07062f83b8fda486c03f687ab837cea26312
youzi <youri@saulx.com>
2026-02-06 10:17:15 +0100

    add capped to references

--- src/schema/schema/references.ts
+++ src/schema/schema/references.ts
@@ -1,7 +1,7 @@
 import { parseBase, type Base } from './base.js'
 import { parseReference, type SchemaReference } from './reference.js'
 import type { SchemaOut } from './schema.js'
-import { assert, isRecord, type RequiredIfStrict } from './shared.js'
+import { assert, isNatural, isRecord, type RequiredIfStrict } from './shared.js'
 
 export type SchemaReferences<strict = true, nested = false> = Base &
   RequiredIfStrict<{ type: 'references' }, strict> & {
@@ -14,8 +14,13 @@ export const parseReferences = (
   locales: SchemaOut['locales'],
 ): SchemaReferences => {
   assert(isRecord(def.items), 'Items should be record')
+  assert(
+    def.capped === undefined || isNatural(def.capped),
+    'Capped should be a number',
+  )
   return parseBase<SchemaReferences>(def, {
     type: 'references',
+    capped: def.capped,
     items: parseReference(def.items, locales, true),
   })
 }
