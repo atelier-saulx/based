8cdef27f11f77a9a6e146cb25dd18b61d0b2defd
Olli Vanhoja <olli.vanhoja@gmail.com>
2026-02-06 15:40:41 +0100

    raw string test

--- test/raw.ts
+++ test/raw.ts
@@ -1,14 +1,14 @@
 import { BasedDb } from '../src/index.js'
 import { deepEqual } from './shared/assert.js'
+import {italy} from './shared/examples.js'
 import test from './shared/test.js'
 
-await test('cardinality', async (t) => {
+await test.skip('cardinality', async (t) => {
   const db = new BasedDb({
     path: t.tmp,
   })
 
   await db.start({ clean: true })
-
   t.after(() => t.backup(db))
 
   await db.setSchema({
@@ -34,4 +34,45 @@ await test('cardinality', async (t) => {
 
   const [a, b] = await db.query('user').get().toObject()
   deepEqual(a.uniqueSkills, b.uniqueSkills)
+})
+
+await test('string', async (t) => {
+  const db = new BasedDb({
+    path: t.tmp,
+  })
+
+  await db.start({ clean: true })
+  t.after(() => t.backup(db))
+
+  await db.setSchema({
+    types: {
+      user: {
+        name: 'string',
+        role: { type: 'string', maxBytes: 4 },
+        resume: { type: 'string', compression: 'deflate' },
+      },
+    },
+  })
+
+  const one = await db.create('user', {
+    name: 'user',
+    role: 'root',
+    resume: italy,
+  })
+  const { name, role, resume } = await db
+    .query('user', one)
+    .include('name', { raw: true })
+    .get()
+    .toObject()
+
+  await db.create('user', {
+    name,
+    role,
+    resume,
+  })
+
+  const [a, b] = await db.query('user').get().toObject()
+  deepEqual(a.name, b.name)
+  deepEqual(a.role, b.role)
+  deepEqual(a.resume, b.resume)
 })
