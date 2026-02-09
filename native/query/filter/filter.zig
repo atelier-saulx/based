19354737825b4576350d06ae1b051e57c483a714
Olli Vanhoja <olli.vanhoja@gmail.com>
2026-02-09 17:09:02 +0100

    Fix filter by alias

--- native/query/filter/filter.zig
+++ native/query/filter/filter.zig
@@ -4,6 +4,7 @@ const utils = @import("../../utils.zig");
 const Node = @import("../../selva/node.zig");
 const Schema = @import("../../selva/schema.zig");
 const Fields = @import("../../selva/fields.zig");
+const Selva = @import("../../selva/selva.zig");
 const t = @import("../../types.zig");
 const Compare = @import("compare.zig");
 const Select = @import("select.zig");
@@ -113,7 +114,11 @@ pub inline fn filter(node: Node.Node, ctx: *Query.QueryCtx, q: []u8) !bool {
 
         if (prop != c.prop) {
             prop = c.prop;
-            v = Fields.getRaw(node, c.fieldSchema);
+            if (c.fieldSchema.type == Selva.c.SELVA_FIELD_TYPE_ALIAS) {
+                v = try Fields.getAliasByNode(try Node.getType(ctx.db, node), node, c.fieldSchema.field);
+            } else {
+                v = Fields.getRaw(node, c.fieldSchema);
+            }
         }
 
         pass = switch (c.op.compare) {
