ff97e73a411d5c8561e6378aaff574012657948e
Marco Vieira <maovieira@gmail.com>
2026-03-05 06:34:00 -0300

    fix iteratorType missing

--- src/db-client/query/aggregates/toByteCode.ts
+++ src/db-client/query/aggregates/toByteCode.ts
@@ -49,7 +49,8 @@ export const aggregateToBuffer = (def: QueryDef): IntermediateByteCode => {
       ...commonHeader,
       op: IncludeOp.referencesAggregation,
       targetProp: def.target.propDef?.prop || 0,
-      aggDefsSize: 0, // FIXME
+      aggDefsSize: 0,
+      iteratorType: getIteratorType(def, hasFilter),
     })
   } else {
     const queryType = isRootCountOnly(def, filterSize)
