6df9b33bf754e68638b245217ba68c9e98eb5379
Marco Vieira <maovieira@gmail.com>
2026-03-05 14:57:50 -0300

    group by edge tests

--- test/aggregate/edges.ts
+++ test/aggregate/edges.ts
@@ -1,6 +1,7 @@
 import test from '../shared/test.js'
 import { deepEqual } from '../shared/assert.js'
 import { testDb } from '../shared/index.js'
+import { uint16 } from '../../src/schema/defs/props/fixed.js'
 
 await test('edges aggregation', async (t) => {
   const db = await testDb(t, {
@@ -14,6 +15,10 @@ await test('edges aggregation', async (t) => {
             prop: 'movies',
             $rating: 'uint16',
             $hating: 'uint16',
+            $role: 'string',
+            $roleType: ['Lead', 'Supporting', 'Cameo', 'Extra', 'Voiceover'],
+            $year: 'uint16',
+            $watched: 'timestamp',
           },
         },
       },
@@ -49,6 +54,10 @@ await test('edges aggregation', async (t) => {
         id: a1,
         $rating: 55,
         $hating: 5,
+        $role: 'Supporting',
+        $roleType: 'Lead',
+        $year: 2003,
+        $watched: new Date('2025-01-01'),
       },
     ],
   })
@@ -59,11 +68,19 @@ await test('edges aggregation', async (t) => {
         id: a1,
         $rating: 63,
         $hating: 7,
+        $role: 'Mia Wallace',
+        $roleType: 'Lead',
+        $year: 1994,
+        $watched: new Date('1994-12-11'),
       },
       {
         id: a2,
         $rating: 77,
         $hating: 3,
+        $role: 'Vincent Vega',
+        $roleType: 'Lead',
+        $year: 1994,
+        $watched: new Date('1994-12-11'),
       },
     ],
   })
@@ -190,8 +207,138 @@ await test('edges aggregation', async (t) => {
     ],
     'multiple edges on same agg function, branched query',
   )
-})
 
-/*-----------------------------------*/
-/*          GROUP BY EDGE TEST       */
-/*-----------------------------------*/
+  /*-----------------------------------*/
+  /*          GROUP BY EDGE.           */
+  /*-----------------------------------*/
+
+  // string edge
+  const strEdg = await db
+    .query2('movie')
+    .sum('actors.strong')
+    .groupBy('actors.$role')
+    .get()
+
+  console.dir(strEdg, { depth: null })
+
+  deepEqual(
+    strEdg,
+    [
+      //@ts-ignore
+      { id: 1, actors: { Supporting: { strong: { sum: 10 } } } },
+      //@ts-ignore
+      {
+        id: 2,
+        actors: {
+          //@ts-ignore
+          'Mia Wallace': { strong: { sum: 10 } },
+          'Vincent Vega': { strong: { sum: 5 } },
+        },
+      },
+    ],
+    'string edge',
+  )
+
+  // enum edge
+  const enumEdg = await db
+    .query2('movie')
+    .sum('actors.$rating')
+    .groupBy('actors.$roleType')
+    .get()
+
+  console.dir(enumEdg, { depth: null })
+
+  deepEqual(
+    enumEdg,
+    [
+      {
+        id: 1,
+        actors: {
+          //@ts-ignore
+          Lead: { $rating: { sum: 55 } },
+        },
+      },
+      {
+        id: 2,
+        actors: {
+          //@ts-ignore
+          Lead: { $rating: { sum: 140 } },
+        },
+      },
+    ],
+    'enum edge',
+  )
+
+  // numeric edge
+  const numEdg = await db
+    .query2('movie')
+    .sum('actors.strong')
+    .groupBy('actors.$year')
+    .get()
+
+  console.dir(numEdg, { depth: null })
+
+  deepEqual(
+    numEdg,
+    [
+      //@ts-ignore
+      { id: 1, actors: { 2003: { strong: { sum: 10 } } } },
+      //@ts-ignore
+      { id: 2, actors: { 1994: { strong: { sum: 15 } } } },
+    ],
+    'numeric edge',
+  )
+
+  // temporal interval edge
+  const tempIntEdg = await db
+    .query2('movie')
+    .sum('actors.strong')
+    .groupBy('actors.$watched', { step: 'year' })
+    .get()
+
+  console.dir(tempIntEdg, { depth: null })
+
+  deepEqual(
+    tempIntEdg,
+    [
+      //@ts-ignore
+      { id: 1, actors: { 2025: { strong: { sum: 10 } } } },
+      //@ts-ignore
+      { id: 2, actors: { 1994: { strong: { sum: 15 } } } },
+    ],
+    'temporal interval edge',
+  )
+
+  // numeric interval edge - is this implemented?
+  const numIntEdg = await db
+    .query2('movie')
+    .sum('actors.strong')
+    .groupBy('actors.$hating', { step: 2 })
+    .get()
+
+  console.dir(numIntEdg, { depth: null })
+
+  deepEqual(
+    numIntEdg,
+    [
+      //@ts-ignore
+      { id: 1, actors: { 5: { strong: { sum: 10 } } } },
+      {
+        id: 2,
+        //@ts-ignore
+        actors: { 3: { strong: { sum: 5 } }, 7: { strong: { sum: 10 } } },
+      },
+    ],
+    'numeric interval edge',
+  )
+
+  // numeric interval edge - is this implemented?
+  console.dir(
+    await db
+      .query2('movie')
+      .sum('actors.strong')
+      .groupBy('actors.$hating', { step: 2 })
+      .get(),
+    { depth: null },
+  )
+})
