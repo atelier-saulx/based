46a98009155d29870f94f4c8c098a238067010a5
Olli Vanhoja <olli.vanhoja@gmail.com>
2026-03-06 14:57:07 +0100

    Working expire

--- native/thread/worker/poll.zig
+++ native/thread/worker/poll.zig
@@ -1,5 +1,6 @@
 const Thread = @import("../thread.zig");
 const std = @import("std");
+const utils = @import("../../utils.zig");
 const common = @import("../common.zig");
 const jemalloc = @import("../../jemalloc.zig");
 const Node = @import("../../selva/node.zig");
@@ -11,18 +12,26 @@ fn handleExpired(threads: *Thread.Threads) void {
     const expired = res.nodes[0..res.n];
 
     for (expired) |node| {
-        var msg = jemalloc.create(t.ModifyDeleteHeader);
-        msg.* = t.ModifyDeleteHeader{
+
+        const msg = jemalloc.alloc(u8, utils.sizeOf(t.ModifyHeader) + utils.sizeOf(t.ModifyDeleteHeader));
+        utils.write(msg, t.ModifyHeader{
+            .opId = 0,
+            .opType = t.OpType.modify,
+            .schema = 0,
+            .count = 1,
+        }, 0);
+        utils.write(msg, t.ModifyDeleteHeader{
             .op = t.Modify.delete,
             .type = Node.getNodeTypeId(node.?),
             .id = Node.getNodeId(node.?),
             .isTmp = false,
             ._padding = 0,
-        };
+        }, utils.sizeOf(t.ModifyHeader));
 
-        threads.modifyLocked(@ptrCast(&msg)) catch |e| {
+        threads.modifyLocked(msg) catch |e| {
             std.log.err("Dispatching expire delete failed: {any}", .{ e });
         };
+        // TODO Lol we just leaked that buffer
     }
 }
 
@@ -44,6 +53,7 @@ pub fn poll(threads: *Thread.Threads) !void {
             const elapsed = now - thread.lastModifyTime;
             if (elapsed > common.SUB_EXEC_INTERVAL) {
                 thread.lastModifyTime = now;
+                // TODO If this throws the mutex is never released
                 try Subscription.fireIdSubscription(threads, thread);
             }
         }
