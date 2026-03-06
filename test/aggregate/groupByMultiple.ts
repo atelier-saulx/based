f997b305a48005f5e3b35432e4c820e740011893
Marco Vieira <maovieira@gmail.com>
2026-03-06 17:24:55 -0300

    test preps

--- test/aggregate/groupByMultiple.ts
+++ test/aggregate/groupByMultiple.ts
@@ -2,96 +2,104 @@ import test from '../shared/test.js'
 import { deepEqual } from '../shared/assert.js'
 import { testDb } from '../shared/index.js'
 
-
 const gender = ['male', 'female']
 const status = ['sent', 'delivered', 'failed']
 
 await test('multiple group by', async (t) => {
-    const db = await testDb(t, {
-        types: {
-            report: {
-                props: {
-                    date: 'timestamp',
-                    customer: {
-                        ref: 'customer',
-                        prop: 'reports',
-                    },
-                    smsStatus: status
-                },
-            },
-            customer: {
-                props: {
-                    name: 'string',
-                    gender: gender,
-                    age: 'uint8',
-                    reports: {
-                        items: {
-                            ref: 'report',
-                            prop: 'customer',
-                        },
-                    },
-                },
-            },
+  const db = await testDb(t, {
+    types: {
+      report: {
+        props: {
+          date: 'timestamp',
+          customer: {
+            ref: 'customer',
+            prop: 'reports',
+          },
+          smsStatus: status,
         },
-    })
+      },
+      customer: {
+        props: {
+          name: 'string',
+          gender: gender,
+          age: 'uint8',
+          reports: {
+            items: {
+              ref: 'report',
+              prop: 'customer',
+            },
+          },
+        },
+      },
+    },
+  })
 
-    const c1 = db.create('customer', {
-        name: 'a',
-        gender: 'male',
-        age: 10,
-    })
-    const c2 = db.create('customer', {
-        name: 'b',
-        gender: 'female',
-        age: 20,
-    })
+  const c1 = db.create('customer', {
+    name: 'Homer',
+    gender: 'male',
+    age: 10,
+  })
+  const c2 = db.create('customer', {
+    name: 'Margie',
+    gender: 'female',
+    age: 20,
+  })
 
-    const dtFormat = new Intl.DateTimeFormat('pt-BR', {
-        dateStyle: 'short',
-        // timeStyle: 'short',
-        timeZone: 'UTC',
-    })
+  const dtFormat = new Intl.DateTimeFormat('pt-BR', {
+    dateStyle: 'short',
+    timeZone: 'UTC',
+  })
 
-    const d1 = db.create('report', {
-        date: new Date('2022-01-01'),
-        customer: c1,
-        smsStatus: 'sent',
-    })
-    const d2 = db.create('report', {
-        date: new Date('2022-01-01'),
-        customer: c2,
-        smsStatus: 'sent',
-    })
-    const d3 = db.create('report', {
-        date: new Date('2022-01-02'),
-        customer: c1,
-        smsStatus: 'sent',
-    })
-    const d4 = db.create('report', {
-        date: new Date('2022-01-02'),
-        customer: c2,
-        smsStatus: 'sent',
-    })
-    const d5 = db.create('report', {
-        date: new Date('2022-01-02'),
-        customer: c2,
-        smsStatus: 'delivered',
-    })
-    const d6 = db.create('report', {
-        date: new Date('2022-01-02'),
-        customer: c2,
-        smsStatus: 'failed',
-    })
+  const d1 = db.create('report', {
+    date: new Date('2022-01-01'),
+    customer: c1,
+    smsStatus: 'sent',
+  })
+  const d2 = db.create('report', {
+    date: new Date('2022-01-01'),
+    customer: c2,
+    smsStatus: 'sent',
+  })
+  const d3 = db.create('report', {
+    date: new Date('2022-01-02'),
+    customer: c1,
+    smsStatus: 'sent',
+  })
+  const d4 = db.create('report', {
+    date: new Date('2022-01-02'),
+    customer: c2,
+    smsStatus: 'sent',
+  })
+  const d5 = db.create('report', {
+    date: new Date('2022-01-02'),
+    customer: c2,
+    smsStatus: 'delivered',
+  })
+  const d6 = db.create('report', {
+    date: new Date('2022-01-02'),
+    customer: c2,
+    smsStatus: 'failed',
+  })
 
-    // get report count by date
-    const r1 = await db.query2('report').count()
-        .groupBy('date', { display: dtFormat })
-        .get()
+  // get report count by date
+  const r1 = await db
+    .query2('report')
+    .count()
+    .groupBy('date', { display: dtFormat })
+    .get()
 
-    console.dir(r1, { depth: null })
+  deepEqual(
+    r1,
+    { '02/01/2022': { count: 4 }, '01/01/2022': { count: 2 } },
+    'report count by date',
+  )
 
-    deepEqual(r1,
-        { '02/01/2022': { count: 4 }, '01/01/2022': { count: 2 } }, 'report count by date'
-    )
+  // get report count by date per genre
+  //await db.query2('customer').avg('age').groupBy('gender').get()
+  const r2 = await db
+    .query2('customer')
+    .include('name', (q) => q('reports').count())
+    .get()
 
+  console.dir(r2, { depth: null })
 })
