4d794149ce57524afe9f8094ee3bb662a11074ef
Marco Vieira <maovieira@gmail.com>
2026-03-07 17:26:57 -0300

    groupBy multiple test prep

--- test/aggregate/groupByMultiple.ts
+++ test/aggregate/groupByMultiple.ts
@@ -16,6 +16,7 @@ await test('multiple group by', async (t) => {
             prop: 'reports',
           },
           smsStatus: status,
+          cost: 'number',
         },
       },
       customer: {
@@ -23,6 +24,7 @@ await test('multiple group by', async (t) => {
           name: 'string',
           gender: gender,
           age: 'uint8',
+          region: 'string',
           reports: {
             items: {
               ref: 'report',
@@ -37,12 +39,32 @@ await test('multiple group by', async (t) => {
   const c1 = db.create('customer', {
     name: 'Homer',
     gender: 'male',
-    age: 10,
+    age: 50,
+    region: 'south',
   })
   const c2 = db.create('customer', {
     name: 'Margie',
     gender: 'female',
-    age: 20,
+    age: 49,
+    region: 'east',
+  })
+  const c3 = db.create('customer', {
+    name: 'Lisa Marie',
+    gender: 'female',
+    age: 7,
+    region: 'east',
+  })
+  const c4 = db.create('customer', {
+    name: 'Bart',
+    gender: 'male',
+    age: 9,
+    region: 'central',
+  })
+  const c5 = db.create('customer', {
+    name: 'Jessica Lovejoy',
+    gender: 'female',
+    age: 8,
+    region: 'central',
   })
 
   const dtFormat = new Intl.DateTimeFormat('pt-BR', {
@@ -54,52 +76,101 @@ await test('multiple group by', async (t) => {
     date: new Date('2022-01-01'),
     customer: c1,
     smsStatus: 'sent',
+    cost: 1.99,
   })
   const d2 = db.create('report', {
     date: new Date('2022-01-01'),
     customer: c2,
     smsStatus: 'sent',
+    cost: 0.85,
   })
   const d3 = db.create('report', {
     date: new Date('2022-01-02'),
     customer: c1,
     smsStatus: 'sent',
+    cost: 1.99,
   })
   const d4 = db.create('report', {
     date: new Date('2022-01-02'),
     customer: c2,
     smsStatus: 'sent',
+    cost: 0.99,
   })
   const d5 = db.create('report', {
     date: new Date('2022-01-02'),
     customer: c2,
     smsStatus: 'delivered',
+    cost: 0.85,
   })
   const d6 = db.create('report', {
     date: new Date('2022-01-02'),
     customer: c2,
     smsStatus: 'failed',
+    cost: 0.5,
   })
 
-  // get report count by date
-  const r1 = await db
-    .query('report')
-    .count()
-    .groupBy('date', { display: dtFormat })
+  //   // get report count by date
+  //   const r1 = await db
+  //     .query2('report')
+  //     .count()
+  //     .groupBy('date', { display: dtFormat })
+  //     .get()
+
+  //   deepEqual(
+  //     r1,
+  //     { '02/01/2022': { count: 4 }, '01/01/2022': { count: 2 } },
+  //     'report count by date',
+  //   )
+
+  //   // get report count by date per genre
+  //   //await db.query2('customer').avg('age').groupBy('gender').get()
+  //   const r2 = await db
+  //     .query2('customer')
+  //     .include('name', (q) => q('reports').count())
+  //     .get()
+
+  //   console.dir(r2, { depth: null })
+
+  //   // get sum of sms costs group by sms status per customer
+  //   const r3 = await db
+  //     .query2('customer')
+  //     .include('name')
+  //     .sum('reports.cost')
+  //     .groupBy('reports.smsStatus')
+  //     .get()
+
+  //   console.dir(r3, { depth: null })
+
+  //
+  const r4 = await db
+    .query('customer')
+    .avg('age')
+    //@ts-ignore
+    .groupBy('region', 'gender') // same effect of  .groupBy('region').groupBy('gender')
     .get()
+
+  console.dir(r4, { depth: null })
 
   deepEqual(
-    r1,
-    { '02/01/2022': { count: 4 }, '01/01/2022': { count: 2 } },
-    'report count by date',
+    r4,
+
+    {
+      central: {
+        //@ts-ignore
+        // female: { age: { avg: 0 } }, // no data, no key
+        male: { age: { avg: 9 } },
+      },
+      south: {
+        //@ts-ignore
+        // female: { age: { avg: 0 } },  // no data, no key
+        male: { age: { avg: 50 } },
+      },
+      east: {
+        //@ts-ignore
+        female: { age: { avg: 28 } },
+        // male: { age: { avg: 0 } }, // no data, no key
+      },
+    },
+    'avg customers age group by region and gender',
   )
-
-  // get report count by date per genre
-  //await db.query('customer').avg('age').groupBy('gender').get()
-  const r2 = await db
-    .query('customer')
-    .include('name', (q) => q('reports').count())
-    .get()
-
-  console.dir(r2, { depth: null })
 })
