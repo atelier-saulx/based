66bdafa30d9eb8a64638508aa59ba221b177eddc
Olli Vanhoja <olli.vanhoja@gmail.com>
2026-03-09 17:38:48 +0100

    Comment

--- native/thread/common.zig
+++ native/thread/common.zig
@@ -8,7 +8,7 @@ const References = @import("../selva/references.zig");
 const Subscription = @import("../subscription/common.zig");
 const EdgeResultSize = @sizeOf(References.ReferencesIteratorEdgesResult);
 
-pub const SUB_EXEC_INTERVAL = 1000_000_000;
+pub const SUB_EXEC_INTERVAL = 1000_000_000; // ns
 
 pub const Thread = struct {
     thread: std.Thread,
