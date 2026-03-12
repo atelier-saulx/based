90254704c7ca9e113d56177cf97829eb1406f628
Marco Vieira <maovieira@gmail.com>
2026-03-12 13:15:18 -0300

    removing log

--- src/db-query/ast/toCtx.ts
+++ src/db-query/ast/toCtx.ts
@@ -106,7 +106,7 @@ export const astToQueryCtx = (
   }
 
   // can use same buf for sub
-  console.log(query.view.slice())
+  // console.log(query.view.slice())
   return {
     query: query.view.slice(),
     readSchema: ctx.readSchema,
